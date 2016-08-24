532d0127df04e378a7294c9da5905860fa1bc081
Joseph Svrcek <joseph.svrcek@rgi-corp.com>
2016-08-24 15:34:16 -0400

    squashed and merged migrations added default provider template

--- oet2/jobs/migrations/create_tag_gin_index.py
+++ oet2/jobs/migrations/create_tag_gin_index.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('jobs', '0027_tag_geom_types'),
+        ('jobs', '0002_auto_20160824_1831'),
     ]
 
     operations = [
