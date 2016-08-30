2f393efed43389f1ab8674726bfc9ebae9da67ed
Joseph Svrcek <joseph.svrcek@rgi-corp.com>
2016-08-30 13:30:41 -0400

    added admin user to procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py migrate && gunicorn eventkit_cloud.wsgi --log-level=debug --logger-class=simple -b 0.0.0.0:$PORT
+web: python manage.py migrate && python manage.py loaddata eventkit_cloud/fixtures/admin_user.json && gunicorn eventkit_cloud.wsgi --log-level=debug --logger-class=simple -b 0.0.0.0:$PORT
--- eventkit_cloud/wsgi.py
+++ eventkit_cloud/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventkit_cloud.settings.prod")
 
 from whitenoise.django import DjangoWhiteNoise
-from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
 
