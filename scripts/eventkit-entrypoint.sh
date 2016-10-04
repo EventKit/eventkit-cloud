a577286cb8e8f94dbd9914ebdc9d339e17bd3543
Luke <luke.rees@rgi-corp.com>
2016-10-04 17:04:08 +0000

    add port for rabbitmq

--- scripts/eventkit-entrypoint.sh
+++ scripts/eventkit-entrypoint.sh
@@ -4,4 +4,4 @@
 /var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py migrate
 /var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py loaddata /var/lib/eventkit/eventkit_cloud/fixtures/admin_user.json
 chown -R eventkit:eventkit /var/log/eventkit /var/lib/eventkit
-cd /var/lib/eventkit && /var/lib/eventkit/.virtualenvs/eventkit/bin/gunicorn eventkit_cloud.wsgi:application --bind cloud.eventkit.dev:80 --worker-class eventlet --workers 1 --threads 2 --name eventkit --user eventkit --no-sendfile
+cd /var/lib/eventkit && /var/lib/eventkit/.virtualenvs/eventkit/bin/gunicorn eventkit_cloud.wsgi:application --bind 0.0.0.0:80 --worker-class eventlet --workers 1 --threads 2 --name eventkit --user eventkit --no-sendfile
