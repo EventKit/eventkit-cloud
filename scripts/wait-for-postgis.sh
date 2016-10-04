5a8d7f2557de14b23d4523c07dd5eb6c86436fe9
lukerees <luke.rees@rgi-corp.com>
2016-10-04 10:55:14 -0400

    Use conn string and change celery entrypoint

--- scripts/wait-for-postgis.sh
+++ scripts/wait-for-postgis.sh
@@ -2,12 +2,11 @@
 
 set -e
 
-host="$1"
-username="$2"
+connectionString="$1"
 shift
 cmd="$@"
 
-until psql -h "$host" -U "$username" -c '\l'; do
+until psql "$connectionString" -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
