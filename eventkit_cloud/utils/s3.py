b0f484c9745898b69c9075766b0eeb95bbe2eb7d
Michael Van Veen <michael@mvanveen.net>
2016-10-05 19:04:24 -0700

    initial s3 tweaks

--- eventkit_cloud/utils/s3.py
+++ eventkit_cloud/utils/s3.py
@@ -4,9 +4,18 @@ import boto3
 from django.conf import settings
 
 
+def get_s3_client():
+    return boto3.client(
+        's3',
+        aws_access_key_id=settings.AWS_ACCESS_KEY,
+        aws_secret_access_key=settings.AWS_SECRET_KEY,
+    )
 
-def upload_to_s3(run_uuid, slug, filename):
-    BUCKET_NAME = settings.AWS_BUCKET_NAME
+
+def upload_to_s3(run_uuid, slug, filename, client=None):
+    if not client:
+        client = get_s3_client()
+
     asset_path = os.path.join(
         settings.EXPORT_DOWNLOAD_ROOT,
         run_uuid,
@@ -14,26 +23,36 @@ def upload_to_s3(run_uuid, slug, filename):
         filename
     )
     asset_remote_path = os.path.join(os.path.join(run_uuid, slug), filename)
-    client = boto3.client(
-        's3',
-        aws_access_key_id=settings.AWS_ACCESS_KEY,
-        aws_secret_access_key=settings.AWS_SECRET_KEY,
-    )
-
     with open(asset_path, 'rb') as asset_file:
         asset_file.seek(0)
         client.put_object(
-            Bucket=BUCKET_NAME, 
+            Bucket=settings.AWS_BUCKET_NAME,
             Key=asset_remote_path,
             Body=asset_file.read()
         )
 
     client.put_object_acl(
         ACL='public-read',
-        Bucket=BUCKET_NAME,
+        Bucket=settings.AWS_BUCKET_NAME,
         Key=asset_remote_path
     )
     return client.generate_presigned_url(
         'get_object',
-        Params={'Bucket': BUCKET_NAME, 'Key': asset_remote_path}
+        Params={'Bucket': settings.AWS_BUCKET_NAME, 'Key': asset_remote_path}
     ).split('?')[0]
+
+
+def delete_from_s3(run_uuid, client=None):
+    if not client:
+        client = get_s3_client()
+
+    path_objects = client.list_objects(
+        Bucket=settings.AWS_BUCKET_NAME,
+        Prefix=run_uuid
+    )
+    for item in path_objects.get('Contents', []):
+        _key = item['Key']
+        client.delete_object(
+            Bucket=settings.AWS_BUCKET_NAME,
+            Key=_key,
+        )
---
+++ eventkit_cloud/utils/tests/test_s3.py
@@ -0,0 +1,120 @@
+import datetime
+import os
+from StringIO import StringIO
+
+import botocore
+import botocore.session
+from botocore.stub import Stubber, ANY
+from django.conf import settings
+from django.test import TestCase
+from django.test.utils  import override_settings
+from mock import patch, mock_open
+
+from eventkit_cloud.utils.s3 import (
+    delete_from_s3,
+    get_s3_client,
+    upload_to_s3,
+)
+
+# TODO: override settings.EXPORT_DOWNLOAD_ROOT to be test dir?
+
+@override_settings(AWS_ACCESS_KEY='d3adb33f')
+@override_settings(AWS_SECRET_KEY='d3adb33f')
+class TestS3Util(TestCase):
+    def setUp(self):
+        settings.EXPORT_DOWNLOAD_ROOT = 'test'
+
+        self._uuid = 'd34db33f'
+        self._slug = 'test'
+        self._filename = 'cool.pbf'
+
+        self._asset_path = os.path.join(
+            settings.EXPORT_DOWNLOAD_ROOT,
+            self._uuid,
+            self._slug,
+            self._filename
+        )
+        self._path = '%s/%s/%s' % (self._uuid, self._slug, self._filename)
+        self._base_response = {
+            'ResponseMetadata': {'RequestId': 'abc123', 'HTTPStatusCode': 200, 'HostId': 'abc123'},
+        }
+
+    def test_s3_put(self):
+        client = get_s3_client()
+        stubber = Stubber(client)
+        stubber.activate()
+        stubber.add_response(
+            'put_object',
+            self._base_response,
+            dict(
+            Bucket=ANY,
+            Key=self._path,
+            Body='test'
+        ))
+        stubber.add_response(
+            'put_object_acl',
+            self._base_response,
+            {'ACL': 'public-read', 'Bucket': ANY, 'Key': ANY}
+        )
+        with patch('eventkit_cloud.utils.s3.open', mock_open(read_data='test'), create=True) as mock_open_obj:
+            upload_to_s3(self._uuid, self._slug, self._filename, client=client)
+
+    def test_s3_delete(self):
+        client = get_s3_client()
+        stubber = Stubber(client)
+        stubber.activate()
+
+        stubber.add_response(
+            'put_object',
+            self._base_response,
+            dict(
+            Bucket=ANY,
+            Key=self._path,
+            Body='test'
+        ))
+        stubber.add_response(
+            'put_object_acl',
+            self._base_response,
+            {'ACL': 'public-read', 'Bucket': ANY, 'Key': self._path}
+        )
+
+        list_objects_response = {
+                'IsTruncated': False,
+                'Name': 'test-bucket',
+                'MaxKeys': 1000, 'Prefix': '',
+                'Contents': [{
+                    u'LastModified': datetime.datetime(2016, 9, 23, 11, 17, 14),
+                    u'ETag': '"20d2cb13afb394301bbea0bcff19e12b"',
+                    u'StorageClass': 'STANDARD',
+                    u'Key': self._path,
+                    u'Owner': {
+                        u'DisplayName': 'test',
+                        u'ID': '31d89f79718dbd4435290740e6fa5e41cffafa7d9a3c323c85b525342e6341ae'
+                    },
+                    u'Size': 77824
+                }],
+                'EncodingType': 'url',
+                'ResponseMetadata': {
+                            'RequestId': 'abc123',
+                            'HTTPStatusCode': 200,
+                            'HostId': 'abc123'
+                        },
+                'Marker': ''
+        }
+        stubber.add_response(
+            'list_objects',
+            list_objects_response, {
+                'Bucket': ANY,
+                'Prefix':self._uuid
+            })
+        stubber.add_response(
+            'delete_object',
+            self._base_response, {
+                'Bucket': ANY,
+                'Key': self._path
+            })
+
+        with patch('eventkit_cloud.utils.s3.open', mock_open(read_data='test'), create=True) as mock_open_obj:
+            upload_to_s3(self._uuid, self._slug, self._filename, client=client)
+
+        delete_from_s3(self._uuid, client=client)
