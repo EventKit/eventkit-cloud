299683f88451b572122ff61c252c065bbec7fa50
lukerees <luke.rees@rgi-corp.com>
2016-10-20 07:39:15 -0400

    add osm provider with fixture

--- scripts/eventkit-entrypoint.sh
+++ scripts/eventkit-entrypoint.sh
@@ -4,5 +4,7 @@
 /var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py migrate
 /var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py loaddata /var/lib/eventkit/eventkit_cloud/fixtures/admin_user.json
 /var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py loaddata /var/lib/eventkit/eventkit_cloud/fixtures/insert_provider_types.json
+/var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py loaddata /var/lib/eventkit/eventkit_cloud/fixtures/osm_provider.json
+
 chown -R eventkit:eventkit /var/log/eventkit /var/lib/eventkit
 cd /var/lib/eventkit && /var/lib/eventkit/.virtualenvs/eventkit/bin/gunicorn eventkit_cloud.wsgi:application --bind 0.0.0.0:6080 --worker-class eventlet --workers 1 --threads 2 --name eventkit --user eventkit --no-sendfile
