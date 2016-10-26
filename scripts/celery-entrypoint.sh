42fc7e40bd569e0fba4115d1d52286774361a90c
Joseph Svrcek <joseph.svrcek@rgi-corp.com>
2016-10-26 09:29:40 -0400

    prototyped setup

--- scripts/celery-entrypoint.sh
+++ scripts/celery-entrypoint.sh
@@ -3,4 +3,4 @@
 #/var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py migrate
 #/var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py loaddata /var/lib/eventkit/eventkit_cloud/fixtures/admin_user.json
 chown -R eventkit:eventkit /var/log/eventkit /var/lib/eventkit
-/var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py celery worker --loglevel debug
+/var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py celery worker --loglevel=$LOGLEVEL -n $HOSTNAME -Q celery,$HOSTNAME
