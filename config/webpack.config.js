437932634356ba6eff382cca43b4e0de21de7d34
Joseph Svrcek <joseph.svrcek@rgi-corp.com>
2016-11-22 07:51:25 -0500

    added hot reload

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,31 +1,50 @@
 var webpack = require('webpack');
 var path = require('path');
+var WriteFilePlugin = require('write-file-webpack-plugin');
 
-var BUILD_DIR = path.resolve('eventkit_cloud','ui','static','ui','build');
-var APP_DIR = path.resolve('eventkit_cloud','ui','static','ui', 'app');
+var BASE_DIR = path.resolve('/var','lib','eventkit','eventkit_cloud', 'ui', 'static', 'ui')
+var BUILD_DIR = path.resolve(BASE_DIR, 'build');
+var APP_DIR = path.resolve(BASE_DIR, 'app');
 
 var config = {
-    entry: APP_DIR + '/main.js',
+    entry: [
+        'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
+        'webpack/hot/dev-server', // "only" prevents reload on syntax errors
+        APP_DIR + '/main.js',
+    ],
     output: {
         path: BUILD_DIR,
-        filename: 'bundle.js'
-    },
+        filename: 'bundle.js',
+        publicPath: BUILD_DIR
+    }
+    ,
     resolve: {
         extensions: ['', '.js', '.jsx']
-      },
+    }
+    ,
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    cacheDirectory: true,
-                    presets: ['react', 'es2015']
-                }
-            }
-        ]
-    }
+                loaders: ['react-hot-loader/webpack', 'babel?presets[]=es2015,presets[]=react']
+            },
+        ],
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new WriteFilePlugin()
+    ],
+    devServer: {
+        hot: true,
+        contentBase: BASE_DIR,
+        outputPath: BUILD_DIR,
+        host: "0.0.0.0",
+        port: 8080,
+    },
+    watchOptions: {
+        poll: true
+    },
 };
 
 
--- eventkit_cloud/ui/static/ui/app/main.js
+++ eventkit_cloud/ui/static/ui/app/main.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <h1>Hello, something NEWEST!!!!</h1>,
+    <h1>Hello, something three!</h1>,
     document.getElementById('root')
 );
