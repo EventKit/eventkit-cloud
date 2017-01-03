2c26a07cf1f998d0605b7a5d5b1488cd24a2a17e
Jen Duncan <jduncan@radiantblue.com>
2017-01-03 15:34:38 -0500

    App Bar has been added to the top of the screen with hamburger menu.

--- eventkit_cloud/ui/static/ui/app/components/Application.js
+++ eventkit_cloud/ui/static/ui/app/components/Application.js
@@ -1,7 +1,7 @@
 import React, {Component, PropTypes} from 'react'
 import {connect} from 'react-redux'
 import {ClassificationBanner} from './ClassificationBanner'
-import {TitleBar} from './TitleBar'
+import TitleBar from './TitleBar'
 import {Navigation} from './Navigation'
 import styles from './Application.css'
 import injectTapEventPlugin from 'react-tap-event-plugin'
@@ -17,7 +17,7 @@ class Application extends Component {
                 <div className={styles.root}>
                     <ClassificationBanner />
                     <TitleBar />
-                    <Navigation />
+                    
                     {this.props.children}
                 </div>
             </MuiThemeProvider>
--- eventkit_cloud/ui/static/ui/app/components/TitleBar.js
+++ eventkit_cloud/ui/static/ui/app/components/TitleBar.js
@@ -1,16 +1,77 @@
 
-import * as React from 'react'
-import styles from './TitleBar.css';
+import React, {Component} from 'react'
 import logo from '../../images/logo_white_medium.png'
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import FontIcon from 'material-ui/FontIcon';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MenuItem from 'material-ui/MenuItem'
+import {red500, yellow500, blue500} from 'material-ui/styles/colors';
 
 
-export const TitleBar = () => (
-    <div className={styles.titleBar}>
-        <div className={styles.mainMenu}>
-            {'MAIN MENU'}
-        </div>
-        <div className={styles.logo}>
-            <img src={logo}/>
-        </div>
-    </div>
-)
+
+
+class TitleBar extends Component {
+    constructor(props){
+        super(props);
+        this.state = {drawerOpen:false};
+    }
+
+    getChildContext() {
+        return {muiTheme: getMuiTheme(baseTheme)};
+    }
+
+
+    handleToggle() {
+        this.setState({drawerOpen: !this.state.drawerOpen});
+        console.log("open")
+    }
+    handleClose() { this.setState({drawerOpen: false}); }
+    render() {
+
+        const styles = {
+            appBar: {
+                backgroundColor: 'black',
+                height: '70px',
+                top: '25px'
+            },
+            img: {
+                position: 'fixed',
+                left: '50%',
+                marginLeft: '-100px',
+                marginTop: '35px'
+            },
+            drawer: {
+                marginTop: '95px',
+                backgroundColor: '#010101',
+            },
+            menuItem: {
+                color: 'white',
+            }
+        };
+
+
+        const img = <img style={styles.img} src={logo}/>
+
+        return (
+        <header className="header">
+            <AppBar style={styles.appBar} title={img} onLeftIconButtonTouchTap={this.handleToggle.bind(this)} />
+            <Drawer containerStyle={styles.drawer}
+                    docked={false}
+                    open={this.state.drawerOpen}
+                    onRequestChange={(open) => this.setState({open})}>
+                <MenuItem style={styles.menuItem} onTouchTap={this.handleClose.bind(this)}>Menu Item 1</MenuItem>
+                <MenuItem style={styles.menuItem} onTouchTap={this.handleClose.bind(this)}>Menu Item 2 </MenuItem>
+                <MenuItem style={styles.menuItem} onTouchTap={this.handleClose.bind(this)}>Menu Item 3 </MenuItem>
+            </Drawer>
+
+        </header>
+        )
+    }
+}
+
+TitleBar.childContextTypes = {
+    muiTheme: React.PropTypes.object.isRequired,
+};
+export default TitleBar
