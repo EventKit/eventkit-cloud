10af2c605324cbdaaa3e3bd1b13b2342bf31abe9
lukerees <luke.rees@rgi-corp.com>
2017-01-12 11:02:43 -0500

    use bootstrap form instead of material-ui for searchbar

--- eventkit_cloud/ui/static/ui/app/components/SearchAOIToolbar.js
+++ eventkit_cloud/ui/static/ui/app/components/SearchAOIToolbar.js
@@ -4,31 +4,25 @@ import ol from 'openlayers'
 import styles from './SearchAOIToolbar.css'
 import DrawControl from './openlayers.DrawControl.js'
 import AutoComplete from 'material-ui/AutoComplete'
+import { FormGroup, FormControl } from 'react-bootstrap';
 
 export default class SearchAOIToolbar extends Component {
 
     constructor(props) {
         super(props)
-        this.handleUpdateInput = this.handleUpdateInput.bind(this)
+
+        this.handleChange = this.handleChange.bind(this);
+
         this.state = {
-            source: ['test',
-                     'testing',
-                     'testing123',
-                     ],
-            searchText: ''
+            value: '',
         }
     }
 
-    handleUpdateInput(input) {
-        this.setState({searchText: input});
-        console.log("Received input '" + input + "'");
-    };
-
-    handleNewRequest() {
-        this.setState({
-            searchText: '',
+    handleChange(e) {
+        this.setState({value: e.target.value}, function() {
+            console.log(this.state.value);
         });
-     };
+    }
 
     render() {
 
@@ -42,16 +36,26 @@ export default class SearchAOIToolbar extends Component {
         return (
             <div className={styles.searchbarDiv}>
                 <i className={'fa fa-search'}/>
-                <AutoComplete
-                    className={styles.searchbarInput}
-                    style={searchbarStyles.searchbar}
-                    searchText={this.state.searchText}
-                    onUpdateInput={this.handleUpdateInput}
-                    onNewRequest={this.handleNewRequest}
-                    dataSource={this.state.source}
-                    hintText={'Search admin boundary or location...'}
-                />
+                <FormGroup validationState={null} controlId={'searchbar'} bsClass={styles.formGroup}>
+                    <FormControl
+                        bsClass={styles.formControl}
+                        type="text"
+                        value={this.state.value}
+                        placeholder="Search admin boundary or location..."
+                        onChange={this.handleChange}
+                    />
+                </FormGroup>
             </div>
         )
     }
 }
+
+//<AutoComplete
+//                    className={styles.searchbarInput}
+//                    style={searchbarStyles.searchbar}
+//                    searchText={this.state.searchText}
+//                    onUpdateInput={this.handleUpdateInput}
+//                    onNewRequest={this.handleNewRequest}
+//                    dataSource={this.state.source}
+//                    hintText={'Search admin boundary or location...'}
+//                />
