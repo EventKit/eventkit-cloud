9ccfcb33c8c78e16f2940e6f8803fc0890a8928d
Joseph Svrcek <joseph.svrcek@rgi-corp.com>
2017-01-12 16:51:48 -0500

    updated entrypoint scripts

--- scripts/celery-entrypoint.sh
+++ scripts/celery-entrypoint.sh
@@ -2,5 +2,5 @@
 
 python /var/lib/eventkit/manage.py collectstatic --noinput
 python /var/lib/eventkit/manage.py migrate
-python /var/lib/eventkit/manage.py celery worker --concurrency=$CONCURRENCY --loglevel=$LOG_LEVEL -n $HOSTNAME -Q celery,$HOSTNAME
+celery worker -A eventkit_cloud --concurrency=$CONCURRENCY --loglevel=$LOG_LEVEL -n $HOSTNAME -Q celery,$HOSTNAME
 
