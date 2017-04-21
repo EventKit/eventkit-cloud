e750ccde7255e060837f5185917c9d9d6d2c9fcc
Joseph Svrcek <joseph.svrcek@rgi-corp.com>
2017-04-21 10:17:43 -0400

    Allow UI to accept any valid GeoJSON

--- eventkit_cloud/ui/static/ui/app/actions/mapToolActions.js
+++ eventkit_cloud/ui/static/ui/app/actions/mapToolActions.js
@@ -7,7 +7,7 @@ export function setBoxButtonSelected() {
 }
 
 export function setFreeButtonSelected() {
-    return {type: types.SET_FREE_SELECTED} 
+    return {type: types.SET_FREE_SELECTED}
 }
 
 export function setMapViewButtonSelected() {
@@ -39,65 +39,117 @@ export function resetGeoJSONFile() {
     }
 }
 
-function bufferGeometry(geometry){
-    const bufferSize = 0.01
-    if(geometry.getGeometryType() === 'Point'){
-        return geometry.buffer(bufferSize)
+/**
+ * Creates a buffer around a jsts geometry if not a Polygon or MultiPolygon.
+ * @param {jstsGeometry} A JSTS geometry.
+ * @return {jstsGeometry} A JSTS geometry.
+ */
+export function bufferGeometry(jstsGeometry) {
+    //This buffers jsts points so that those features will have an actual area to be collected.
+    // The buffer size is relative to the unit of measurement for the projection.
+    // In order to get meters and circles, 3857 should be used.
+    const bufferSize = 1000;
+    if (!(jstsGeometry.getGeometryType() === "Polygon" || jstsGeometry.getGeometryType() === "MultiPolygon" )) {
+        var temp_geom = transformJSTSGeometry(jstsGeometry, 'EPSG:4326', 'EPSG:3857')
+        return transformJSTSGeometry(temp_geom.buffer(bufferSize), 'EPSG:3857', 'EPSG:4326')
+    }
+    return jstsGeometry;
+}
+
+/**
+ * Converts a GeoJSON to a JSTS Polygon/MultiPolygon geometry
+ * @param {jstsGeometry} A JSTS geometry.
+ * @param {from_srs} An EPSG code as a string, example: "EPSG:4326".
+ * @param {to_srs} An EPSG code as a string, example: "EPSG:3857".
+ * @return {jstsGeometry} A JSTS geometry.
+ */
+export function transformJSTSGeometry(jstsGeometry, from_srs, to_srs) {
+    //This all seems excessive, however jsts ol3Parser wasn't working with versions
+    // "jsts": "~1.4.0" and "openlayers": "~3.19.1", worth revisting in the future.
+    const writer = new jsts.io.GeoJSONWriter();
+    const geojsonReader = new jsts.io.GeoJSONReader();
+    const ol3GeoJSON = new ol.format.GeoJSON();
+    const geom = (new ol.format.GeoJSON()).readGeometry(writer.write(jstsGeometry)).transform(from_srs, to_srs);
+    return geojsonReader.read(ol3GeoJSON.writeGeometry(geom));
+}
+
+/**
+ * Converts a GeoJSON to a JSTS Polygon/MultiPolygon geometry
+ * @param {geojson} A geojson object.
+ * @return {geometry} A JSTS Polygon or MultiPolygon
+ */
+export function convertGeoJSONtoJSTS(geojson) {
+    const geojsonReader = new jsts.io.GeoJSONReader();
+
+    var jstsGeoJSON = geojsonReader.read(geojson);
+    if (jstsGeoJSON.features) {
+        var features = jstsGeoJSON.features;
+        var geometry = bufferGeometry(features[0].geometry);
+        for (var i = 1; i < features.length; i++) {
+            geometry = geometry.union(bufferGeometry(features[i].geometry));
+        }
+    } else if (jstsGeoJSON.geometries) {
+        var geometries = jstsGeoJSON.geometries;
+        var geometry = bufferGeometry(geometries[0]);
+        for (var i = 1; i < geometries.length; i++) {
+            geometry = geometry.union(bufferGeometry(geometries[i]));
+        }
+    } else {
+        var geometry = bufferGeometry(jstsGeoJSON);
     }
     return geometry;
 }
 
-export function processGeoJSONFile(file) {
-    return (dispatch) => {
-        dispatch({type: types.FILE_PROCESSING});
-        const fileName = file.name;
-        const ext = fileName.split('.').pop();
-        if(ext != 'geojson') {
-            dispatch({type: types.FILE_ERROR, error: 'File must be .geojson NOT .' + ext});
-            return;
-        }
-        const reader = new FileReader();
-        reader.onload = () => {
-            const dataURL = reader.result;
-            let geojson = null;
-            try {
-                geojson = JSON.parse(dataURL);
-            }
-            catch (e) {
-                dispatch({type: types.FILE_ERROR, error: 'Could not parse GeoJSON'});
-                return;
-            }
-            try {
-                const geojsonReader = new jsts.io.GeoJSONReader();
-
-                var features = geojsonReader.read(geojson).features;
-
-                //Because the UI doesn't support multiple features combine all polygons into one feature.
-                var multipolygon = bufferGeometry(features[0].geometry);
-                for (var i = 1; i < features.length; i++) {
-                    multipolygon = multipolygon.union(bufferGeometry(features[i].geometry));
-                }
-
-                var writer = new jsts.io.GeoJSONWriter();
-
-                const geom = (new ol.format.GeoJSON()).readGeometry(writer.write(multipolygon)).transform('EPSG:4326', 'EPSG:3857');
-                if(geom.getType() == 'Polygon' || geom.getType() == 'MultiPolygon') {
-                    dispatch({type: types.FILE_PROCESSED, geom: geom});
-                }
-                else {
-                    dispatch({type: types.FILE_ERROR, error: 'Geometry must be Polygon or MultiPolygon type, not ' + geom.getType()})
-                }
-            }
-            catch(err){
-                dispatch({type: types.FILE_ERROR, error: 'There was an error processing the geojson file.'});
-                console.log(err)
-                return;
-            }
-
-        }
-        reader.onerror = () => {
-            dispatch({type: types.FILE_ERROR, error: 'An error was encountered while reading your file'});
-        }
-        reader.readAsText(file);
+export const processGeoJSONFile = file => dispatch => {
+    dispatch({type: types.FILE_PROCESSING});
+    const fileName = file.name;
+    const ext = fileName.split('.').pop();
+    if (ext != 'geojson') {
+        const error_msg = 'File must be .geojson NOT .' + ext;
+        dispatch({type: types.FILE_ERROR, error: error_msg});
+        return Promise.reject(new Error(error_msg)).then(function (error) {
+        }, function (error) {
+            console.log(error);
+        });
     }
+    const reader = new FileReader();
+    reader.onload = () => {
+        const dataURL = reader.result;
+        let geojson = null;
+        try {
+            geojson = JSON.parse(dataURL);
+        }
+        catch (e) {
+            dispatch({type: types.FILE_ERROR, error: 'Could not parse GeoJSON'});
+            return Promise.reject(new Error(e)).then(function (error) {
+            }, function (error) {
+                console.log(error);
+            });
+        }
+        try {
+
+            //Because the UI doesn't support multiple features combine all polygons into one feature.
+            var multipolygon = convertGeoJSONtoJSTS(geojson);
+
+            const writer = new jsts.io.GeoJSONWriter();
+            const geom = (new ol.format.GeoJSON()).readGeometry(writer.write(multipolygon)).transform('EPSG:4326', 'EPSG:3857');
+            if (geom.getType() == 'Polygon' || geom.getType() == 'MultiPolygon') {
+                dispatch({type: types.FILE_PROCESSED, geom: geom});
+            }
+        }
+        catch (err) {
+            dispatch({type: types.FILE_ERROR, error: 'There was an error processing the geojson file.'});
+            return Promise.reject(new Error(err)).then(function (error) {
+            }, function (error) {
+                console.log(error);
+            });
+        }
+
+    }
+    reader.onerror = () => {
+        dispatch({type: types.FILE_ERROR, error: 'An error was encountered while reading your file'});
+    }
+    reader.readAsText(file);
+    return Promise.resolve();
 }
+
--- eventkit_cloud/ui/static/ui/app/tests/mapToolActions.spec.js
+++ eventkit_cloud/ui/static/ui/app/tests/mapToolActions.spec.js
@@ -1,8 +1,9 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import * as actions from '../actions/mapToolActions';
-import nock from 'nock';
-import sinon from 'sinon';
+import ol from 'openlayers';
+import * as jsts from 'jsts';
+import types from '../actions/mapToolActionTypes'
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
@@ -56,5 +57,258 @@ describe('mapTool actions', () => {
         expect(actions.resetGeoJSONFile()).toEqual({
             type: 'FILE_RESET'
         });
+    });
+
+    it('convertJSTSGeometry should covert a JSTS from one SRS to another', () => {
+
+        const writer = new jsts.io.GeoJSONWriter();
+        const geojsonReader = new jsts.io.GeoJSONReader();
+        const ol3GeoJSON = new ol.format.GeoJSON();
+        var reader = new jsts.io.WKTReader();
+        var jstsGeom = reader.read('POINT (-20 0)');
+        expect(jstsGeom.getCoordinate()).toEqual({x: -20, y: 0, z: undefined});
+        var newGeom = actions.transformJSTSGeometry(jstsGeom, 'EPSG:4326', 'EPSG:3857')
+        expect(newGeom.getCoordinate()).toEqual({
+            "x": -2226389.8158654715,
+            "y": -7.081154551613622e-10,
+            "z": undefined
+        });
+        var newerGeom = actions.transformJSTSGeometry(newGeom, 'EPSG:3857', 'EPSG:4326')
+        expect(newerGeom.getCoordinate()).toEqual({x: -20, y: 0, z: undefined});
+        expect(newerGeom).toEqual(jstsGeom);
+    });
+
+    it('bufferGeometry should covert a point to a polygon', () => {
+        const reader = new jsts.io.WKTReader();
+        var jstsGeom = reader.read('POINT (-20 0)');
+        expect(jstsGeom.getGeometryType()).toEqual("Point");
+        var returnedGeom = actions.bufferGeometry(jstsGeom)
+        expect(returnedGeom.getGeometryType()).toEqual("Polygon");
+    });
+
+    it('bufferGeometry should not change a polygon', () => {
+        const reader = new jsts.io.WKTReader();
+        var jstsGeom = reader.read('POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))');
+        expect(jstsGeom.getGeometryType()).toEqual("Polygon");
+        var returnedGeom = actions.bufferGeometry(jstsGeom)
+        expect(returnedGeom.getGeometryType()).toEqual("Polygon");
+        expect(jstsGeom).toEqual(returnedGeom);
+    });
+
+    it('processGeoJSONFile should create error if extension is not geojson ', () => {
+        // Initialize mockstore with empty state
+        const initialState = {}
+        const store = mockStore(initialState)
+        const file = new File(["<!doctype html><div>file</div>"],
+            "test.shp",
+            {"type": "text/html"}
+        )
+        // Test if your store dispatched the expected actions
+        const expectedPayload = [{type: types.FILE_PROCESSING},
+            {type: types.FILE_ERROR, error: 'File must be .geojson NOT .shp'}]
+        store.dispatch(actions.processGeoJSONFile(file))
+            .catch(() => {
+                expect(store.getActions()).toEqual(expectedPayload)
+            })
+    });
+
+    it('processGeoJSONFile should raise error if not a json', () => {
+        // Initialize mockstore with empty state
+        const initialState = {}
+        const store = mockStore(initialState)
+        const file = new File(["Data"],
+            "test.geojson",
+            {"type": "application/json"}
+        )
+
+        // Test if your store dispatched the expected actions
+        const expectedPayload = [{type: types.FILE_PROCESSING},
+            {type: types.FILE_ERROR, error: 'Could not parse GeoJSON'}]
+        store.dispatch(actions.processGeoJSONFile(file))
+            .catch(() => {
+                expect(store.getActions()).toEqual(expectedPayload)
+            })
+    });
+
+    it('convertGeoJSONtoJSTS should convert GeoJSON Point to JSTS Geometry', () => {
+        const point = {"type": "Point", "coordinates": [100.0, 0.0]};
+        const returnedGeom = actions.convertGeoJSONtoJSTS(point)
+        expect(returnedGeom.getGeometryType()).toEqual("Polygon");
+    });
+
+    it('convertGeoJSONtoJSTS should convert GeoJSON LineString to JSTS Geometry', () => {
+        const lineString = {
+            "type": "LineString",
+            "coordinates": [[100.0, 0.0], [101.0, 1.0]]
+        }
+        const returnedGeom = actions.convertGeoJSONtoJSTS(lineString)
+        expect(returnedGeom.getGeometryType()).toEqual("Polygon");
+    });
+
+    it('convertGeoJSONtoJSTS should convert GeoJSON Polygon(no holes) to JSTS Geometry', () => {
+        const polygon = {
+            "type": "Polygon",
+            "coordinates": [
+                [[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]]
+            ]
+        }
+        const returnedGeom = actions.convertGeoJSONtoJSTS(polygon)
+        expect(returnedGeom.getGeometryType()).toEqual("Polygon");
+    });
+
+    it('convertGeoJSONtoJSTS should convert GeoJSON Polygon(holes) to JSTS Geometry', () => {
+        const polygon = {
+            "type": "Polygon",
+            "coordinates": [
+                [[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]],
+                [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]]
+            ]
+        }
+        const returnedGeom = actions.convertGeoJSONtoJSTS(polygon)
+        expect(returnedGeom.getGeometryType()).toEqual("Polygon");
+    });
+
+    it('convertGeoJSONtoJSTS should convert GeoJSON MultiPoint to JSTS Geometry', () => {
+        const multiPoint = {
+            "type": "MultiPoint",
+            "coordinates": [[100.0, 0.0], [101.0, 1.0]]
+        }
+        const returnedGeom = actions.convertGeoJSONtoJSTS(multiPoint)
+        expect(returnedGeom.getGeometryType()).toEqual("MultiPolygon");
+    });
+
+    it('convertGeoJSONtoJSTS should convert GeoJSON MultiLineString to JSTS Geometry', () => {
+        const multiLineString = {
+            "type": "MultiLineString",
+            "coordinates": [
+                [[100.0, 0.0], [101.0, 1.0]],
+                [[102.0, 2.0], [103.0, 3.0]]
+            ]
+        }
+        const returnedGeom = actions.convertGeoJSONtoJSTS(multiLineString)
+        expect(returnedGeom.getGeometryType()).toEqual("MultiPolygon");
+    });
+
+    it('convertGeoJSONtoJSTS should convert GeoJSON MultiPolygon to JSTS Geometry', () => {
+        const multiPolygon = {
+            "type": "MultiPolygon",
+            "coordinates": [
+                [[[102.0, 2.0], [103.0, 2.0], [103.0, 3.0], [102.0, 3.0], [102.0, 2.0]]],
+                [[[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]],
+                    [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]]]
+            ]
+        }
+        const returnedGeom = actions.convertGeoJSONtoJSTS(multiPolygon)
+        expect(returnedGeom.getGeometryType()).toEqual("MultiPolygon");
+    });
+
+    it('convertGeoJSONtoJSTS should convert GeoJSON GeometryCollection to JSTS Geometry', () => {
+        const geometryCollection = {
+            "type": "GeometryCollection",
+            "geometries": [
+                {
+                    "type": "Point",
+                    "coordinates": [100.0, 0.0]
+                },
+                {
+                    "type": "LineString",
+                    "coordinates": [[101.0, 0.0], [102.0, 1.0]]
+                }
+            ]
+        }
+        const returnedGeom = actions.convertGeoJSONtoJSTS(geometryCollection)
+        expect(returnedGeom.getGeometryType()).toEqual("MultiPolygon");
+    });
+
+    it('convertGeoJSONtoJSTS should convert GeoJSON FeatureCollection to JSTS Geometry', () => {
+        const featureCollection = {
+            "type": "FeatureCollection",
+            "features": [{
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [102.0, 0.5]
+                },
+                "properties": {
+                    "prop0": "value0"
+                }
+            }, {
+                "type": "Feature",
+                "geometry": {
+                    "type": "LineString",
+                    "coordinates": [
+                        [102.0, 0.0],
+                        [103.0, 1.0],
+                        [104.0, 0.0],
+                        [105.0, 1.0]
+                    ]
+                },
+                "properties": {
+                    "prop0": "value0",
+                    "prop1": 0.0
+                }
+            }, {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [
+                        [
+                            [100.0, 0.0],
+                            [101.0, 0.0],
+                            [101.0, 1.0],
+                            [100.0, 1.0],
+                            [100.0, 0.0]
+                        ]
+                    ]
+                },
+                "properties": {
+                    "prop0": "value0",
+                    "prop1": {
+                        "this": "that"
+                    }
+                }
+            }]
+        }
+        const returnedGeom = actions.convertGeoJSONtoJSTS(featureCollection)
+        expect(returnedGeom.getGeometryType()).toEqual("MultiPolygon");
+    });
+
+    it('processGeoJSONFile should add AOI to state if valid geojson', () => {
+        // Initialize mockstore with empty state
+        const initialState = {}
+        const store = mockStore(initialState)
+        const point = {"type": "Point", "coordinates": [100.0, 0.0]};
+        const returnedGeom = actions.convertGeoJSONtoJSTS(point)
+        const file = new File([point],
+            "test.geojson",
+            {"type": "application/json"}
+        )
+
+        // Test if your store dispatched the expected actions
+        const expectedPayload = [{type: types.FILE_PROCESSING},
+            {type: types.FILE_PROCESSED, geom: returnedGeom}]
+        store.dispatch(actions.processGeoJSONFile(file))
+            .catch(() => {
+                expect(store.getActions()).toEqual(expectedPayload)
+            })
+    });
+
+    it('processGeoJSONFile should not add AOI to state if invalid geojson', () => {
+        // Initialize mockstore with empty state
+        const initialState = {}
+        const store = mockStore(initialState)
+        const point = {"type": "point", "coordinates": [100.0, 0.0]};
+        const file = new File([point],
+            "test.geojson",
+            {"type": "application/json"}
+        )
+
+        // Test if your store dispatched the expected actions
+        const expectedPayload = [{type: types.FILE_PROCESSING},
+            {type: types.FILE_ERROR, error: 'There was an error processing the geojson file.'}]
+        store.dispatch(actions.processGeoJSONFile(file))
+            .catch(() => {
+                expect(store.getActions()).toEqual(expectedPayload)
+            })
     });
 });
