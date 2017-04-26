64540ecf808e7eb5d93356786920659ac9dfcaef
lukerees <luke.rees@rgi-corp.com>
2017-04-26 12:37:57 -0400

    Update css
    Breakpoints have been updated to use bootstrap values. Media queries
    were moved to the bottom of file where approriate so that they will be
    the last applied to element. Made its so two conditional items are
    compelely hidden from page. Updated tests.

--- eventkit_cloud/ui/static/ui/app/components/InvalidDrawWarning.js
+++ eventkit_cloud/ui/static/ui/app/components/InvalidDrawWarning.js
@@ -1,32 +1,15 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import styles from '../styles/InvalidDrawWarning.css';
-import {showInvalidDrawWarning, hideInvalidDrawWarning} from '../actions/drawToolBarActions.js';
 
 export class InvalidDrawWarning extends Component {
-
     constructor(props) {
         super(props);
-        this.state = {
-            visibilityClass: styles.hidden,
-        }
-    }
-
-    componentWillReceiveProps(nextProps){
-        if(nextProps.show != this.props.show) {
-            if(nextProps.show) {
-                this.setState({visibilityClass: styles.visible});
-            }
-            else {
-                this.setState({visibilityClass: styles.hidden});
-            }
-        }
     }
 
     render() {
-
         return (
-            <div className={styles.invalidWarning + " " + this.state.visibilityClass}>
+            <div className={styles.invalidWarning} style={{display: this.props.show ? 'initial' : 'none'}}>
                 <span>You drew an invalid bounding box, please redraw.</span>
             </div>
         )
@@ -39,18 +22,11 @@ function mapStateToProps(state) {
     };
 }
 
-function mapDispatchToProps(dispatch) {
-    return {
-        hideInvalidDrawWarning: () => {
-            dispatch(hideInvalidDrawWarning());
-        },
-        showInvalidDrawWarning: () => {
-            dispatch(showInvalidDrawWarning());
-        },
-    }
-}
+InvalidDrawWarning.propTypes = {
+    show: React.PropTypes.bool.isRequired,
+};
 
 export default connect(
     mapStateToProps,
-    mapDispatchToProps
+    null
 )(InvalidDrawWarning);
