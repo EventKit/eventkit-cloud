896f31b4c38d5dc11387c1e29bc37e9a1976716d
Joseph Svrcek <joseph.svrcek@rgi-corp.com>
2017-05-12 14:20:25 -0400

    fixed webpack dependencies and build options

--- eventkit_cloud/ui/static/ui/app/actions/userActions.js
+++ eventkit_cloud/ui/static/ui/app/actions/userActions.js
@@ -1,5 +1,5 @@
 import actions from './actionTypes'
-import { push } from 'react-router-redux'
+import {push} from 'react-router-redux'
 import axios from 'axios'
 import cookie from 'react-cookie'
 
@@ -27,7 +27,7 @@ export const login = data => (dispatch) => {
 
     const form_data = new FormData();
     var method = 'get';
-    if(data && (data.username && data.password)) {
+    if (data && (data.username && data.password)) {
         form_data.append('username', data.username);
         form_data.append('password', data.password);
         method = 'post';
@@ -39,11 +39,15 @@ export const login = data => (dispatch) => {
         data: form_data,
         headers: {"X-CSRFToken": csrftoken}
     }).then((response) => {
-        dispatch({
-            type: actions.USER_LOGGED_IN,
-            payload: response.data
-        });
-    }).catch((error) => {
+            if (response.data) {
+                dispatch({
+                    type: actions.USER_LOGGED_IN,
+                    payload: response.data
+                });
+            } else {
+                dispatch(logout());
+            }
+        }).catch((error) => {
         dispatch(logout());
     });
 }
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ var devtool = 'source-map';
 var plugins = [
     new WriteFilePlugin(),
     new ExtractTextPlugin('styles.css'),
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js', Infinity),
+    new webpack.optimize.CommonsChunkPlugin({name:'vendor', filename: 'vendor.bundle.js', minChunks: 'Infinity'}),
 ];
 var app = [APP_DIR + '/index.js'];
 
@@ -28,69 +28,51 @@ var config = {
         publicPath: '/static/ui/build/'
     },
     resolve: {
-        extensions: ['', '.js', '.jsx']
+        extensions: ['.js', '.jsx']
     },
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
                 exclude: [/node_modules/, /staticfiles/],
-                loader: ['babel-loader'],
-                query: {
-                    presets: ["es2015", "react", "stage-0"]
-                }
+                loader: ['babel-loader?presets[]=es2015,presets[]=react,presets[]=stage-0'],
             },
-            // {
-            //     test: /\.(png|jpg|gif)$/,
-            //     loader: 'file'
-            // },
             {
                 test: /\.css$/,
                 loader: 'style-loader'
             }, {
                 test: /\.css$/,
-                loader: 'css-loader',
-                query: {
-                    modules: true,
-                    localIdentName: '[name]__[local]___[hash:base64:5]'
-                }
+                loader: 'css-loader?modules=true,localIdentName=[name]__[local]___[hash:base64:5]',
             },
             {
                 test: /\.(woff2?|ttf|eot|svg|png|jpg|gif)$/,
-                loader: 'url-loader?limit=100000',
-                options: {
-                    name: 'fonts/[hash].[ext]',
-                }
+                loader: 'url-loader?limit=100000,name=fonts/[hash].[ext]',
             },
-            // {
-            //     test: /\.(png|jpg|gif)$/,
-            //     loader: "file-loader"
-            // }
         ],
     },
     plugins: plugins,
     devServer: {
         hot: true,
         contentBase: BASE_DIR,
-        outputPath: BUILD_DIR,
+        watchContentBase: true,
+        publicPath: BUILD_DIR,
         host: "0.0.0.0",
         port: 8080,
         historyApiFallback: true,
         disableHostCheck: true,
+        watchOptions: {
+            poll: true
+        },
+        inline: true
     },
-    watchOptions: {
-        poll: true
-    },
+
 };
 
 if (PROD) {
-    config.plugins.push(new webpack.optimize.AggressiveMergingPlugin());
-    config.plugins.push(new webpack.DefinePlugin({'process.env': {'NODE_ENV': JSON.stringify('production')}}));
+    // config.plugins.push(new webpack.DefinePlugin({'process.env': {'NODE_ENV': JSON.stringify('production')}}));
     config.plugins.push(new webpack.optimize.UglifyJsPlugin({compressor: {warnings: false}}));
-}
-else {
-    config.entry.app.push('webpack-dev-server/client?http://0.0.0.0:8080') // WebpackDevServer host and port
-    config.entry.app.push('webpack/hot/dev-server')// "only" prevents reload on syntax errors)
+} else {
+    config.entry.app.push('webpack-dev-server/client')
     config.plugins.push(new webpack.HotModuleReplacementPlugin());
     config.devtool = 'inline-source-map';
 }
