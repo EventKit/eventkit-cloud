b9ba88fa1b286cc2ae8953dbf7e63f53fd557fdd
Rees <luke.rees@rgi-corp.com>
2017-05-23 11:39:24 -0400

    re-add exports to urls

--- eventkit_cloud/ui/urls.py
+++ eventkit_cloud/ui/urls.py
@@ -10,6 +10,7 @@ from django.conf import settings
 urlpatterns = [
     url(r'^login', ensure_csrf_cookie(TemplateView.as_view(template_name='ui/index.html')), name='login'),
     url(r'^$', login_required(TemplateView.as_view(template_name='ui/index.html')), name="home"),
+    url(r'^exports$', login_required(TemplateView.as_view(template_name='ui/index.html')), name="exports"),
     url(r'^status', login_required(TemplateView.as_view(template_name='ui/index.html')), name="status"),
     url(r'^create$', login_required(TemplateView.as_view(template_name='ui/index.html')), name="create"),
     url(r'^account', login_required(TemplateView.as_view(template_name='ui/index.html')), name="account"),
