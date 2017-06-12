32c70c3700e2f9b59c704a7c96e8bc7f2944f32e
jduncan <jduncan@radiantblue.com>
2017-06-12 12:08:40 -0400

    updated the PR to take out the provider level file size if it is 0.000.  Also have the decimals fixed to a length of 3.

--- eventkit_cloud/ui/static/ui/app/components/StatusDownloadPage/ProviderRow.js
+++ eventkit_cloud/ui/static/ui/app/components/StatusDownloadPage/ProviderRow.js
@@ -48,9 +48,11 @@ export class ProviderRow extends React.Component {
         nextProps.provider.tasks.forEach((task) => {
             if (task.result != null){
                 let textReplace = task.result.size.replace(' MB','');
-                let number = textReplace
+                let number = textReplace;
                 fileSize = Number(fileSize) + Number(number);
-                this.setState({fileSize})
+                if (fileSize > 0.000){
+                    this.setState({fileSize: fileSize.toFixed(3)});
+                }
             }
         })
     }
@@ -222,7 +224,7 @@ export class ProviderRow extends React.Component {
                             {this.props.provider.name}
                         </TableHeaderColumn>
                         <TableHeaderColumn style={{width:'128px',textAlign: 'center', color: 'black!important', fontSize: textFontSize}}>
-                            {this.state.fileSize + " MB"}
+                            {this.state.fileSize == 0 ? '' : this.state.fileSize + " MB"}
                         </TableHeaderColumn>
                         <TableHeaderColumn style={{width:'120px',textAlign: 'center', color: 'black!important', fontSize: textFontSize}}/>
                         <TableHeaderColumn style={{width: '124px',textAlign: 'right'}}>
