3af3248a30911970ee046fff251dc9041ba3cf38
Luke <luke.rees@rgi-corp.com>
2017-06-14 10:16:09 -0400

    Add placeholder screenshots

--- eventkit_cloud/ui/static/ui/app/components/About/About.js
+++ eventkit_cloud/ui/static/ui/app/components/About/About.js
@@ -2,8 +2,27 @@ import React, {Component} from 'react';
 import AppBar from 'material-ui/AppBar';
 import CustomScrollbar from '../CustomScrollbar';
 import QuickTourSection from './QuickTourSection';
-import test from '../../../images/about_screen_shot.png';
-import test2 from '../../../images/about_screen_shot2.png';
+import create_img from '../../../images/3_stage_create.png';
+import manage_img from '../../../images/3_stage_manage.png';
+import use_img from '../../../images/3_stage_use.png';
+
+import manage_1 from '../../../images/manage_slide_1.png';
+import manage_2 from '../../../images/manage_slide_2.png';
+import manage_3 from '../../../images/manage_slide_3.png';
+
+import create_1_1 from '../../../images/create_slide_1.png';
+import create_1_2 from '../../../images/create_slide_2.png';
+import create_1_3 from '../../../images/create_slide_3.png';
+
+import create_2_1 from '../../../images/create_step_2_slide_1.png';
+import create_2_2 from '../../../images/create_step_2_slide_2.png';
+import create_2_3 from '../../../images/create_step_2_slide_3.png';
+
+import create_3_1 from '../../../images/create_step_3_slide_1.png';
+import create_3_2 from '../../../images/create_step_3_slide_2.png';
+
+import create_4_1 from '../../../images/create_status_download_slide_1.png';
+import create_4_2 from '../../../images/create_status_download_slide_2.png';
 
 export class About extends Component {
 
@@ -45,10 +64,17 @@ export class About extends Component {
                 overflowY: 'hidden',
             },
             bodyContent: {
-                padding: '30px 34px',
+                padding: '30px 34px 60px',
                 maxWidth: '1000px', 
-                margin: 'auto'
+                margin: 'auto',
             },
+            threeStepCaption: {
+                backgroundColor: '#4598bf', 
+                padding: '5px', 
+                color: '#fff', 
+                minHeight: '50px', 
+                width: '95%',
+            }
             
         };
 
@@ -66,56 +92,88 @@ export class About extends Component {
                     <CustomScrollbar style={{height: window.innerHeight - 130, width: '100%'}}>
                         <div style={styles.bodyContent}>
                             <h4><strong>Overview</strong></h4>
-                            <div style={{marginBottom: '30px'}}>
+                            <div>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum varius, mauris id accumsan lobortis, augue ex elementum ante, 
                                 sit amet luctus odio felis sit amet ex. Nunc congue porttitor libero eu semper. Nunc mauris ipsum, laoreet sit amet ullamcorper at, 
                                 feugiat sit amet nisi. Nulla a leo nulla. Ut lacinia nulla quis luctus bibendum. Cras pellentesque dolor risus, 
                                 non scelerisque nulla tempus eu. Aliquam consectetur pellentesque nibh. Aenean lobortis aliquet lacus, 
                                 at convallis urna tempor scelerisque. Nullam eleifend vulputate felis sit amet feugiat. Donec at dignissim purus, sed tempus augue.
                             </div>
-                            <h4><strong>What is a DataPack?</strong></h4>
-                            <div style={{marginBottom: '30px'}}>
+                            <h4 style={{marginTop: '30px'}}><strong>What is a DataPack?</strong></h4>
+                            <div>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum varius, mauris id accumsan lobortis, augue ex elementum ante, 
                                 sit amet luctus odio felis sit amet ex. Nunc congue porttitor libero eu semper. Nunc mauris ipsum, laoreet sit amet ullamcorper at, 
                                 feugiat sit amet nisi. Nulla a leo nulla. Ut lacinia nulla quis luctus bibendum. Cras pellentesque dolor risus, 
                                 non scelerisque nulla tempus eu. Aliquam consectetur pellentesque nibh. Aenean lobortis aliquet lacus, 
                                 at convallis urna tempor scelerisque. Nullam eleifend vulputate felis sit amet feugiat. Donec at dignissim purus, sed tempus augue.
                             </div>
-                            <table style={{borderCollapse: 'collapse', marginBottom: '30px'}}>
+                            <table style={{borderCollapse: 'collapse', marginTop: '30px'}}>
                                 <tbody>
                                     <tr>
                                         <td style={{verticalAlign: 'top'}}>
                                             <img 
-                                                src={test2}
+                                                src={create_img}
                                                 style={{display: 'block', width: '95%', marginRight: '5%'}}
                                             />
-                                            <div style={{backgroundColor: '#4598bf', padding: '5px', color: '#fff', minHeight: '50px', width: '95%', marginRight: '5%'}}>Create DataPacks</div>
+                                            <div style={{...styles.threeStepCaption, marginRight: '5%'}}>Create DataPacks</div>
                                         </td>
                                         <td style={{verticalAlign: 'top', margin: '0px 5px'}}>
                                             <img 
-                                                src={test2}
+                                                src={manage_img}
                                                 style={{display: 'block', width: '95%', margin: 'auto'}}
                                             />
-                                            <div style={{backgroundColor: '#4598bf', padding: '5px', color: '#fff', minHeight: '50px', width: '95%', margin: 'auto'}}>Manage DataPacks</div>
+                                            <div style={{...styles.threeStepCaption, margin: 'auto'}}>Manage DataPacks</div>
                                         </td>
                                         <td style={{verticalAlign: 'top'}}>
                                             <img 
-                                                src={test2}
+                                                src={use_img}
                                                 style={{display: 'block', width: '95%', marginLeft: '5%'}}
                                             />
-                                            <div style={{backgroundColor: '#4598bf', padding: '5px', color: '#fff', minHeight: '50px', width: '95%', marginLeft: '5%'}}>Use with other open source geospatial software like QGIS</div>
+                                            <div style={{...styles.threeStepCaption, marginLeft: '5%'}}>Use with other open source geospatial software like QGIS</div>
                                         </td>
                                     </tr>
                                 </tbody>
                             </table>
-                            <h3 style={{margin: '30px 0px'}}>Quick Tour</h3>
-                            <h4>MANAGE DATAPACKS</h4>
+                            <h3 style={{marginTop: '60px'}}><strong>Quick Tour</strong></h3>
+                            <h4 style={{marginTop: '30px'}}><strong>MANAGE DATAPACKS</strong></h4>
                             <QuickTourSection 
                                 steps={[
-                                    {img: test, caption: 'Here you can create or search, sort, and filter all private an public datapacks'}, 
-                                    {img: test, caption: 'Check the status of previously created datapacks'}
+                                    {img: manage_1, caption: 'Here you can create or search, sort, and filter all private an public DataPacks.'}, 
+                                    {img: manage_2, caption: 'Check the status of previously created DataPacks.'},
+                                    {img: manage_3, caption: 'Make other actions like "Go to Export Detail" to check export statuses and make downloads.'}
                                 ]}
                                 sectionTitle={'DataPack Library'}
+                            />
+                            <h4 style={{marginTop: '60px'}}><strong>CREATE DATAPACKS</strong></h4>
+                            <QuickTourSection 
+                                steps={[
+                                    {img: create_1_1, caption: 'Use your tools to set your Area of Interest.'}, 
+                                    {img: create_1_2, caption: 'You can cancel or clear your selection using the "X"'},
+                                    {img: create_1_3, caption: 'Once your area of interest is set, move to the next step with the green right arrow button.'}
+                                ]}
+                                sectionTitle={'Step 1: Define Area of Interest'}
+                            />
+                            <QuickTourSection 
+                                steps={[
+                                    {img: create_2_1, caption: 'Enter general details and identifying information.'}, 
+                                    {img: create_2_2, caption: 'Choose your layers.'},
+                                    {img: create_2_3, caption: 'Use the right green arrow to review your DataPack.'}
+                                ]}
+                                sectionTitle={'Step 2: Enter General Information'}
+                            />
+                            <QuickTourSection 
+                                steps={[
+                                    {img: create_3_1, caption: 'Review your information to make sure it\'s correct.'}, 
+                                    {img: create_3_2, caption: 'Click the green check mark to submit your DataPack or use the back arrow to edit previous pages.'},
+                                ]}
+                                sectionTitle={'Step 3: Review & Submit'}
+                            />
+                            <QuickTourSection 
+                                steps={[
+                                    {img: create_4_1, caption: 'You\'ll get an e-mail when your files are ready. Use the table to download.'}, 
+                                    {img: create_4_2, caption: 'You can alse edit your DataPack expiration date an viewing permisions.'},
+                                ]}
+                                sectionTitle={'Get Your Files: Export Status & Download'}
                             />
                         </div>
                     </CustomScrollbar> 
--- eventkit_cloud/ui/static/ui/app/components/About/QuickTourSection.js
+++ eventkit_cloud/ui/static/ui/app/components/About/QuickTourSection.js
@@ -1,16 +1,24 @@
 import React, {Component} from 'react';
 import {Card, CardHeader, CardMedia} from 'material-ui/Card';
+import Swipeable from 'react-swipeable';
 import ChevronLeft from 'material-ui/svg-icons/navigation/chevron-left';
 import ChevronRight from 'material-ui/svg-icons/navigation/chevron-right';
+import NavigationArrowDropDown from 'material-ui/svg-icons/navigation/arrow-drop-down';
+import NavigationArrowDropUp from 'material-ui/svg-icons/navigation/arrow-drop-up';
 
 export class QuickTourSection extends Component {
 
     constructor(props) {
         super(props);
         this.nextStep = this.nextStep.bind(this);
+        this.previousStep = this.previousStep.bind(this);
         this.goToStep = this.goToStep.bind(this);
+        this.setArrowVisibility = this.setArrowVisibility.bind(this);
         this.state = {
-            step: 0
+            step: 0,
+            showLeftArrow: false,
+            showRightArrow: false,
+            arrowsVisible: false
         }
     };
 
@@ -23,14 +31,23 @@ export class QuickTourSection extends Component {
         }
     }
 
+    previousStep() {
+        if(this.state.step > 0) {
+            this.setState({step: this.state.step - 1});
+        }
+        else {
+            this.setState({step: this.props.steps.length - 1});
+        }
+    }
+
     goToStep(step) {
         if(step < this.props.steps.length && step >= 0) {
             this.setState({step: step});
         }
     }
 
-    mouseOver() {
-
+    setArrowVisibility(visible) {
+        this.setState({arrowsVisible: visible});
     }
 
     render() {
@@ -45,7 +62,8 @@ export class QuickTourSection extends Component {
                 display: 'inline-block', 
                 marginLeft: '10px', 
                 backgroundColor: '#fff', 
-                color: '#4598bf'
+                color: '#4598bf',
+                cursor: 'pointer',
             },
             nextImgArrow: {
                 width: '48px', 
@@ -61,31 +79,51 @@ export class QuickTourSection extends Component {
                 minWidth: 'none',
                 borderRadius: '50%',
                 color: '#4598bf', 
-                top: 'calc(50% - 32px)',
-                backgroundColor: 'rgba(69, 152, 191, 0.3)'
+                top: 'calc(50% - 24px)',
+                backgroundColor: 'rgba(69, 152, 191, 0.2)'
             }
         };
         const stepTotal = this.props.steps.length;
+        const arrowOpacity = this.state.arrowsVisible ? 1: 0;
 
         return (
-            <div>
-                <Card initiallyExpanded={true} style={{backgroundColor: 'whitesmoke'}}>
+            <div style={{margin: '10px 0px'}}>
+                <Card initiallyExpanded={true} style={{backgroundColor: '#dcdcdc'}}>
                     <CardHeader
                         showExpandableButton={true}
                         title={<strong>{this.props.sectionTitle}</strong>}
+                        openIcon={<NavigationArrowDropUp style={{fill: 'red'}}/>}
+                        closeIcon={<NavigationArrowDropDown style={{fill: 'green'}}/>}
                     />
                     <CardMedia expandable={true} style={{padding: '0px 10px 10px 10px'}}>
-                        <div>
-                            <img src={this.props.steps[this.state.step].img} style={{width: '100%'}}/>
-                            <div style={{...styles.nextImgDiv, left: '20px'}}>
+                        <div style={{width: '100%', height: '100%', position: 'relative'}}
+                            onMouseEnter={() => {this.setArrowVisibility(true)}}
+                            onTouchStart={() => {this.setArrowVisibility(true)}}
+                            onMouseLeave={() => {this.setArrowVisibility(false)}}
+                            onTouchMove={() => {this.setArrowVisibility(false)}}
+                            onClick={() => {this.setArrowVisibility(false)}}
+                        >
+                            <Swipeable
+                                onSwipedLeft={this.nextStep}
+                                onSwipedRight={this.previousStep}
+                            >
+                                <img src={this.props.steps[this.state.step].img} style={{width: '100%'}}/>
+                            </Swipeable>
+                            <div 
+                                style={{...styles.nextImgDiv, left: '20px', opacity: arrowOpacity}}
+                            >
                                 <ChevronLeft style={styles.nextImgArrow} onClick={this.nextStep}/>
                             </div>
-                            <div style={{...styles.nextImgDiv, right: '20px'}}>
+                            <div 
+                                style={{...styles.nextImgDiv, right: '20px', opacity: arrowOpacity}}
+                            >
                                 <ChevronRight style={styles.nextImgArrow} onClick={this.nextStep}/>
                             </div>
                         </div>
                         <div style={{width: '100%', backgroundColor: '#4598bf', color: '#fff', padding: '7px 10px'}}>
-                            <div style={{display: 'inline-block', width: `calc(100% - ${stepTotal * 30}px)`, lineHeight: '22px'}}>{this.props.steps[this.state.step].caption}</div>
+                            <div style={{display: 'inline-block', width: `calc(100% - ${stepTotal * 30}px)`, lineHeight: '22px'}}>
+                                {this.props.steps[this.state.step].caption}
+                            </div>
                             <div style={{display: 'inline-block', width: `${stepTotal * 30}px`, verticalAlign: 'top'}}>
                                 {this.props.steps.map((item, ix) => {
                                     const style = {
