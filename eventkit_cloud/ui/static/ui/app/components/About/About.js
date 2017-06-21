ecdc9b6355386cb96e96c7452c46721ff6c22147
Rees <luke.rees@rgi-corp.com>
2017-06-21 12:44:19 -0400

    put AboutPage info in config

--- eventkit_cloud/ui/static/ui/app/components/About/About.js
+++ eventkit_cloud/ui/static/ui/app/components/About/About.js
@@ -5,89 +5,7 @@ import InfoParagraph from './InfoParagraph';
 import ThreeStepInfo from './ThreeStepInfo';
 import QuickTour from './QuickTour';
 import QuickTourSection from './QuickTourSection';
-import create_img from '../../../images/3_stage_create.png';
-import manage_img from '../../../images/3_stage_manage.png';
-import use_img from '../../../images/3_stage_use.png';
-
-import manage_1 from '../../../images/manage_slide_1.png';
-import manage_2 from '../../../images/manage_slide_2.png';
-import manage_3 from '../../../images/manage_slide_3.png';
-
-import create_1_1 from '../../../images/create_slide_1.png';
-import create_1_2 from '../../../images/create_slide_2.png';
-import create_1_3 from '../../../images/create_slide_3.png';
-
-import create_2_1 from '../../../images/create_step_2_slide_1.png';
-import create_2_2 from '../../../images/create_step_2_slide_2.png';
-import create_2_3 from '../../../images/create_step_2_slide_3.png';
-
-import create_3_1 from '../../../images/create_step_3_slide_1.png';
-import create_3_2 from '../../../images/create_step_3_slide_2.png';
-
-import create_4_1 from '../../../images/create_status_download_slide_1.png';
-import create_4_2 from '../../../images/create_status_download_slide_2.png';
-
-const pageInfo = {
-    textParagraphs: [
-        {header: 'Overview', body: 'EventKit is blah blah blah'},
-        {header: 'What is a DataPack?', body: 'DataPacks are blah blah blah'},
-    ],
-    threeStep: [
-        {img: create_img, caption: 'Create DataPacks'},
-        {img: manage_img, caption: 'Manage DataPacks'},
-        {img: use_img, caption: 'Use with other open source geospatial software like QGIS'}
-    ],
-    quickTour: [
-        {
-            header: 'MANAGE DATAPACKS',
-            tourSections: [
-                {
-                    sectionTitle: 'DataPack Library',
-                    steps: [
-                        {img: manage_1, caption: 'Here you can create or search, sort, and filter all private an public DataPacks.'}, 
-                        {img: manage_2, caption: 'Check the status of previously created DataPacks.'},
-                        {img: manage_3, caption: 'Make other actions like "Go to Export Detail" to check export statuses and make downloads.'}
-                    ]
-                }
-            ]
-        },
-        {
-            header: 'CREATE DATAPACKS',
-            tourSections: [
-                {
-                    sectionTitle: 'Step 1: Define Area of Interest',
-                    steps: [
-                        {img: create_1_1, caption: 'Use your tools to set your Area of Interest.'}, 
-                        {img: create_1_2, caption: 'You can cancel or clear your selection using the "X"'},
-                        {img: create_1_3, caption: 'Once your area of interest is set, move to the next step with the green right arrow button.'}
-                    ]
-                },
-                {
-                    sectionTitle: 'Step 2: Enter General Information',
-                    steps: [
-                        {img: create_2_1, caption: 'Enter general details and identifying information.'}, 
-                        {img: create_2_2, caption: 'Choose your layers.'},
-                        {img: create_2_3, caption: 'Use the right green arrow to review your DataPack.'}
-                    ]
-                },
-                {
-                    sectionTitle: 'Step 3: Review & Submit',
-                    steps: [
-                        {img: create_3_1, caption: 'Review your information to make sure it\'s correct.'}, 
-                        {img: create_3_2, caption: 'Click the green check mark to submit your DataPack or use the back arrow to edit previous pages.'},
-                    ]
-                },
-                {
-                    sectionTitle: 'Get Your Files: Export Status & Download',
-                    steps: [
-                        {img: create_4_1, caption: 'You\'ll get an e-mail when your files are ready. Use the table to download.'}, 
-                        {img: create_4_2, caption: 'You can alse edit your DataPack expiration date an viewing permisions.'},
-                    ]
-                }
-            ]
-        },
-    ]
-}
+import {Config} from '../../config.js';
 
 export class About extends Component {
 
@@ -101,8 +19,8 @@ export class About extends Component {
 
     componentDidMount() {
         window.addEventListener('resize', this.handleResize);
-        // later this will be api call --> set state
-        this.setState({pageInfo: pageInfo});
+        // later this could be api call --> set state
+        this.setState({pageInfo: Config.ABOUT_PAGE});
     };
 
     componentWillUnmount() {
--- eventkit_cloud/ui/static/ui/app/config.js
+++ eventkit_cloud/ui/static/ui/app/config.js
@@ -1,3 +1,20 @@
+import create_img from '../images/AboutPage/3_stage_create.png';
+import manage_img from '../images/AboutPage/3_stage_manage.png';
+import use_img from '../images/AboutPage/3_stage_use.png';
+import manage_1 from '../images/AboutPage/manage_slide_1.png';
+import manage_2 from '../images/AboutPage/manage_slide_2.png';
+import manage_3 from '../images/AboutPage/manage_slide_3.png';
+import create_1_1 from '../images/AboutPage/create_slide_1.png';
+import create_1_2 from '../images/AboutPage/create_slide_2.png';
+import create_1_3 from '../images/AboutPage/create_slide_3.png';
+import create_2_1 from '../images/AboutPage/create_step_2_slide_1.png';
+import create_2_2 from '../images/AboutPage/create_step_2_slide_2.png';
+import create_2_3 from '../images/AboutPage/create_step_2_slide_3.png';
+import create_3_1 from '../images/AboutPage/create_step_3_slide_1.png';
+import create_3_2 from '../images/AboutPage/create_step_3_slide_2.png';
+import create_4_1 from '../images/AboutPage/create_status_download_slide_1.png';
+import create_4_2 from '../images/AboutPage/create_status_download_slide_2.png';
+
 export const Config = {
     REGIONS_URL : '/api/regions.json',
     REGION_MASK_URL : '/api/maskregions.json',
@@ -14,8 +31,66 @@ export const Config = {
     GEONAMES_SEARCH_URL : 'http://api.geonames.org/searchJSON',
     CREATE_ERROR_URL : '/error',
     UPDATE_BROWSER_URL : '/update',
-    AUTH_URL : '/login'
+    AUTH_URL : '/login',
+    ABOUT_PAGE: {
+        textParagraphs: [
+            {header: 'Overview', body: 'EventKit is blah blah blah'},
+            {header: 'What is a DataPack?', body: 'DataPacks are blah blah blah'},
+        ],
+        threeStep: [
+            {img: create_img, caption: 'Create DataPacks'},
+            {img: manage_img, caption: 'Manage DataPacks'},
+            {img: use_img, caption: 'Use with other open source geospatial software like QGIS'}
+        ],
+        quickTour: [
+            {
+                header: 'MANAGE DATAPACKS',
+                tourSections: [
+                    {
+                        sectionTitle: 'DataPack Library',
+                        steps: [
+                            {img: manage_1, caption: 'Here you can create or search, sort, and filter all private an public DataPacks.'}, 
+                            {img: manage_2, caption: 'Check the status of previously created DataPacks.'},
+                            {img: manage_3, caption: 'Make other actions like "Go to Export Detail" to check export statuses and make downloads.'}
+                        ]
+                    }
+                ]
+            },
+            {
+                header: 'CREATE DATAPACKS',
+                tourSections: [
+                    {
+                        sectionTitle: 'Step 1: Define Area of Interest',
+                        steps: [
+                            {img: create_1_1, caption: 'Use your tools to set your Area of Interest.'}, 
+                            {img: create_1_2, caption: 'You can cancel or clear your selection using the "X"'},
+                            {img: create_1_3, caption: 'Once your area of interest is set, move to the next step with the green right arrow button.'}
+                        ]
+                    },
+                    {
+                        sectionTitle: 'Step 2: Enter General Information',
+                        steps: [
+                            {img: create_2_1, caption: 'Enter general details and identifying information.'}, 
+                            {img: create_2_2, caption: 'Choose your layers.'},
+                            {img: create_2_3, caption: 'Use the right green arrow to review your DataPack.'}
+                        ]
+                    },
+                    {
+                        sectionTitle: 'Step 3: Review & Submit',
+                        steps: [
+                            {img: create_3_1, caption: 'Review your information to make sure it\'s correct.'}, 
+                            {img: create_3_2, caption: 'Click the green check mark to submit your DataPack or use the back arrow to edit previous pages.'},
+                        ]
+                    },
+                    {
+                        sectionTitle: 'Get Your Files: Export Status & Download',
+                        steps: [
+                            {img: create_4_1, caption: 'You\'ll get an e-mail when your files are ready. Use the table to download.'}, 
+                            {img: create_4_2, caption: 'You can alse edit your DataPack expiration date an viewing permisions.'},
+                        ]
+                    }
+                ]
+            },
+        ]
+    }
 };
-
-
-
