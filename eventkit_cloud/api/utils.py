68fcecc1026622c2d23b36d22808b615dcd57b00
Gibran Parvez <gibran.parvez@rgi-corp.com>
2017-10-20 10:02:58 -0400

    Add handling for specific form field errors

--- eventkit_cloud/api/utils.py
+++ eventkit_cloud/api/utils.py
@@ -19,19 +19,36 @@ def eventkit_exception_handler(exc, context):
     }
     """
     # Call REST framework's default exception handler first,
-    # to get the standard error response.
+    # to get the standard error response. Parse the response accordingly.
     response = exception_handler(exc, context)
     error = response.data
     error_response = {'status': response.status_code,
-                      'title': error.get('title') or stringify(error.get('id')),
-                      'detail': error.get('detail') or stringify(error.get('message'))}
+                      'title': error.get('title') or stringify(error.get('id')) or stringify(error.get('description')),
+                      'detail': error.get('detail') or stringify(error.get('message')) or parseProviderTasks(error)}
+    # Parse title if not already defined.
+    # Define it as the title of the erroneous form field if such data is present.
     if not error_response.get('title'):
-        error_response['title'] = "Improperly formatted JSON"
-        error_response['detail'] = str(error)
+        if error.get('provider_tasks'):
+            error_response['title'] = error.get('provider_tasks')[0].keys()[0];
+        else:
+            error_response['title'] = "Improperly formatted JSON"
     response.data = {'errors':[error_response]}
-
     return response
 
+
+def parseProviderTasks(error):
+    """
+    Parse out provider tasks depending on what kind of object it is and isolate the message about a specific form field if necessary.
+    :param error:
+    :return:
+    """
+    if error.get('provider_tasks'):
+        if isinstance(error.get('provider_tasks')[0], dict):
+            return stringify(error.get('provider_tasks')[0].values()[0]);
+        else:
+            return error.get('provider_tasks')[0]
+    else:
+        return error.get('provider_tasks');
 
 def stringify(item):
     """
@@ -43,7 +60,7 @@ def stringify(item):
         logger.error("Exceptions should have a title and description per message not multiple.")
         logger.error("Exception: {0}".format(str(item)))
     if isinstance(item, dict):
-        return "{0}: {1}".format(item.iteritems().next())
+        return "{0}: {1}".get(item.iteritems().next())
     elif isinstance(item, list):
         return "{0}".format(item[0])
     elif isinstance(item, str):
