05b756a8455f2e79d0f4ba7577fd9e3232deda40
Lars Kendall <lars@prominentedge.com>
2017-11-01 19:13:29 -0700

    Screenshot comparisons in e2e tests. Launch url environment variable.

--- eventkit_cloud/ui/static/ui/app/tests/e2e/tests/eventkit/Login.js
+++ eventkit_cloud/ui/static/ui/app/tests/e2e/tests/eventkit/Login.js
@@ -8,10 +8,13 @@ module.exports = {
     "Login": function(browser) {
         bindHelpers(browser);
         bindComponents(browser);
+        screenshot.bindHelpers(browser);
 
         browser
             .url(`${browser.launchUrl}/login`, screenshot.width, screenshot.height, `Load page... "${browser.launchUrl}/login"`)
             .elementPresent(`.qa-LoginPage-Paper`, `CSS`, `Login page is present`)
+            // .saveScreenshot('screenshots/login.png')
+            // .matchesScreenshot('login', 0.95)
             .end();
     }
 };
