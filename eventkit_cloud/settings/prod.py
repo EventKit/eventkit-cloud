dbe3fc0db00dc968286368c7b8fc6321f609bb2f
Luke <luke.rees@rgi-corp.com>
2017-11-20 09:40:24 -0500

    env var comes in as a string, need to cast to int

--- eventkit_cloud/settings/prod.py
+++ eventkit_cloud/settings/prod.py
@@ -345,7 +345,8 @@ MAX_EXPORTRUN_EXPIRATION_DAYS = os.environ.get('MAX_EXPORTRUN_EXPIRATION_DAYS', 
 LAND_DATA_URL = os.environ.get('LAND_DATA_URL', "http://data.openstreetmapdata.com/land-polygons-split-3857.zip")
 
 AUTO_LOGOUT_COOKIE_NAME = 'eventkit_auto_logout'
-AUTO_LOGOUT_SECONDS = os.getenv('AUTO_LOGOUT_SECONDS', None)
-AUTO_LOGOUT_WARNING_AT_SECONDS_LEFT = os.getenv('AUTO_LOGOUT_WARNING_AT_SECONDS_LEFT', 5 * 60)
+
+AUTO_LOGOUT_SECONDS = int(os.getenv('AUTO_LOGOUT_SECONDS', 0))
+AUTO_LOGOUT_WARNING_AT_SECONDS_LEFT = int(os.getenv('AUTO_LOGOUT_WARNING_AT_SECONDS_LEFT', 5 * 60))
 if AUTO_LOGOUT_SECONDS:
     MIDDLEWARE += ['eventkit_cloud.auth.auth.auto_logout']
