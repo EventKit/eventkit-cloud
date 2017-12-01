deb27bbd01a8cb9a3056a19f9b367c8c26371770
David Sherman <david@david-sherman.com>
2017-12-01 14:47:40 -0500

    initial checkin. New tests for qgs styles

---
+++ eventkit_cloud/ui/tests/test_styles.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from django.test import TestCase
+
+from mock import Mock, patch, MagicMock
+from ..data_estimator import get_size_estimate, get_gb_estimate
+from ..helpers import generate_qgs_style
+from eventkit_cloud.tasks.models import (
+    ExportRun,
+    ExportTaskRecord,
+    FileProducingTaskResult,
+    DataProviderTaskRecord
+)
+
+from eventkit_cloud.tasks import TaskStates
+
+logger = logging.getLogger(__name__)
+
+# @patch 'modules.class.creates_files`
+# def test_create_file(self, mock_creates_files):
+#    some_parent_function()
+#    mock_creates_files.assert_called_once_with(params)
+
+
+class TestStyles(TestCase):
+
+    def test_get_gb_estimate(self):
+        expected_return_value = 0.0001572864
+        actual_return_value = get_gb_estimate(4)
+        self.assertAlmostEqual(expected_return_value, actual_return_value, places=9)
+
+    @patch('eventkit_cloud.ui.data_estimator.get_gb_estimate')
+    @patch('eventkit_cloud.ui.data_estimator.DataProvider')
+    def test_get_size_estimate(self, export_provider, get_estimate):
+        provider_name = "Test_name"
+        get_estimate.return_value = 4
+        export_provider.objects.get.return_value = Mock(level_from=0, level_to=1)
+        returned_values = get_size_estimate(provider_name, bbox=[-1, -1, 0, 0])
+        export_provider.objects.get.assert_called_once_with(name=provider_name)
+        # two tiles, an arbritary value of four from the mock, one tile per level represented in array.
+        expected_values = [2, 4, [1, 1]]
+        self.assertEquals(returned_values, expected_values)
+
+#    @patch('eventkit_cloud.ui.helpers.os.path')
+    @patch('eventkit_cloud.tasks.models.ExportRun')
+
+    def test_generate_qgs_style(self,ExportRun):
+#        os_path.join.return_value = 'mock.style.qgs'
+
+        run_uid = 1234
+
+        # Fill out the behavior for mocked ExportRun by adding a provider task with
+        # subtasks for each file in all_file_list
+        mocked_provider_subtasks = []
+        for fname in [ 'F1']:
+            mps = MagicMock()
+            mps.result.filename = fname
+            mocked_provider_subtasks.append(mps)
+
+        mocked_provider_task = MagicMock()
+        mocked_provider_task.status = TaskStates.COMPLETED.value
+        mocked_provider_task.slug = 'mocked_slug'
+        mocked_provider_task.tasks.all.return_value = mocked_provider_subtasks
+
+        mocked_run = MagicMock()
+        mocked_run.job.include_zipfile = True
+        mocked_run.job.name = 'mocked_job_name'
+        mocked_run.provider_tasks.all.return_value = [mocked_provider_task]
+
+        ExportRun.objects.get.return_value = mocked_run
+
+        returnvalue = generate_qgs_style(run_uid, mocked_provider_task)
+        self.assertEquals(returnvalue,'ABC')
