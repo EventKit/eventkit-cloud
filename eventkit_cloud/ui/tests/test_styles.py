25aa572cbc8b51fda98574726cc246a3a8e9a3ae
David Sherman <david@david-sherman.com>
2017-12-12 16:20:35 -0500

    Removed duplicated tests

--- eventkit_cloud/ui/tests/test_styles.py
+++ eventkit_cloud/ui/tests/test_styles.py
@@ -18,36 +18,10 @@ from eventkit_cloud.tasks.models import (
 
 from eventkit_cloud.tasks import TaskStates
 
-logger = logging.getLogger(__name__)
-
-# @patch 'modules.class.creates_files`
-# def test_create_file(self, mock_creates_files):
-#    some_parent_function()
-#    mock_creates_files.assert_called_once_with(params)
-
-
 class TestStyles(TestCase):
-
-    def test_get_gb_estimate(self):
-        expected_return_value = 0.0001572864
-        actual_return_value = get_gb_estimate(4)
-        self.assertAlmostEqual(expected_return_value, actual_return_value, places=9)
-
-    @patch('eventkit_cloud.ui.data_estimator.get_gb_estimate')
-    @patch('eventkit_cloud.ui.data_estimator.DataProvider')
-    def test_get_size_estimate(self, export_provider, get_estimate):
-        provider_name = "Test_name"
-        get_estimate.return_value = 4
-        export_provider.objects.get.return_value = Mock(level_from=0, level_to=1)
-        returned_values = get_size_estimate(provider_name, bbox=[-1, -1, 0, 0])
-        export_provider.objects.get.assert_called_once_with(name=provider_name)
-        # two tiles, an arbritary value of four from the mock, one tile per level represented in array.
-        expected_values = [2, 4, [1, 1]]
-        self.assertEquals(returned_values, expected_values)
 
     @patch('__builtin__.open')
     @patch('eventkit_cloud.tasks.models.ExportRun')
-
     def test_generate_qgs_style(self,ExportRun, mock_open):
 
         run_uid = 1234
