35f37d46844186404bfbb027fd610dfb6cf25d10
Rees <luke.rees@rgi-corp.com>
2017-12-20 14:32:47 -0500

    add version to about and login page

--- eventkit_cloud/ui/static/ui/app/components/About/About.js
+++ eventkit_cloud/ui/static/ui/app/components/About/About.js
@@ -1,25 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component, PropTypes } from 'react';
 import AppBar from 'material-ui/AppBar';
 import CustomScrollbar from '../CustomScrollbar';
 import InfoParagraph from './InfoParagraph';
 import ThreeStepInfo from './ThreeStepInfo';
 import QuickTour from './QuickTour';
-import QuickTourSection from './QuickTourSection';
-import {Config} from '../../config.js';
+import { Config } from '../../config';
 
 export class About extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
-            pageInfo: null
-        }
-    };
-
-    componentDidMount() {
-        // later this could be api call --> set state
-        this.setState({pageInfo: Config.ABOUT_PAGE});
-    };
+            pageInfo: Config.ABOUT_PAGE,
+        };
+    }
 
     render() {
         const styles = {
@@ -28,7 +21,7 @@ export class About extends Component {
                 height: '35px',
                 color: 'white',
                 fontSize: '14px',
-                padding: '0px 34px'
+                padding: '0px 34px',
             },
             headerTitle: {
                 fontSize: '18px',
@@ -43,58 +36,71 @@ export class About extends Component {
             },
             bodyContent: {
                 padding: '30px 34px 60px',
-                maxWidth: '1000px', 
+                maxWidth: '1000px',
                 margin: 'auto',
             },
             threeStepCaption: {
-                backgroundColor: '#4598bf', 
-                padding: '5px', 
-                color: '#fff', 
-                minHeight: '50px', 
+                backgroundColor: '#4598bf',
+                padding: '5px',
+                color: '#fff',
+                minHeight: '50px',
                 width: '95%',
-            }
-            
+            },
         };
 
-        if(!this.state.pageInfo) {
+        if (!this.state.pageInfo) {
             return null;
         }
 
         return (
-            <div style={{backgroundColor: 'white'}}>
+            <div style={{ backgroundColor: 'white' }}>
                 <AppBar
-                    className={'qa-About-AppBar'}
-                    title={'About EventKit'}
+                    className="qa-About-AppBar"
+                    title="About EventKit"
                     style={styles.header}
                     titleStyle={styles.headerTitle}
                     showMenuIconButton={false}
                 >
+                    {this.context.config && this.context.config.VERSION ?
+                        <span
+                            style={{ height: '35px', lineHeight: '35px' }}
+                            className="qa-About-version"
+                        >
+                            Version {this.context.config.VERSION}
+                        </span>
+                        :
+                        null
+                    }
                 </AppBar>
                 <div style={styles.body}>
-                    <CustomScrollbar style={{height: window.innerHeight - 130, width: '100%'}}>
-                        <div style={styles.bodyContent} className={'qa-About-bodyContent'}>
+                    <CustomScrollbar style={{ height: window.innerHeight - 130, width: '100%' }}>
+                        <div style={styles.bodyContent} className="qa-About-bodyContent">
                             {this.state.pageInfo.textParagraphs.map((paragraph, ix) => {
                                 return (
-                                    <InfoParagraph key={ix} header={paragraph.header} body={paragraph.body} bodyStyle={{marginBottom: '30px'}}/>
-                                )
+                                    <InfoParagraph key={ix} header={paragraph.header} body={paragraph.body} bodyStyle={{ marginBottom: '30px' }} />
+                                );
                             })}
-                            <ThreeStepInfo steps={this.state.pageInfo.threeStep}/>
+                            <ThreeStepInfo steps={this.state.pageInfo.threeStep} />
                             {this.state.pageInfo.quickTour.length > 0 ?
-                                <h3 style={{marginTop: '60px'}}><strong>Quick Tour</strong></h3>
+                                <h3 style={{ marginTop: '60px' }}><strong>Quick Tour</strong></h3>
                                 :
                                 null
                             }
                             {this.state.pageInfo.quickTour.map((tour, ix) => {
                                 return (
-                                    <QuickTour key={ix} header={tour.header} tourSections={tour.tourSections} containerStyle={{marginTop: '30px'}}/>
-                                )
+                                    <QuickTour key={ix} header={tour.header} tourSections={tour.tourSections} containerStyle={{ marginTop: '30px' }} />
+                                );
                             })}
                         </div>
-                    </CustomScrollbar> 
+                    </CustomScrollbar>
                 </div>
             </div>
-        )
-    };
+        );
+    }
+}
+
+About.contextTypes = {
+    config: PropTypes.object,
 };
 
 export default About;
