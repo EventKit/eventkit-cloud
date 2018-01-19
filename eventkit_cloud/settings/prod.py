14ee20c274bf37365bc19df6815a61942750908c
E.J. Lane <ej.lane@rgi-corp.com>
2018-01-19 15:57:31 -0500

    adding docker-compose links, fixing url reference

--- eventkit_cloud/settings/prod.py
+++ eventkit_cloud/settings/prod.py
@@ -264,7 +264,7 @@ elif os.environ.get("REDIS_URL"):
     CACHES = {
         "default": {
             "BACKEND": "django_redis.cache.RedisCache",
-            "LOCATION": "redis://127.0.0.1:6379/1",
+            "LOCATION": os.environ.get("REDIS_URL"),
             "OPTIONS": {
                 "CLIENT_CLASS": "django_redis.client.DefaultClient",
             }
