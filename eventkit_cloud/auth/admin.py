2f04e8a704eaf429eefe9ac55409d5b4daf14967
E.J. Lane <ej.lane@rgi-corp.com>
2018-03-13 10:45:07 -0400

    removing delete option for OAuths from admin UI

--- eventkit_cloud/auth/admin.py
+++ eventkit_cloud/auth/admin.py
@@ -7,4 +7,16 @@ from django.contrib import admin
 
 logger = logging.getLogger(__name__)
 
-admin.site.register(OAuth)
+
+class OAuthAdmin(admin.ModelAdmin):
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_actions(self, request):
+        actions = super(OAuthAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+
+
+admin.site.register(OAuth, OAuthAdmin)
