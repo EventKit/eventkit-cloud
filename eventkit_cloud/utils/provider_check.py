ef7e013e5349eee82d8a39d7c0e0b2cdce7b67f1
E.J. Lane <ej.lane@rgi-corp.com>
2018-04-05 11:39:01 -0400

    updating status dictionaries to have status and type separate

--- eventkit_cloud/utils/provider_check.py
+++ eventkit_cloud/utils/provider_check.py
@@ -36,39 +36,50 @@ class CheckResults(Enum):
         (NB: for OWS sources in some cases, GetCapabilities may return 200 while GetMap/Coverage/Feature returns 403.
         In these cases, a success case will be falsely reported instead of ERR_UNAUTHORIZED.)
     """
-    TIMEOUT = {"status": "ERR_TIMEOUT",
+    TIMEOUT = {"status": "ERR",
+               "type": "TIMEOUT",
                "message": _("Your connection has timed out; the provider may be offline. Refresh to try again.")},
 
-    CONNECTION = {"status": "ERR_CONNECTION",
+    CONNECTION = {"status": "ERR",
+                  "type": "CONNECTION",
                   "message": _("A connection to this data provider could not be established.")},
 
-    UNAUTHORIZED = {"status": "ERR_UNAUTHORIZED",
+    UNAUTHORIZED = {"status": "ERR",
+                    "type": "UNAUTHORIZED",
                     "message": _("Authorization is required to connect to this data provider.")},
 
-    NOT_FOUND = {"status": "ERR_NOT_FOUND",
+    NOT_FOUND = {"status": "ERR",
+                 "type": "NOT_FOUND",
                  "message": _("The data provider was not found on the server (status 404).")},
 
-    SSL_EXCEPTION = {"status": "WARN_SSL_EXCEPTION",
+    SSL_EXCEPTION = {"status": "WARN",
+                     "type": "SSL_EXCEPTION",
                      "message": _("Could not connect securely to provider; possibly missing client certificate")},
 
-    UNAVAILABLE = {"status": "WARN_UNAVAILABLE",
+    UNAVAILABLE = {"status": "WARN",
+                   "type": "UNAVAILABLE",
                    "message": _("This data provider may be unavailable (status %(status)s).")},
 
-    UNKNOWN_FORMAT = {"status": "WARN_UNKNOWN_FORMAT",
+    UNKNOWN_FORMAT = {"status": "WARN",
+                      "type": "UNKNOWN_FORMAT",
                       "message": _("This data provider returned metadata in an unexpected format; "
                                    "errors may occur when creating the DataPack.")},
 
-    LAYER_NOT_AVAILABLE = {"status": "WARN_LAYER_NOT_AVAILABLE",
+    LAYER_NOT_AVAILABLE = {"status": "WARN",
+                           "type": "LAYER_NOT_AVAILABLE",
                            "message": _("This data provider does not offer the requested layer.")},
 
-    NO_INTERSECT = {"status": "WARN_NO_INTERSECT",
+    NO_INTERSECT = {"status": "WARN",
+                    "type": "NO_INTERSECT",
                     "message": _("The selected AOI does not intersect the data provider's layer.")},
 
-    NO_URL = {"status": "WARN_NO_URL",
+    NO_URL = {"status": "WARN",
+              "type": "NO_URL",
               "message": _("No Service URL was found in the data provider config; "
                            "availability cannot be checked")},
 
     SUCCESS = {"status": "SUCCESS",
+               "type": "SUCCESS",
                "message": _("Export should proceed without issues.")},
 
 
