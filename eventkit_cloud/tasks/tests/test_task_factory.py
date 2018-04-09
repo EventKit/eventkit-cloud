a4df8af6ea819178af861d13b6e2cc498d3026e7
David Sherman <david@david-sherman.com>
2018-04-09 11:45:12 -0400

    Repaired logic problem in task_factory create run
    User permissions were not  used correctly.

--- eventkit_cloud/tasks/tests/test_task_factory.py
+++ eventkit_cloud/tasks/tests/test_task_factory.py
@@ -14,6 +14,7 @@ from eventkit_cloud.tasks.task_factory import (TaskFactory, create_run, create_f
     get_invalid_licenses)
 from mock import patch, Mock, MagicMock, ANY
 
+from ...core.models import GroupPermission,JobPermission
 
 logger = logging.getLogger(__name__)
 
@@ -46,6 +47,8 @@ class TestExportTaskFactory(TestCase):
         self.job.region = self.region
         self.uid = str(provider_task.uid)
         self.job.save()
+        self.jp = JobPermission(job=self.job,content_object=self.user,permission=JobPermission.Permissions.ADMIN.value);
+        self.jp.save()
 
     def test_create_run_success(self):
         run_uid = create_run(job_uid=self.job.uid)
--- eventkit_cloud/tasks/tests/test_task_runners.py
+++ eventkit_cloud/tasks/tests/test_task_runners.py
@@ -16,6 +16,7 @@ from ..task_runners import (
     ExportOSMTaskRunner, ExportExternalRasterServiceTaskRunner, create_export_task_record
 )
 from ..task_factory import create_run
+from ...core.models import GroupPermission,JobPermission
 
 logger = logging.getLogger(__name__)
 
@@ -43,6 +44,10 @@ class TestExportTaskRunner(TestCase):
         self.region, created = Region.objects.get_or_create(name='Africa', the_geom=the_geom)
         self.job.region = self.region
         self.job.save()
+
+        self.jp = JobPermission(job=self.job,content_object=self.user,permission=JobPermission.Permissions.ADMIN.value);
+        self.jp.save()
+
         create_run(job_uid=self.job.uid)
 
     @patch('eventkit_cloud.tasks.task_runners.chain')
