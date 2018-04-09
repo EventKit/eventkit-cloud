d4b1241d5de89f6c150de2830170055b9ddd64e0
E.J. Lane <ej.lane@rgi-corp.com>
2018-04-09 10:13:29 -0400

    removing Sites from admin UI

--- eventkit_cloud/core/admin.py
+++ eventkit_cloud/core/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib.sites.models import Site
 
 from .models import GroupPermission, JobPermission
+
+admin.site.unregister(Site)
 
 admin.site.register(GroupPermission)
 admin.site.register(JobPermission)
