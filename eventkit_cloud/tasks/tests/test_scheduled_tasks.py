1e3cd2dbcace6dd09fbfc0d4bac42fb8e29dc848
E.J. Lane <ej.lane@rgi-corp.com>
2018-04-11 16:06:45 -0400

    making test use the CheckResults enum rather than hardcoding

--- eventkit_cloud/tasks/tests/test_scheduled_tasks.py
+++ eventkit_cloud/tasks/tests/test_scheduled_tasks.py
@@ -96,9 +96,9 @@ class TestCheckProviderAvailabilityTask(TestCase):
         statuses = DataProviderStatus.objects.filter(related_provider=first_provider)
         self.assertEqual(len(statuses), 2)
         most_recent_first_provider_status = statuses.order_by('-id')[0]
-        self.assertEqual(most_recent_first_provider_status.status, 'SUCCESS')
-        self.assertEqual(most_recent_first_provider_status.status_type, 'SUCCESS')
-        self.assertEqual(most_recent_first_provider_status.message, 'Export should proceed without issues.')
+        self.assertEqual(most_recent_first_provider_status.status, CheckResults.SUCCESS['status'])
+        self.assertEqual(most_recent_first_provider_status.status_type, CheckResults.SUCCESS['type'])
+        self.assertEqual(most_recent_first_provider_status.message, CheckResults.SUCCESS['message'])
 
 
 class TestEmailNotifications(TestCase):
