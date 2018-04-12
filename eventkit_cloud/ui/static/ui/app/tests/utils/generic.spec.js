f0719b84ef714742ecd2dc7e0b1bc37cb74ce99d
Luke <luke.rees@rgi-corp.com>
2018-04-12 10:54:21 -0400

    fix test

--- eventkit_cloud/ui/static/ui/app/tests/utils/generic.spec.js
+++ eventkit_cloud/ui/static/ui/app/tests/utils/generic.spec.js
@@ -10,14 +10,7 @@ describe('test generic utils', () => {
         detectorStub.restore();
     });
 
-    it('isBrowserValid should return true if IE version is 10 or greater', () => {
-        const detectorStub = sinon.stub(detector, 'detect')
-            .returns({ name: 'ie', version: '10.0.0' });
-        expect(utils.isBrowserValid()).toBe(true);
-        detectorStub.restore();
-    });
-
-    it('isBrowserValid should return false if IE version is less than 10', () => {
+    it('isBrowserValid should return false if browser is IE', () => {
         const detectorStub = sinon.stub(detector, 'detect')
             .returns({ name: 'ie', version: '8.7.5' });
         expect(utils.isBrowserValid()).toBe(false);
