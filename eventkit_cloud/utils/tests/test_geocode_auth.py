99231bd4b5832a1d193b5d427c1ebb64f061bff8
Gibran Parvez <gibran.parvez@rgi-corp.com>
2018-04-18 11:16:47 -0400

    Test for geocode auth

---
+++ eventkit_cloud/utils/tests/test_geocode_auth.py
@@ -0,0 +1,33 @@
+from mock import Mock, patch, call, MagicMock
+from uuid import uuid4
+from django.test import TestCase, override_settings
+import logging
+import requests_mock
+from django.conf import settings
+import json
+from django.core.cache import cache
+from ..geocode_auth import getAuthHeaders
+
+logger = logging.getLogger(__name__)
+
+class TestGeoCodeAuth(TestCase):
+
+    def setUp(self):
+        self.mock_requests = requests_mock.Mocker()
+        self.mock_requests.start()
+        self.addCleanup(self.mock_requests.stop)
+
+    def testGetHeadersWithNoURL(self):        
+        self.assertEquals(getAuthHeaders(), {})
+
+    @override_settings(GEOCODING_AUTH_URL="http://fake.url/", GEOCODING_AUTH_CERT="./readme.md", GEOCODING_AUTH_KEY="./readme.md")
+    def testGetHeaders(self):
+        testJwt = {'token': 'hello_world'}
+        self.mock_requests.get(settings.GEOCODING_AUTH_URL, text=json.dumps(testJwt), status_code=200)
+        self.assertEquals(getAuthHeaders(), {'Authorization': 'Bearer ' + testJwt['token']})
+
+    @override_settings(GEOCODING_AUTH_URL="http://fake.url/")
+    def testGetHeaders(self):        
+        cacheValue = 'this_was_in_cache'
+        cache.set('pelias_token', cacheValue, None)
+        self.assertEquals(getAuthHeaders(), {'Authorization': 'Bearer ' + cacheValue})
