23b2d590e3b07d00a0c0320e620167616fc8ebc0
Gibran Parvez <gibran.parvez@rgi-corp.com>
2018-04-24 12:41:16 -0400

    Use tmpfile

--- eventkit_cloud/utils/geocode_auth.py
+++ eventkit_cloud/utils/geocode_auth.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.core.cache import cache
 import tempfile
-
+from ..utils import auth_requests
 import logging
 import requests
 import json
+import traceback
 
 logger = logging.getLogger(__name__)
 
@@ -19,18 +20,38 @@ def getAuthHeaders():
 def authenticate():
     logger.info('Receiving new authentication token for geocoder')
     try:  
-        cert = tempfile.NamedTemporaryFile(delete=False)
-        cert.write(getattr(settings, 'GEOCODING_AUTH_CERT'))
-        cert.close()
-        key = tempfile.NamedTemporaryFile(delete=False)
-        key.write(getattr(settings, 'GEOCODING_AUTH_KEY'))
-        key.close()
+        #certString = getattr(settings, 'GEOCODING_AUTH_CERT')
+        certArray = getattr(settings, 'GEOCODING_AUTH_CERT').split("\\n")
+        certFile = tempfile.NamedTemporaryFile(suffix='.crt', delete=False)
+        for certLine in certArray:
+            certFile.write(certLine + '\n')
+
+        certFile.flush()    # ensure all data written
+        keyArray = getattr(settings, 'GEOCODING_AUTH_KEY').split("\\n")
+        keyFile = tempfile.NamedTemporaryFile(suffix='.key', delete=False)
+        for keyLine in keyArray:
+            keyFile.write(keyLine + '\n')
+
+        keyFile.flush()    # ensure all data written
+
+        certFile.close()
+        keyFile.close()
+
+
+        authResponse = requests.get(getattr(settings, 'GEOCODING_AUTH_URL'), verify=True, cert=(certFile.name, keyFile.name), headers={ 'SSL_CLIENT_CERT': getattr(settings, 'GEOCODING_AUTH_CERT').replace('\\n','')})
+
+        #authResponse = requests.get(getattr(settings, 'GEOCODING_AUTH_URL'), cert=(certFile.name))
+
+        logger.info(authResponse)
         
-        with open(getattr(settings, 'GEOCODING_AUTH_CERT'), 'r') as myfile:
-            pem=myfile.read().replace('\n', '')
-        authResponse = requests.get(getattr(settings, 'GEOCODING_AUTH_URL'), verify=True, cert=(cert, key), headers={ 'SSL_CLIENT_CERT': pem})
         cache.set('pelias_token', authResponse.json()['token'], None)
         return 
     except requests.exceptions.RequestException as e:
-        logger.error(e)
+        logger.info(traceback.print_exc())
         return
+
+#@auth_requests.content_to_file(getattr(settings, 'GEOCODING_AUTH_CERT'))
+#def getToken(*args, **kwargs):
+    #logger.info("getToken:")
+    #logger.info(kwargs.get("cert"))
+    #return requests.get(requests.get(getattr(settings, 'GEOCODING_AUTH_URL'), verify=True, cert=(), headers={ 'SSL_CLIENT_CERT': getattr(settings, 'GEOCODING_AUTH_CERT')}))
