d593c0ea2acf8e2cc12ec9c50d60f5810a3c0f97
Lars Kendall <lars@prominentedge.com>
2018-04-25 15:02:54 -0700

    Handle "removed_as_group_admin" notification and tweak verb types.

--- eventkit_cloud/ui/static/ui/app/utils/notificationUtils.js
+++ eventkit_cloud/ui/static/ui/app/utils/notificationUtils.js
@@ -6,14 +6,15 @@ import Error from 'material-ui/svg-icons/alert/error';
 import { Link } from 'react-router';
 
 const verbs = {
-    run_started: 'run_started',
-    run_canceled: 'run_canceled',
-    run_completed: 'run_completed',
-    run_failed: 'run_failed',
-    run_deleted: 'run_deleted',
-    removed_from_group: 'removed_from_group',
-    added_to_group: 'added_to_group',
-    set_as_group_admin: 'set_as_group_admin',
+    runStarted: 'run_started',
+    runCanceled: 'run_canceled',
+    runCompleted: 'run_completed',
+    runFailed: 'run_failed',
+    runDeleted: 'run_deleted',
+    removedFromGroup: 'removed_from_group',
+    addedToGroup: 'added_to_group',
+    setAsGroupAdmin: 'set_as_group_admin',
+    removedAsGroupAdmin: 'removed_as_group_admin',
 };
 
 // NOTE: This should ideally be a NotificationMessage component, but we need to return the bare elements without
@@ -48,7 +49,7 @@ export function getNotificationMessage({ notification, textStyle, linkStyle, onL
 
     const verb = notification.verb.toLowerCase();
     switch (verb) {
-        case verbs.run_started:
+        case verbs.runStarted:
             return [
                 <span key={`${notification.id}-span0`} style={styles.text}>DataPack&nbsp;</span>,
                 <Link
@@ -63,7 +64,7 @@ export function getNotificationMessage({ notification, textStyle, linkStyle, onL
                 </Link>,
                 <span key={`${notification.id}-span1`} style={styles.text}>&nbsp;has started.</span>
             ];
-        case verbs.run_canceled:
+        case verbs.runCanceled:
             return [
                 <span key={`${notification.id}-span0`} style={styles.text}>DataPack&nbsp;</span>,
                 <Link
@@ -78,7 +79,7 @@ export function getNotificationMessage({ notification, textStyle, linkStyle, onL
                 </Link>,
                 <span key={`${notification.id}-span1`} style={styles.text}>&nbsp;was canceled.</span>
             ];
-        case verbs.run_completed:
+        case verbs.runCompleted:
             return [
                 <span key={`${notification.id}-span0`} style={styles.text}>DataPack&nbsp;</span>,
                 <Link
@@ -93,7 +94,7 @@ export function getNotificationMessage({ notification, textStyle, linkStyle, onL
                 </Link>,
                 <span key={`${notification.id}-span1`} style={styles.text}>&nbsp;is complete.</span>
             ];
-        case verbs.run_failed:
+        case verbs.runFailed:
             return [
                 <span key={`${notification.id}-span0`} style={styles.text}>DataPack&nbsp;</span>,
                 <Link
@@ -108,7 +109,7 @@ export function getNotificationMessage({ notification, textStyle, linkStyle, onL
                 </Link>,
                 <span key={`${notification.id}-span1`} style={styles.text}>&nbsp;failed to complete.</span>
             ];
-        case verbs.run_deleted:
+        case verbs.runDeleted:
             return [
                 <span key={`${notification.id}-span0`} style={styles.text}>DataPack&nbsp;</span>,
                 <Link
@@ -123,19 +124,24 @@ export function getNotificationMessage({ notification, textStyle, linkStyle, onL
                 </Link>,
                 <span key={`${notification.id}-span1`} style={styles.text}>&nbsp;was deleted.</span>
             ];
-        case verbs.added_to_group:
+        case verbs.addedToGroup:
             return [
                 <span key={`${notification.id}-span0`} style={styles.text}>Added to group&nbsp;</span>,
                 <span key={`${notification.id}-span1`} style={styles.textBold}>{notification.action_object.details.name}</span>,
             ];
-        case verbs.removed_from_group:
+        case verbs.removedFromGroup:
             return [
                 <span key={`${notification.id}-span0`} style={styles.text}>Removed from group&nbsp;</span>,
                 <span key={`${notification.id}-span1`} style={styles.textBold}>{notification.action_object.details.name}</span>,
             ];
-        case verbs.set_as_group_admin:
+        case verbs.setAsGroupAdmin:
             return [
                 <span key={`${notification.id}-span0`} style={styles.text}>Set as admin of group&nbsp;</span>,
+                <span key={`${notification.id}-span1`} style={styles.textBold}>{notification.action_object.details.name}</span>,
+            ];
+        case verbs.removedAsGroupAdmin:
+            return [
+                <span key={`${notification.id}-span0`} style={styles.text}>Removed as admin of group&nbsp;</span>,
                 <span key={`${notification.id}-span1`} style={styles.textBold}>{notification.action_object.details.name}</span>,
             ];
         default:
@@ -159,12 +165,12 @@ export function getNotificationIcon({ notification, iconStyle }) {
 
     const verb = notification.verb.toLowerCase();
     switch (verb) {
-        case verbs.run_completed:
+        case verbs.runCompleted:
             return checkCircleIcon;
-        case verbs.run_deleted:
-        case verbs.run_canceled:
+        case verbs.runDeleted:
+        case verbs.runCanceled:
             return warningIcon;
-        case verbs.run_failed:
+        case verbs.runFailed:
             return errorIcon;
         default:
             return infoIcon;
@@ -174,15 +180,16 @@ export function getNotificationIcon({ notification, iconStyle }) {
 export function getNotificationViewPath(notification) {
     const verb = notification.verb.toLowerCase();
     switch (verb) {
-        case verbs.run_started:
-        case verbs.run_canceled:
-        case verbs.run_completed:
-        case verbs.run_deleted:
-        case verbs.run_failed:
+        case verbs.runStarted:
+        case verbs.runCanceled:
+        case verbs.runCompleted:
+        case verbs.runDeleted:
+        case verbs.runFailed:
             return `/status/${notification.actor.details.job.uid}`;
-        case verbs.added_to_group:
-        case verbs.removed_from_group:
-        case verbs.set_as_group_admin:
+        case verbs.addedToGroup:
+        case verbs.removedFromGroup:
+        case verbs.setAsGroupAdmin:
+        case verbs.removedAsGroupAdmin:
             return '/groups';
         default:
             return null;
