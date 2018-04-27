8c6dba500e1221da0150e7fb3e9244fda3d1fe8d
John McDouall <john.mcdouall@rgi-corp.com>
2018-04-27 11:12:11 -0400

    Slightly adjust yaml configuration field help text

--- eventkit_cloud/jobs/migrations/0013_auto_20170417_1755.py
+++ eventkit_cloud/jobs/migrations/0013_auto_20170417_1755.py
@@ -35,10 +35,10 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='exportprovider',
             name='config',
-            field=models.TextField(blank=True, default='', help_text="""WMS, TMS, WMTS, and ArcGIS-Raster require a mapproxy configuration with a 
-                              `Sources` key of imagery and a Service Layer name of imagery, the validator will also 
-                              require a layers section, but this isn't used. 
-                              OSM Services require a YAML configuration.""", null=True, verbose_name='Configuration'),
+            field=models.TextField(blank=True, default='', help_text="""WMS, TMS, WMTS, and ArcGIS-Raster require a MapProxy YAML configuration
+                              with a Sources key of imagery and a Service Layer name of imagery; the validator also
+                              requires a layers section, but this isn't used.
+                              OSM Services also require a YAML configuration.""", null=True, verbose_name='Configuration'),
         ),
         migrations.DeleteModel(
             name='ExportConfig',
