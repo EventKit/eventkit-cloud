487362a8cc9f289d9303f8d21b08222d1888f3b0
Joseph Svrcek <joseph.svrcek@rgi-corp.com>
2018-05-02 11:04:03 -0400

    changed visibility

--- eventkit_cloud/jobs/migrations/add_permissions_to_existing_jobs.py
+++ eventkit_cloud/jobs/migrations/add_permissions_to_existing_jobs.py
@@ -3,14 +3,30 @@
 from django.db import migrations
 from django.contrib.auth.models import Group, User
 
-from ..models import Job
+from ..models import Job, VisibilityState
 from ...core.models import JobPermission
 
-def add_permissions(apps, schema_editor):
+
+def update_permissions(apps, schema_editor):
     for job in Job.objects.all():
-        jp = JobPermission.objects.create(job=job, content_object=job.user,
-                                          permission=JobPermission.Permissions.ADMIN.value)
-        jp.save()
+        job_permissions = JobPermission.jobpermissions(job)
+        # first check that the job wasn't already modified with the new permissions
+        if not (job_permissions.get('users') or job_permissions.get('groups')):
+            # Then make the job creator an administrator
+            jp = JobPermission.objects.create(job=job, content_object=job.user,
+                                              permission=JobPermission.Permissions.ADMIN.value)
+            if job.published:
+                job.visibility = VisibilityState.PUBLIC.value
+                job.save()
+            jp.save()
+
+
+def rollback_permissions(apps, schema_editor):
+    # if jobs were made public then they should be set to published.
+    for job in Job.objects.all():
+        if job.visibility == VisibilityState.PUBLIC.value:
+            job.published = True
+            job.save()
 
 
 class Migration(migrations.Migration):
@@ -20,5 +36,5 @@ class Migration(migrations.Migration):
 
     operations = [
         # Existing jobs are assigned  visibility=PRIVATE. Add admin permission for the owner
-        migrations.RunPython(add_permissions,reverse_code=migrations.RunPython.noop),
+        migrations.RunPython(update_permissions, reverse_code=rollback_permissions),
     ]
