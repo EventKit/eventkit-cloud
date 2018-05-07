0ac7ffbc6c5e9efd266b03fc3120ffc646a1e8c2
Joseph Svrcek <joseph.svrcek@rgi-corp.com>
2018-05-07 15:52:25 -0400

    cleaned up migrations

--- eventkit_cloud/jobs/migrations/0029_add_permissions_to_existing_jobs.py
+++ eventkit_cloud/jobs/migrations/0029_add_permissions_to_existing_jobs.py
@@ -3,18 +3,21 @@
 from django.db import migrations
 from django.contrib.auth.models import Group, User
 
-from ..models import Job, VisibilityState
+from ..models import VisibilityState
 from ...core.models import JobPermission
 
 
 def update_permissions(apps, schema_editor):
+    Job = apps.get_model('jobs', 'Job')
+    ExportRun = apps.get_model('tasks', 'ExportRun')
+
     for job in Job.objects.all():
         job_permissions = JobPermission.jobpermissions(job)
         # first check that the job wasn't already modified with the new permissions
         if not (job_permissions.get('users') or job_permissions.get('groups')):
             # Then make the job creator an administrator
             jp = JobPermission.objects.create(job=job, content_object=job.user,
-                                              permission=JobPermission.Permissions.ADMIN.value)
+                                              permission=JobPermissionLevel.ADMIN.value)
             if job.published:
                 job.visibility = VisibilityState.PUBLIC.value
                 job.save()
@@ -31,7 +34,7 @@ def rollback_permissions(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('jobs', '0030_set_last_export_run'),
+        ('jobs', '0028_merge_20180417_1723'),
     ]
 
     operations = [
