9d59dff5cd8e7c335f179ce87bd92d8f8f81545a
Joseph Svrcek <joseph.svrcek@rgi-corp.com>
2018-08-27 07:30:23 -0400

    fix ascii unicode issues in helpers

--- eventkit_cloud/tasks/helpers.py
+++ eventkit_cloud/tasks/helpers.py
@@ -136,7 +136,7 @@ def create_license_file(provider_task):
                                      '{0}.txt'.format(normalize_name(data_provider_license.name)))
 
     with open(license_file_path, 'w') as license_file:
-        license_file.write(data_provider_license.text)
+        license_file.write(data_provider_license.text.encode('utf-8'))
 
     return license_file_path
 
@@ -223,7 +223,7 @@ def generate_qgs_style(run_uid=None, data_provider_task_record=None):
                                                                       'provider_details': provider_details,
                                                                       'bbox': run.job.extents,
                                                                       'has_raster': has_raster,
-                                                                      'has_elevation': has_elevation}))
+                                                                      'has_elevation': has_elevation}).encode('utf-8'))
     return style_file
 
 
@@ -244,7 +244,7 @@ def get_human_readable_metadata_document(run_uid):
         data_provider = DataProvider.objects.get(slug=provider_task.slug)
         provider_type = data_provider.export_provider_type.type_name
         data_provider_metadata = {'name': data_provider.name,
-                                  'description': data_provider.service_description.replace('\r\n', '\n').replace('\n', '\r\n\t'),
+                                  'description': data_provider.service_description.encode('utf-8').replace('\r\n', '\n').replace('\n', '\r\n\t'),
                                   'last_update': get_last_update(data_provider.url,
                                                                  provider_type,
                                                                  slug=data_provider.slug),
@@ -264,7 +264,7 @@ def get_human_readable_metadata_document(run_uid):
     metadata_file = os.path.join(stage_dir, '{0}_ReadMe.txt'.format(normalize_name(run.job.name)))
 
     with open(metadata_file, 'w') as open_file:
-        open_file.write(render_to_string('styles/metadata.txt', context={'metadata': metadata}).replace('\r\n', '\n').replace('\n', '\r\n'))
+        open_file.write(render_to_string('styles/metadata.txt', context={'metadata': metadata}).encode('utf-8').replace('\r\n', '\n').replace('\n', '\r\n'))
 
     return metadata_file
 
