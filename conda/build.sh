834470a75b5a25ec88a111f980c6291507778932
Joseph Svrcek <joesvrcek@hotmail.com>
2018-09-29 10:03:14 -0400

    EVK-99 added new recipes

--- conda/build.sh
+++ conda/build.sh
@@ -23,7 +23,7 @@ conda config --add channels local
 
 cd /root/repo
 mkdir -p /root/repo/linux-64 /root/repo/noarch
-conda index linux-64 noarch
+conda index linux-64 noarch || echo "JSON Parse Error"
 conda config --add channels file://root/repo/
 
 echo "Building recipes"
@@ -36,7 +36,7 @@ if [ -z "$1" ]; then
     done < /root/recipes.txt
 else
  echo "***Building $@...***"
-    for i in 1 2 3; do conda build $@ && s=0 && break || sleep 15; done; (exit $s)
+    for i in 1; do conda build $@ && s=0 && break || sleep 15; done; (exit $s)
 fi
 echo "Move files and create index"
 cp /root/miniconda3/pkgs/*.tar.bz2 /root/repo/linux-64/
@@ -44,5 +44,5 @@ cp /root/miniconda3/conda-bld/linux-64/*.tar.bz2 /root/repo/linux-64/
 
 conda config --add channels file://root/repo/
 cd /root/repo
-conda index linux-64 noarch
+conda index linux-64 noarch || echo "JSON Parse Error"
 
