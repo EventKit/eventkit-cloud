aa8585c8409270f468bc28c456f172ad172048b8
Joseph Svrcek <joesvrcek@hotmail.com>
2018-10-12 10:18:32 -0400

    fixed admin, add TaskResults view, fix Downloads

--- eventkit_cloud/tasks/admin.py
+++ eventkit_cloud/tasks/admin.py
@@ -1,11 +1,51 @@
+from django import forms
 from django.contrib import admin
 
-from eventkit_cloud.tasks.models import ExportRun, UserDownload
+from eventkit_cloud.tasks.models import ExportRun, UserDownload, ExportTaskRecord, FileProducingTaskResult, \
+    ExportTaskException
+
+from eventkit_cloud.tasks.export_tasks import TaskStates
+
+import logging
+import pickle
+import traceback
+
+logger = logging.getLogger(__name__)
+
+
+class ExportTaskRecordAdmin(admin.ModelAdmin):
+    model = ExportTaskRecord
+
+    readonly_fields = ('export_provider_task', 'celery_uid', 'name', 'status', 'progress', 'estimated_finish',
+                       'pid', 'worker', 'cancel_user', 'result', 'exceptions')
+    search_fields = ['uid', 'name', 'status', 'export_provider_task__uid', 'export_provider_task__slug']
+
+    @staticmethod
+    def exceptions(obj):
+
+        exceptions = []
+        for exc in obj.exceptions.all():
+            # set a default (incase not found)
+            exc_info = ["", "Exception info not found or unreadable."]
+            try:
+                exc_info = pickle.loads(exc.exception.encode()).exc_info
+            except Exception as te:
+                logger.warning(te)
+            exceptions += [' '.join(traceback.format_exception(*exc_info))]
+        return '\n'.join(exceptions)
+
+    list_display = ['uid', 'name', 'progress', 'status', 'display']
+
+    def has_add_permission(self, request, obj=None):
+        return False
 
 
 class ExportRunAdmin(admin.ModelAdmin):
-    readonly_fields=('delete_user', 'user', 'status', 'started_at', 'finished_at')
+    readonly_fields = ('delete_user', 'user', 'status', 'started_at', 'finished_at')
     list_display = ['uid', 'status', 'user', 'notified', 'expiration', 'deleted']
+
+    def has_add_permission(self, request, obj=None):
+        return False
 
 
 class UserDownloadAdmin(admin.ModelAdmin):
@@ -18,6 +58,7 @@ class UserDownloadAdmin(admin.ModelAdmin):
             return 'DataPack Zip'
         else:
             return self.provider.slug
+
     list_display_provider.short_description = 'Provider'
     model = UserDownload
 
@@ -31,3 +72,4 @@ class UserDownloadAdmin(admin.ModelAdmin):
 
 admin.site.register(ExportRun, ExportRunAdmin)
 admin.site.register(UserDownload, UserDownloadAdmin)
+admin.site.register(ExportTaskRecord, ExportTaskRecordAdmin)
