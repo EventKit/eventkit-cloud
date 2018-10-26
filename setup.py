1cf6eaaa0e6fa83c48bf54eff4cf2f40787de8e3
Joseph Svrcek <joesvrcek@hotmail.com>
2018-10-26 14:36:34 -0400

    updated install files

--- setup.py
+++ setup.py
@@ -6,21 +6,19 @@ def read(*rnames):
 
 setup(
     name="eventkit",
-    version="0.1",
-    author="",
-    author_email="",
+    version="1.2.1",
+    author="Joseph Svrcek",
+    author_email="joseph.svrcek@rgi-corp.com",
     description="Eventkit Cloud",
     long_description=(read('readme.md')),
     # Full list of classifiers can be found at:
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 4 - Beta',
     ],
     license="BSD",
-    keywords="osm-export-tool2 django eventkit",
+    keywords="eventkit osm-export-tool django",
     packages=['eventkit_cloud',],
     include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-    ]
 )
+
