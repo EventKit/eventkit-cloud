39fb6fa8f945fbabfe95f18cedb042a049d0a55f
Joseph Svrcek <joesvrcek@hotmail.com>
2018-11-15 09:56:53 -0500

    fix run script

--- scripts/run-celery.sh
+++ scripts/run-celery.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-celery worker -A eventkit_cloud --concurrency=$$CONCURRENCY --loglevel=$$LOG_LEVEL -n worker@%h -Q $$HOSTNAME & \
-celery worker -A eventkit_cloud --loglevel=$$LOG_LEVEL -n celery@%h -Q celery & \
-celery worker -A eventkit_cloud --loglevel=$$LOG_LEVEL -n cancel@%h -Q $$HOSTNAME.cancel & \
-celery worker -A eventkit_cloud --loglevel=$$LOG_LEVEL -n finalize@%h -Q $$HOSTNAME.finalize & \
-celery worker -A eventkit_cloud --loglevel=$$LOG_LEVEL -n osm@%h -Q $$HOSTNAME.osm
+celery worker -A eventkit_cloud --concurrency=$CONCURRENCY --loglevel=$LOG_LEVEL -n worker@%h -Q $HOSTNAME & \
+celery worker -A eventkit_cloud --loglevel=$LOG_LEVEL -n celery@%h -Q celery & \
+celery worker -A eventkit_cloud --loglevel=$LOG_LEVEL -n cancel@%h -Q $HOSTNAME.cancel & \
+celery worker -A eventkit_cloud --loglevel=$LOG_LEVEL -n finalize@%h -Q $HOSTNAME.finalize & \
+celery worker -A eventkit_cloud --loglevel=$LOG_LEVEL -n osm@%h -Q $HOSTNAME.osm
