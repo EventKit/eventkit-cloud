b9238725f8f033bbe4c6f021ed1d057aa2ea6627
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2019-05-06 10:51:51 -0400

    Update whitenoise settings to follow the version upgrade instructions.

--- eventkit_cloud/tasks/scripts/debug_celery.py
+++ eventkit_cloud/tasks/scripts/debug_celery.py
@@ -9,11 +9,8 @@ proj_path = "/var/lib/eventkit"
 sys.path.append(proj_path)
 os.chdir(proj_path)
 
-from whitenoise.django import DjangoWhiteNoise
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventkit_cloud.settings.prod")
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
 
 from eventkit_cloud.tasks.scripts.debug import run_chain
 
