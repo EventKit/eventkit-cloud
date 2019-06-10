2faa438ddf11c345a5f389ba6527716fc27f59d7
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2019-06-10 11:55:49 -0400

    Fix spacing issues in the command string.

--- eventkit_cloud/tasks/scheduled_tasks.py
+++ eventkit_cloud/tasks/scheduled_tasks.py
@@ -94,14 +94,14 @@ def pcf_scale_celery(max_instances):
         return
 
     celery_group_name = uuid.uuid4()
-    command = (f"python manage.py runinitial && echo 'Starting celery workers' && celery worker -A eventkit_cloud"
-    f"--concurrency=$CONCURRENCY --loglevel=$LOG_LEVEL -n runs@{celery_group_name} -Q runs & exec celery worker -A"
-    f"eventkit_cloud --concurrency=$CONCURRENCY --loglevel=$LOG_LEVEL -n worker@{celery_group_name} -Q"
-    f"{celery_group_name} & exec celery worker -A eventkit_cloud --loglevel=$LOG_LEVEL -n celery@{celery_group_name} -Q"
-    f"celery & exec celery worker -A eventkit_cloud --loglevel=$LOG_LEVEL -n cancel@{celery_group_name} -Q"
-    f"{celery_group_name}.cancel & exec celery worker -A eventkit_cloud --concurrency=2 -n"
-    f"finalize@{celery_group_name} -Q {celery_group_name}.finalize & exec celery worker -A eventkit_cloud"
-    f"--concurrency=1 --loglevel=$LOG_LEVEL -n osm@{celery_group_name} -Q {celery_group_name}.osm &"
+    command = (f"python manage.py runinitial && echo 'Starting celery workers' && celery worker -A eventkit_cloud "
+    f"--concurrency=$CONCURRENCY --loglevel=$LOG_LEVEL -n runs@{celery_group_name} -Q runs & exec celery worker -A "
+    f"eventkit_cloud --concurrency=$CONCURRENCY --loglevel=$LOG_LEVEL -n worker@{celery_group_name} -Q "
+    f"{celery_group_name} & exec celery worker -A eventkit_cloud --loglevel=$LOG_LEVEL -n celery@{celery_group_name} -Q "
+    f"celery & exec celery worker -A eventkit_cloud --loglevel=$LOG_LEVEL -n cancel@{celery_group_name} -Q "
+    f"{celery_group_name}.cancel & exec celery worker -A eventkit_cloud --concurrency=2 -n "
+    f"finalize@{celery_group_name} -Q {celery_group_name}.finalize & exec celery worker -A eventkit_cloud "
+    f"--concurrency=1 --loglevel=$LOG_LEVEL -n osm@{celery_group_name} -Q {celery_group_name}.osm & "
     f"exec export CELERY_GROUP_NAME={celery_group_name}")
 
     message_count = get_message_count("runs")
