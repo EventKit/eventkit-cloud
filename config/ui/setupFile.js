e81fce84e27c03830ac2f64167edeecee918b8ac
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2019-07-10 16:00:26 -0400

    Update jsdom syntax to allow tests to run.

--- config/ui/setupFile.js
+++ config/ui/setupFile.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { jsdom } from 'jsdom';
+import jsdom from 'jsdom';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'raf/polyfill';
@@ -7,8 +7,9 @@ import theme from './eventkit_cloud/ui/static/ui/app/styles/eventkit_theme';
 
 // this adds the ability to change the dom size when testing components that render
 // differently for different dom sizes
+const { JSDOM } = jsdom;
 const documentHTML = '<!doctype html><html><body><div id="root"></div></body></html>';
-global.document = jsdom(documentHTML);
+global.document = new JSDOM(documentHTML);
 global.window = document.parentWindow;
 
 global.window.resizeTo = (width, height) => {
