24b22a1bdd8101d350a8e78c8e1f1936c0824471
Caboose71 <50183410+Caboose71@users.noreply.github.com>
2019-07-31 15:52:29 -0400

    Add login authentication error handling (#708)
    * Force redirect from callback view when error is caught
    
    * Rework redirection handling
    
    * Adjust message color and positioning
    
    * Change error catching to ignore status checking with GET calls
    
    * Fix error message displaying early
    
    * Remove unused action
    
    * Remove unneeded style and elements
    
    * Add contact url
    
    * Rework error contact display
    
    * Remove broken test component
    
    * Fix typo in error message

---
+++ eventkit_cloud/ui/static/ui/app/components/auth/ErrorMessage.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import {withTheme, Theme, createStyles, withStyles} from '@material-ui/core/styles';
+import { Breakpoint } from '@material-ui/core/styles/createBreakpoints';
+import {Button} from "@material-ui/core";
+import * as PropTypes from "prop-types";
+
+export interface Props {
+    theme: Eventkit.Theme & Theme;
+    width: Breakpoint;
+    router: any;
+}
+
+export class ErrorMessage extends React.Component<Props, {}> {
+
+    static contextTypes = {
+        config: PropTypes.object,
+    };
+
+    constructor(props: Props) {
+        super(props);
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick(event) {
+        event.preventDefault();
+        // Redirect to the logout page (logout container).
+        // This is done as an attempt to reset the user to a blank slate.
+        window.location.assign('/logout');
+    }
+
+    getErrorMessage() {
+        let errorMessage;
+        if(this.context.config.CONTACT_URL) {
+            errorMessage = (
+                <a className={`qa-Error-contact`}
+                   href={this.context.config.CONTACT_URL}>contact us.</a>)
+        }
+        else {
+            errorMessage = "contact an administrator.";
+        }
+        return errorMessage;
+    }
+
+    render() {
+        const { colors } = this.props.theme.eventkit;
+
+        const styles = {
+            form: {
+                verticalAlign: 'middle',
+                margin: '0 auto',
+                maxWidth: 300,
+            },
+            message: {
+                width: '100%',
+                fontSize: '18px',
+                color: colors.white,
+                marginTop: '30px'
+            },
+        };
+
+
+        return (
+            <div style={{ verticalAlign: 'middle', textAlign: 'center' }}>
+                    <div style={{
+                        ...styles.message,
+                        fontSize: '20px',
+                        marginTop: '0px',
+                        marginBottom: '90px'
+                    }}>
+                        <strong>SERVER ERROR</strong>
+                        <p style={styles.message}>
+                        An error occurred during the authentication process. Please try again or {this.getErrorMessage()}
+                        </p>
+                    </div>
+                    <Button
+                        onClick={this.onClick}
+                        name="submit"
+                        color="primary"
+                        variant="contained"
+                    >
+                        Return to Login
+                    </Button>
+            </div>
+        );
+    }
+}
+
+export default withTheme()(ErrorMessage);
---
+++ eventkit_cloud/ui/static/ui/app/components/auth/LoginErrorPage.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import { withTheme, Theme } from '@material-ui/core/styles';
+import withWidth, { isWidthDown } from '@material-ui/core/withWidth';
+import { Breakpoint } from '@material-ui/core/styles/createBreakpoints';
+import Paper from '@material-ui/core/Paper';
+import ErrorMessage from "./ErrorMessage";
+
+interface Props {
+    theme: Eventkit.Theme & Theme;
+    width: Breakpoint;
+    router: any;
+}
+
+export class LoginErrorPage extends React.Component<Props, {}> {
+    render() {
+        const { colors, images } = this.props.theme.eventkit;
+
+        const mobile = isWidthDown('sm', this.props.width);
+        const styles = {
+            wholeDiv: {
+                width: '100%',
+                height: 'calc(100vh - 95px)',
+                backgroundColor: colors.background,
+            },
+            paper: {
+                display: 'inline-block',
+                backgroundImage: `url(${images.topo_light})`,
+                backgroundRepeat: 'repeat repeat',
+                padding: '30px',
+                height: '390px',
+                width: '100%',
+            },
+            container: {
+                position: 'relative' as 'relative',
+                display: 'flex',
+                justifyContent: 'center',
+                flexWrap: 'wrap' as 'wrap',
+                minHeight: '100%',
+            },
+            paperContainer: {
+                flex: '1 1 50%',
+                maxWidth: '600px',
+                padding: '15px',
+                alignSelf: 'center',
+            },
+            footerText: {
+                color: colors.grey,
+                padding: '5px 10px 5px',
+                opacity: 0.5,
+                fontSize: '9px',
+                textAlign: 'right' as 'right',
+            },
+            desktopFooter: {
+                position: 'absolute' as 'absolute',
+                bottom: '0px',
+                right: '0px',
+            },
+            mobileFooter: {
+                flex: '1 1 100%',
+                alignSelf: 'flex-end',
+            },
+        };
+
+        return (
+            <div style={styles.wholeDiv}>
+                    <div style={styles.container} className="qa-LoginPage-container">
+                        <div style={styles.paperContainer}>
+                            <Paper className="qa-LoginErrorPage-Paper" style={styles.paper}>
+                                <ErrorMessage {...this.props}/>
+                            </Paper>
+                        </div>
+                        <div style={mobile ? styles.mobileFooter : styles.desktopFooter}>
+                            <div style={styles.footerText} className="qa-LoginErrorPage-browser-text">
+                                Supported Browsers: Chrome, Firefox, Opera, and Edge
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        );
+    }
+}
+
+export default withWidth()(withTheme()(LoginErrorPage));
