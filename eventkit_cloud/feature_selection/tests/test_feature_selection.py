b489a3ae2feb9378e1b712056c94fb26e9cf7ec8
Carrie Savinell <carrielane89@gmail.com>
2019-11-26 12:01:10 -0500

    Reformatting feature_selection module

--- eventkit_cloud/feature_selection/tests/test_feature_selection.py
+++ eventkit_cloud/feature_selection/tests/test_feature_selection.py
@@ -20,61 +20,63 @@ column1 http://wiki.openstreetmap.org/wiki/Key:column1
 
 (c) OpenStreetMap contributors.
 
-This file is made available under the Open Database License: http://opendatacommons.org/licenses/odbl/1.0/. Any rights in individual contents of the database are licensed under the Database Contents License: http://opendatacommons.org/licenses/dbcl/1.0/
+This file is made available under the Open Database License: http://opendatacommons.org/licenses/odbl/1.0/.
+Any rights in individual contents of the database are licensed under the
+Database Contents License: http://opendatacommons.org/licenses/dbcl/1.0/
 """
+
 
 class TestFeatureSelection(unittest.TestCase):
     maxDiff = None
 
     def test_reserved_table_names(self):
         # these table names are used by the ogr2ogr gpkg importer
-        y = '''
+        y = """
         points:
             select:
                 - name
-        '''
+        """
         f = FeatureSelection(y)
         self.assertFalse(f.valid)
-        self.assertEqual(f.errors[0],"Theme name reserved: points")
-        y = '''
+        self.assertEqual(f.errors[0], "Theme name reserved: points")
+        y = """
         rtree_something:
             select:
                 - name
-        '''
+        """
         f = FeatureSelection(y)
         self.assertFalse(f.valid)
-        self.assertEqual(f.errors[0],"Theme name reserved: rtree_something")
-        y = '''
+        self.assertEqual(f.errors[0], "Theme name reserved: rtree_something")
+        y = """
         gpkg_something:
             select:
                 - name
-        '''
+        """
         f = FeatureSelection(y)
         self.assertFalse(f.valid)
-        self.assertEqual(f.errors[0],"Theme name reserved: gpkg_something")
+        self.assertEqual(f.errors[0], "Theme name reserved: gpkg_something")
 
     def test_empty_feature_selection(self):
-        y = '''
-        '''
+        y = """
+        """
         f = FeatureSelection(y)
         self.assertFalse(f.valid)
 
     def test_theme_names(self):
-        y = '''
+        y = """
         A Theme Name:
             select:
                 - name
-        '''
+        """
         f = FeatureSelection(y)
         self.assertTrue(f.valid)
-        self.assertEqual(f.themes,["A Theme Name"])
-        self.assertEqual(f.slug_themes,["a_theme_name"])
-
+        self.assertEqual(f.themes, ["A Theme Name"])
+        self.assertEqual(f.slug_themes, ["a_theme_name"])
 
     def test_key_union_and_filters(self):
-        y = '''
+        y = """
         waterways:
-            types: 
+            types:
                 - lines
                 - polygons
             select:
@@ -89,18 +91,20 @@ class TestFeatureSelection(unittest.TestCase):
                 - name
                 - building
             where: building IS NOT NULL
-        '''
+        """
         f = FeatureSelection(y)
-        self.assertCountEqual(f.themes,['buildings','waterways'])
-        self.assertCountEqual(f.geom_types('waterways'),['lines','polygons'])
-        self.assertCountEqual(f.key_selections('waterways'),['name','waterway'])
-        self.assertEqual(f.filter_clause('waterways'),'"name" IS NOT NULL OR "waterway" IS NOT NULL')
-        self.assertCountEqual(f.key_union(), ['building','name','waterway'])
-        self.assertCountEqual(f.key_union('points'), ['building','name'])
-        self.assertEqual(f.filter_clause('buildings'),'building IS NOT NULL')
+        self.assertCountEqual(f.themes, ["buildings", "waterways"])
+        self.assertCountEqual(f.geom_types("waterways"), ["lines", "polygons"])
+        self.assertCountEqual(f.key_selections("waterways"), ["name", "waterway"])
+        self.assertEqual(
+            f.filter_clause("waterways"), '"name" IS NOT NULL OR "waterway" IS NOT NULL'
+        )
+        self.assertCountEqual(f.key_union(), ["building", "name", "waterway"])
+        self.assertCountEqual(f.key_union("points"), ["building", "name"])
+        self.assertEqual(f.filter_clause("buildings"), "building IS NOT NULL")
 
     def test_sql_list(self):
-        y = '''
+        y = """
         waterways:
             types:
                 - polygons
@@ -109,12 +113,14 @@ class TestFeatureSelection(unittest.TestCase):
             where:
                 - name IS NOT NULL
                 - name = 'some building'
-        '''
+        """
         f = FeatureSelection(y)
-        self.assertEqual(f.filter_clause('waterways'),"name IS NOT NULL OR name = 'some building'")
+        self.assertEqual(
+            f.filter_clause("waterways"), "name IS NOT NULL OR name = 'some building'"
+        )
 
     def test_sqls(self):
-        y = '''
+        y = """
         buildings:
             types:
                 - points
@@ -122,157 +128,177 @@ class TestFeatureSelection(unittest.TestCase):
             select:
                 - name
                 - addr:housenumber
-        '''
+        """
         f = FeatureSelection(y)
         create_sqls, index_sqls = f.sqls
-        self.assertEqual(create_sqls[0],'CREATE TABLE buildings_points(\nfid INTEGER PRIMARY KEY AUTOINCREMENT,\ngeom POINT,\nosm_id TEXT,"name" TEXT,"addr:housenumber" TEXT\n);\nINSERT INTO buildings_points(geom, osm_id,"name","addr:housenumber") select geom, osm_id,"name","addr:housenumber" from points WHERE ("name" IS NOT NULL OR "addr:housenumber" IS NOT NULL);\n')
-        self.assertEqual(create_sqls[1],'CREATE TABLE buildings_polygons(\nfid INTEGER PRIMARY KEY AUTOINCREMENT,\ngeom MULTIPOLYGON,\nosm_id TEXT,osm_way_id TEXT,"name" TEXT,"addr:housenumber" TEXT\n);\nINSERT INTO buildings_polygons(geom, osm_id,osm_way_id,"name","addr:housenumber") select geom, osm_id,osm_way_id,"name","addr:housenumber" from multipolygons WHERE ("name" IS NOT NULL OR "addr:housenumber" IS NOT NULL);\n')
+        self.assertEqual(
+            create_sqls[0],
+            'CREATE TABLE buildings_points(\nfid INTEGER PRIMARY KEY AUTOINCREMENT,\ngeom POINT,\nosm_id TEXT,'
+            '"name" TEXT,"addr:housenumber" TEXT\n);\nINSERT INTO buildings_points(geom, osm_id,'
+            '"name","addr:housenumber") select geom, osm_id,"name","addr:housenumber" from points '
+            'WHERE ("name" IS NOT NULL OR "addr:housenumber" IS NOT NULL);\n',
+        )
+        self.assertEqual(
+            create_sqls[1],
+            'CREATE TABLE buildings_polygons(\nfid INTEGER PRIMARY KEY AUTOINCREMENT,\ngeom MULTIPOLYGON,\nosm_id '
+            'TEXT,osm_way_id TEXT,"name" TEXT,"addr:housenumber" TEXT\n);\n'
+            'INSERT INTO buildings_polygons(geom, osm_id,osm_way_id,"name","addr:housenumber") '
+            'select geom, osm_id,osm_way_id,"name","addr:housenumber" from multipolygons '
+            'WHERE ("name" IS NOT NULL OR "addr:housenumber" IS NOT NULL);\n',
+        )
 
     def test_zindex(self):
-        y = '''
+        y = """
         roads:
             types:
-                - lines 
+                - lines
             select:
                 - highway
-        '''
+        """
         f = FeatureSelection(y)
         create_sqls, index_sqls = f.sqls
-        self.assertEqual(create_sqls[0],'CREATE TABLE roads_lines(\nfid INTEGER PRIMARY KEY AUTOINCREMENT,\ngeom MULTILINESTRING,\nosm_id TEXT,"highway" TEXT,"z_index" TEXT\n);\nINSERT INTO roads_lines(geom, osm_id,"highway","z_index") select geom, osm_id,"highway","z_index" from lines WHERE ("highway" IS NOT NULL);\n')
-
+        self.assertEqual(
+            create_sqls[0],
+            'CREATE TABLE roads_lines(\nfid INTEGER PRIMARY KEY AUTOINCREMENT,\ngeom MULTILINESTRING,\nosm_id '
+            'TEXT,"highway" TEXT,"z_index" TEXT\n);\nINSERT INTO roads_lines(geom, osm_id,"highway","z_index") '
+            'select geom, osm_id,"highway","z_index" from lines WHERE ("highway" IS NOT NULL);\n',
+        )
 
     def test_unsafe_yaml(self):
-        y = '''
+        y = """
         !!python/object:feature_selection.feature_selection.FeatureSelection
         a: 0
-        '''
+        """
         f = FeatureSelection(y)
         self.assertFalse(f.valid)
-        self.assertEqual(1,len(f.errors))
+        self.assertEqual(1, len(f.errors))
 
     def test_malformed_yaml(self):
         # if it's not a valid YAML document
         # TODO: errors for if yaml indentation is incorrect
-        y = '''
+        y = """
         all
             select:
                 - name
-        '''
+        """
         f = FeatureSelection(y)
         self.assertFalse(f.valid)
 
     def test_minimal_yaml(self):
         # the shortest valid feature selection
-        y = '''
-        all: 
+        y = """
+        all:
             select:
                 - name
-        '''
+        """
         f = FeatureSelection(y)
         self.assertTrue(f.valid)
-        self.assertEqual(f.geom_types('all'),['points','lines','polygons'])
+        self.assertEqual(f.geom_types("all"), ["points", "lines", "polygons"])
 
     def test_unspecified_yaml(self):
         # top level is a list and not a dict
-        y = '''
-        - all: 
+        y = """
+        - all:
             select:
                 - name
-        '''
+        """
         f = FeatureSelection(y)
         self.assertFalse(f.valid)
-        self.assertEqual(f.errors[0],"YAML must be dict, not list")
+        self.assertEqual(f.errors[0], "YAML must be dict, not list")
 
     def test_dash_spacing_yaml(self):
         # top level is a list and not a dict
-        y = '''
-        all: 
+        y = """
+        all:
           select:
             -name
-        '''
+        """
         f = FeatureSelection(y)
         self.assertFalse(f.valid)
 
     def test_no_select_yaml(self):
         # top level is a list and not a dict
-        y = '''
-        all: 
+        y = """
+        all:
           -select:
             - name
-        '''
+        """
         f = FeatureSelection(y)
         self.assertFalse(f.valid)
-        self.assertEqual(f.errors[0],"Each theme must have a 'select' key")
+        self.assertEqual(f.errors[0], "Each theme must have a 'select' key")
 
     # refer to https://taginfo.openstreetmap.org/keys
     def test_valid_invalid_key_yaml(self):
-        y = '''
-        all: 
+        y = """
+        all:
           select:
             - has space
             - has_underscore
             - has:colon
             - UPPERCASE
-        '''
+        """
         f = FeatureSelection(y)
         self.assertTrue(f.valid)
-        y = '''
-        all: 
+        y = """
+        all:
           select:
             - na?me
-        '''
+        """
         f = FeatureSelection(y)
         self.assertFalse(f.valid)
-        self.assertEqual(f.errors[0],"Invalid OSM key: na?me")
-        y = '''
-        all: 
+        self.assertEqual(f.errors[0], "Invalid OSM key: na?me")
+        y = """
+        all:
           select:
             -
-        '''
+        """
         f = FeatureSelection(y)
         self.assertFalse(f.valid)
-        self.assertEqual(f.errors[0],"Missing OSM key")
+        self.assertEqual(f.errors[0], "Missing OSM key")
 
     def test_passes_sqlvalidator_errors(self):
-        y = '''
+        y = """
         buildings:
             select:
                 - name
                 - addr:housenumber
             where: addr:housenumber IS NOT NULL
-        '''
+        """
         f = FeatureSelection(y)
         self.assertFalse(f.valid)
-        self.assertEqual(f.errors[0], "SQL WHERE Invalid: identifier with colon : must be in double quotes.")
+        self.assertEqual(
+            f.errors[0],
+            "SQL WHERE Invalid: identifier with colon : must be in double quotes.",
+        )
 
     def test_enforces_subset_columns(self):
-        y = '''
+        y = """
         buildings:
             types:
                 - polygons
             select:
-                - column1 
+                - column1
             where: column2 IS NOT NULL
         other:
             types:
                 - points
             select:
                 - column3
-        '''
+        """
         f = FeatureSelection(y)
         self.assertTrue(f.valid)
-        self.assertEqual(f.key_union(), ['column1','column2','column3'])
-        self.assertEqual(f.key_union('points'), ['column3'])
+        self.assertEqual(f.key_union(), ["column1", "column2", "column3"])
+        self.assertEqual(f.key_union("points"), ["column3"])
 
     def test_zip_readme(self):
-        y = '''
+        y = """
         buildings:
             select:
-                - column1 
+                - column1
             where: column2 IS NOT NULL
         other:
             select:
                 - column3
-        '''
+        """
         f = FeatureSelection(y)
-        self.assertMultiLineEqual(f.zip_readme('buildings'),ZIP_README)
+        self.assertMultiLineEqual(f.zip_readme("buildings"), ZIP_README)
--- eventkit_cloud/feature_selection/tests/test_sql.py
+++ eventkit_cloud/feature_selection/tests/test_sql.py
@@ -7,7 +7,6 @@ from eventkit_cloud.feature_selection.sql import SQLValidator, OsmfilterRule
 
 
 class TestSQLValidator(unittest.TestCase):
-
     def test_basic(self):
         s = SQLValidator("name = 'a name'")
         self.assertTrue(s.valid)
@@ -16,7 +15,7 @@ class TestSQLValidator(unittest.TestCase):
         s = SQLValidator("natural in ('water','cliff')")
         self.assertTrue(s.valid)
 
-    #TODO OGR uses text for all things so numerical comparisons will not be correct
+    # TODO OGR uses text for all things so numerical comparisons will not be correct
     def test_float_value(self):
         s = SQLValidator("height > 20")
         self.assertTrue(s.valid)
@@ -42,7 +41,9 @@ class TestSQLValidator(unittest.TestCase):
     def test_colons_etc(self):
         s = SQLValidator("addr:housenumber IS NOT NULL")
         self.assertFalse(s.valid)
-        self.assertEqual(s.errors,['identifier with colon : must be in double quotes.'])
+        self.assertEqual(
+            s.errors, ["identifier with colon : must be in double quotes."]
+        )
         s = SQLValidator("admin_level IS NOT NULL")
         self.assertTrue(s.valid)
         s = SQLValidator('"addr:housenumber" IS NOT NULL')
@@ -53,44 +54,44 @@ class TestSQLValidator(unittest.TestCase):
     def test_invalid_sql(self):
         s = SQLValidator("drop table planet_osm_polygon")
         self.assertFalse(s.valid)
-        self.assertEqual(s.errors,['SQL could not be parsed.'])
+        self.assertEqual(s.errors, ["SQL could not be parsed."])
         s = SQLValidator("(drop table planet_osm_polygon)")
         self.assertFalse(s.valid)
-        self.assertEqual(s.errors,['SQL could not be parsed.'])
-        s = SQLValidator ("")
+        self.assertEqual(s.errors, ["SQL could not be parsed."])
+        s = SQLValidator("")
         self.assertFalse(s.valid)
-        self.assertEqual(s.errors,['SQL could not be parsed.'])
+        self.assertEqual(s.errors, ["SQL could not be parsed."])
         s = SQLValidator("name = 'a name'; blah")
         self.assertFalse(s.valid)
-        self.assertEqual(s.errors,['SQL could not be parsed.'])
+        self.assertEqual(s.errors, ["SQL could not be parsed."])
 
     def test_column_names(self):
         s = SQLValidator("(admin IS NOT NULL and level > 4) AND height is not null")
         self.assertTrue(s.valid)
-        self.assertCountEqual(s.column_names,['height','level','admin'])
+        self.assertCountEqual(s.column_names, ["height", "level", "admin"])
 
 
 class TestOsmfilterRule(unittest.TestCase):
     def test_basic(self):
         s = OsmfilterRule("name = 'somename'")
-        self.assertEqual(s.rule(),"name=somename")
+        self.assertEqual(s.rule(), "name=somename")
         s = OsmfilterRule("level > 4")
-        self.assertEqual(s.rule(),"level>4")
+        self.assertEqual(s.rule(), "level>4")
 
     def test_basic_list(self):
         s = OsmfilterRule("name IN ('val1','val2')")
-        self.assertEqual(s.rule(),"name=val1 =val2")
+        self.assertEqual(s.rule(), "name=val1 =val2")
 
     def test_whitespace(self):
         s = OsmfilterRule("name = 'some value'")
-        self.assertEqual(s.rule(),"name=some\\ value")
+        self.assertEqual(s.rule(), "name=some\\ value")
 
     def test_notnull(self):
         s = OsmfilterRule("name is not null")
-        self.assertEqual(s.rule(),"name=*")
+        self.assertEqual(s.rule(), "name=*")
 
     def test_and_or(self):
         s = OsmfilterRule("name1 = 'foo' or name2 = 'bar'")
-        self.assertEqual(s.rule(),"( name1=foo or name2=bar )")
+        self.assertEqual(s.rule(), "( name1=foo or name2=bar )")
         s = OsmfilterRule("(name1 = 'foo' and name2 = 'bar') or name3 = 'baz'")
-        self.assertEqual(s.rule(),"( ( name1=foo and name2=bar ) or name3=baz )")
+        self.assertEqual(s.rule(), "( ( name1=foo and name2=bar ) or name3=baz )")
