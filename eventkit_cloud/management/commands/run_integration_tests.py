c040579bb698e2c52b48c39824b637b4741d3f4b
Carrie Savinell <carrielane89@gmail.com>
2019-11-26 13:22:58 -0500

    Final reformatting for management module

--- eventkit_cloud/management/commands/run_integration_tests.py
+++ eventkit_cloud/management/commands/run_integration_tests.py
@@ -2,26 +2,29 @@ import unittest
 
 from django.core.management import BaseCommand
 
-from eventkit_cloud.jobs.tests.integration_test_jobs import TestJob, load_providers, delete_providers
+from eventkit_cloud.jobs.tests.integration_test_jobs import (
+    TestJob,
+    load_providers,
+    delete_providers,
+)
 
 
 class Command(BaseCommand):
     help = "Runs all integration tests"
 
     def add_arguments(self, parser):
-        parser.add_argument('tests', nargs='*')
+        parser.add_argument("tests", nargs="*")
 
     def handle(self, *args, **options):
-        if options['tests']:
-            suite = unittest.TestLoader().loadTestsFromNames(options['tests'])
+        if options["tests"]:
+            suite = unittest.TestLoader().loadTestsFromNames(options["tests"])
         else:
-            print('Loading test providers')
+            print("Loading test providers")
             delete_providers()
             load_providers()
             suite = unittest.TestLoader().loadTestsFromTestCase(TestJob)
-            print('Removing test providers')
+            print("Removing test providers")
             delete_providers()
         result = unittest.TextTestRunner(verbosity=2).run(suite)
         if result.errors or result.failures:
             exit(1)
-
--- eventkit_cloud/management/tests/shutdown_celery_workers_test_script.py
+++ eventkit_cloud/management/tests/shutdown_celery_workers_test_script.py
@@ -1,13 +1,9 @@
-
-
 import os
+
 # from django.test import TestCase
 import sys
 
 import django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventkit_cloud.settings.prod")
-django.setup()
 
 from django.core.management import call_command
 from eventkit_cloud.tasks import test_chain
@@ -16,6 +12,9 @@ from eventkit_cloud.celery import app
 
 from celery.result import AsyncResult
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventkit_cloud.settings.prod")
+django.setup()
+
 # CELERY_ALWAYS_EAGER = False
 
 
@@ -23,7 +22,7 @@ def chain_success(chain_async_result):
     """ Checks that each task for the chain producing @chain_result completed successfully.
         Returns True if all tasks are successful, False otherwise.
     """
-    print('Waiting on chain.ready()')
+    print("Waiting on chain.ready()")
     # Make sure the chain has completed
     result = chain_async_result.get()
     msgs = []
@@ -31,7 +30,7 @@ def chain_success(chain_async_result):
     def traverse_chain_result(result):
         result_id, remaining_chain = result[0]
         link_result = AsyncResult(result_id)
-        msgs.append('{}: {}'.format(result_id, link_result.status))
+        msgs.append("{}: {}".format(result_id, link_result.status))
 
         if type(remaining_chain) == list:
             remaining_links_completed = traverse_chain_result(remaining_chain)
@@ -42,10 +41,10 @@ def chain_success(chain_async_result):
         return statuses
 
     statuses = traverse_chain_result(result)
-    if all([s == 'SUCCESS' for s in statuses]):
+    if all([s == "SUCCESS" for s in statuses]):
         return True
     else:
-        print('\n'.join(msgs))
+        print("\n".join(msgs))
         return False
 
 
@@ -55,46 +54,69 @@ def test_shutdown_celery_workers_mgmt_cmd():
     """
     # Ensure celery container is running
     if app.control.inspect().ping() is None:
-        return (100, 'No worker nodes found running prior to test')
+        return (100, "No worker nodes found running prior to test")
 
-    worker_nodename = [n for n in list(app.control.inspect().ping().keys()) if 'worker' in n][0]
-    worker_hostname = worker_nodename.split('@')[1]
+    worker_nodename = [
+        n for n in list(app.control.inspect().ping().keys()) if "worker" in n
+    ][0]
+    worker_hostname = worker_nodename.split("@")[1]
 
     # Fire off a number of tasks & wait for them to be picked up
-    tc1 = test_chain.apply_async(kwargs={'subtask_queuename':worker_hostname}, expires=6)
+    tc1 = test_chain.apply_async(
+        kwargs={"subtask_queuename": worker_hostname}, expires=6
+    )
     tc1.get()
 
     # Instruct system to finish current tasks
-    call_command('finish_worker_tasks')
+    call_command("finish_worker_tasks")
 
     # fire off more tasks
-    tc2 = test_chain.apply_async(kwargs={'subtask_queuename':worker_hostname}, expires=6)
+    tc2 = test_chain.apply_async(
+        kwargs={"subtask_queuename": worker_hostname}, expires=6
+    )
 
-    if tc1.status != 'SUCCESS':
-        return (100, 'Tasks started before "finish_worker_tasks", status "{}" != "SUCCESS"'.format(tc1.status))
+    if tc1.status != "SUCCESS":
+        return (
+            100,
+            'Tasks started before "finish_worker_tasks", status "{}" != "SUCCESS"'.format(
+                tc1.status
+            ),
+        )
     if not chain_success(tc1):
-        return (100, 'Tasks started before "finish_worker_tasks", did not all complete successfully')
-    if tc2.status != 'PENDING':
-        return (100, 'Tasks started after "finish_worker_tasks", status {} != "PENDING"'.format(tc2.status))
+        return (
+            100,
+            'Tasks started before "finish_worker_tasks", did not all complete successfully',
+        )
+    if tc2.status != "PENDING":
+        return (
+            100,
+            'Tasks started after "finish_worker_tasks", status {} != "PENDING"'.format(
+                tc2.status
+            ),
+        )
 
     # Invoke shutdown_celery_workers
-    call_command('shutdown_celery_workers')
+    call_command("shutdown_celery_workers")
 
     # Check that the celery workers are all stopped
     if app.control.inspect().ping() is not None:
-        return (100, 'Worker nodes still running after test')
+        return (100, "Worker nodes still running after test")
 
     # There should be one test_chain task on the celery queue that's left after "finish_worker_tasks" call.
     n_purged_tasks = app.control.purge()
     if n_purged_tasks != 1:
-        return (100, 'Number of pending tasks purged, {} != 4'.format(n_purged_tasks))
+        return (100, "Number of pending tasks purged, {} != 4".format(n_purged_tasks))
 
-    return (0, 'Ok')
+    return (0, "Ok")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     n_preexisting_tasks = app.control.purge()
     if n_preexisting_tasks != 0:
-        return_code, msg = (100, 'Found {} preexisting tasks, please clean up queue before executing this test')
+        return_code, msg = (
+            100,
+            "Found {} preexisting tasks, please clean up queue before executing this test",
+        )
     else:
         return_code, msg = test_shutdown_celery_workers_mgmt_cmd()
     print(msg)
