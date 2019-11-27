a6c47a913f9c74e7e5e7c32840c076adae85edf7
Carrie Savinell <carrielane89@gmail.com>
2019-11-27 13:47:32 -0500

    Reformatting settings app with black ONLY -passing tests

--- eventkit_cloud/settings/utils.py
+++ eventkit_cloud/settings/utils.py
@@ -5,9 +5,8 @@ import os
 
 # Absolute filesystem path to the Django project directory:
 DJANGO_ROOT = os.path.dirname(
-    os.path.dirname(
-        os.path.dirname(os.path.abspath(__file__))
-    ))
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+)
 
 
 def ABS_PATH(*args):
@@ -17,14 +16,19 @@ def ABS_PATH(*args):
 def ensure_secret_key_file():
     """Checks that secret.py exists in settings dir. If not, creates one
 with a random generated SECRET_KEY setting."""
-    secret_path = ABS_PATH('eventkit_cloud', 'settings', 'secret.py')
+    secret_path = ABS_PATH("eventkit_cloud", "settings", "secret.py")
     if not os.path.exists(secret_path):
         from django.utils.crypto import get_random_string
+
         secret_key = get_random_string(
-            50, 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)')
-        with open(secret_path, 'w') as f:
+            50, "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"
+        )
+        with open(secret_path, "w") as f:
             f.write("import os\n")
-            f.write("SECRET_KEY = os.environ.get('SECRET_KEY', " + repr(secret_key) + "\n")
+            f.write(
+                "SECRET_KEY = os.environ.get('SECRET_KEY', " + repr(secret_key) + "\n"
+            )
+
 
 # Import the secret key
 ensure_secret_key_file()
