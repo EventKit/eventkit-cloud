1bb34dad9a92c7a5748e1d5fa8da43fe556d3f97
CarrieSav <36865861+CSavi@users.noreply.github.com>
2019-11-27 19:20:47 -0500

    Settings reformats (#767)
    * Reformatting settings app with black ONLY -passing tests
    
    * Reformatting settings app wtih flake8 --passing tests

--- eventkit_cloud/settings/celery.py
+++ eventkit_cloud/settings/celery.py
@@ -4,7 +4,6 @@ import os
 import socket
 
 from celery.schedules import crontab
-from kombu import Exchange, Queue
 
 from eventkit_cloud.celery import app
 from eventkit_cloud.settings.contrib import *  # NOQA
@@ -23,8 +22,8 @@ CELERY_TRACK_STARTED = True
 # CELERY_CHORD_PROPAGATES = False
 
 CELERYD_PREFETCH_MULTIPLIER = 1
-CELERYBEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
-CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'django-db')
+CELERYBEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "django-db")
 
 # Pickle used to be the default, and accepting pickled content is a security concern.  Using the new default json,
 # causes a circular reference error, that will need to be resolved.
@@ -33,36 +32,42 @@ CELERY_ACCEPT_CONTENT = ["json"]
 # configure periodic task
 
 BEAT_SCHEDULE = {
-    'expire-runs': {
-        'task': 'Expire Runs',
-        'schedule': crontab(minute='0', hour='0', day_of_week='*')
+    "expire-runs": {
+        "task": "Expire Runs",
+        "schedule": crontab(minute="0", hour="0", day_of_week="*"),
     },
-    'provider-statuses': {
-        'task': 'Check Provider Availability',
-        'schedule': crontab(minute='*/{}'.format(os.getenv('PROVIDER_CHECK_INTERVAL', '30')))
+    "provider-statuses": {
+        "task": "Check Provider Availability",
+        "schedule": crontab(
+            minute="*/{}".format(os.getenv("PROVIDER_CHECK_INTERVAL", "30"))
+        ),
     },
-    'clean-up-queues': {
-        'task': 'Clean Up Queues',
-        'schedule': crontab(minute='0', hour='0', day_of_week='*')
+    "clean-up-queues": {
+        "task": "Clean Up Queues",
+        "schedule": crontab(minute="0", hour="0", day_of_week="*"),
     },
 }
 
 PCF_SCALING = os.getenv("PCF_SCALING", False)
 if PCF_SCALING:
-    BEAT_SCHEDULE.update({
-        'pcf-scale-celery': {
-            'task': 'PCF Scale Celery',
-            'schedule': 60.0,
-            'kwargs' : {"max_instances": int(os.getenv("CELERY_INSTANCES", 3))},
-            'options': {'priority': 90,
-                        'queue': "scale".format(socket.gethostname()),
-                        'routing_key': "scale".format(socket.gethostname())}
-        },
-    })
+    BEAT_SCHEDULE.update(
+        {
+            "pcf-scale-celery": {
+                "task": "PCF Scale Celery",
+                "schedule": 60.0,
+                "kwargs": {"max_instances": int(os.getenv("CELERY_INSTANCES", 3))},
+                "options": {
+                    "priority": 90,
+                    "queue": "scale".format(socket.gethostname()),
+                    "routing_key": "scale".format(socket.gethostname()),
+                },
+            },
+        }
+    )
 
 app.conf.beat_schedule = BEAT_SCHEDULE
 
-CELERYD_USER = CELERYD_GROUP = 'eventkit'
+CELERYD_USER = CELERYD_GROUP = "eventkit"
 if os.getenv("VCAP_SERVICES"):
     CELERYD_USER = CELERYD_GROUP = "vcap"
 CELERYD_USER = os.getenv("CELERYD_USER", CELERYD_USER)
@@ -72,28 +77,30 @@ BROKER_URL = None
 if os.getenv("VCAP_SERVICES"):
     for service, listings in json.loads(os.getenv("VCAP_SERVICES")).items():
         try:
-            if 'rabbitmq' in service:
-                BROKER_URL = listings[0]['credentials']['protocols']['amqp']['uri']
-            if 'cloudamqp' in service:
-                BROKER_URL = listings[0]['credentials']['uri']
+            if "rabbitmq" in service:
+                BROKER_URL = listings[0]["credentials"]["protocols"]["amqp"]["uri"]
+            if "cloudamqp" in service:
+                BROKER_URL = listings[0]["credentials"]["uri"]
         except KeyError as TypeError:
             continue
         if BROKER_URL:
             break
 if not BROKER_URL:
-    BROKER_URL = os.environ.get('BROKER_URL', 'amqp://guest:guest@localhost:5672//')
+    BROKER_URL = os.environ.get("BROKER_URL", "amqp://guest:guest@localhost:5672//")
 
 BROKER_API_URL = None
 if os.getenv("VCAP_SERVICES"):
     for service, listings in json.loads(os.getenv("VCAP_SERVICES")).items():
         try:
-            if 'rabbitmq' in service:
-                BROKER_API_URL = listings[0]['credentials']['http_api_uri']
-            if 'cloudamqp' in service:
-                BROKER_API_URL = listings[0]['credentials']['http_api_uri']
+            if "rabbitmq" in service:
+                BROKER_API_URL = listings[0]["credentials"]["http_api_uri"]
+            if "cloudamqp" in service:
+                BROKER_API_URL = listings[0]["credentials"]["http_api_uri"]
         except KeyError as TypeError:
             continue
         if BROKER_API_URL:
             break
 if not BROKER_API_URL:
-    BROKER_API_URL = os.environ.get('BROKER_API_URL', 'http://guest:guest@localhost:15672/api/')
+    BROKER_API_URL = os.environ.get(
+        "BROKER_API_URL", "http://guest:guest@localhost:15672/api/"
+    )
--- eventkit_cloud/settings/contrib.py
+++ eventkit_cloud/settings/contrib.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
-
 import os
 
 from eventkit_cloud.settings.base import *  # NOQA
+from eventkit_cloud.settings.base import INSTALLED_APPS
 
 # Extra installed apps
 INSTALLED_APPS += (
     # any 3rd party apps
-    'rest_framework',
-    'rest_framework_gis',
-    'rest_framework.authtoken',
+    "rest_framework",
+    "rest_framework_gis",
+    "rest_framework.authtoken",
     # 'rest_framework_swagger',
     # 'social.apps.django_app.default'
 )
@@ -19,26 +19,34 @@ INSTALLED_APPS += (
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',
-                                'rest_framework.filters.SearchFilter',
-                                'rest_framework.filters.OrderingFilter'),
-    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.SessionAuthentication',
-                                       'rest_framework.authentication.TokenAuthentication'),
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-        'eventkit_cloud.api.renderers.HOTExportApiRenderer',
+    "DEFAULT_FILTER_BACKENDS": (
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
     ),
-    'EXCEPTION_HANDLER': 'eventkit_cloud.api.utils.eventkit_exception_handler',
-    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.AcceptHeaderVersioning',
-    'DEFAULT_VERSION': '1.0',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
+        "eventkit_cloud.api.renderers.HOTExportApiRenderer",
+    ),
+    "EXCEPTION_HANDLER": "eventkit_cloud.api.utils.eventkit_exception_handler",
+    "DEFAULT_VERSIONING_CLASS": "rest_framework.versioning.AcceptHeaderVersioning",
+    "DEFAULT_VERSION": "1.0",
 }
 
 
 SWAGGER_SETTINGS = {
-    'LOGIN_URL': 'rest_framework:login',
-    'LOGOUT_URL': 'rest_framework:logout',
-    'JSON_EDITOR': False if os.getenv('SWAGGER_JSON_EDITOR', 'False').lower() == 'false' else True,
-    'SHOW_REQUEST_HEADERS': False if os.getenv('SWAGGER_SHOW_REQUEST_HEADERS', 'False').lower() == 'false' else True,
-    'VALIDATOR_URL': os.getenv('SWAGGER_VALIDATOR_URL', None)
+    "LOGIN_URL": "rest_framework:login",
+    "LOGOUT_URL": "rest_framework:logout",
+    "JSON_EDITOR": False
+    if os.getenv("SWAGGER_JSON_EDITOR", "False").lower() == "false"
+    else True,
+    "SHOW_REQUEST_HEADERS": False
+    if os.getenv("SWAGGER_SHOW_REQUEST_HEADERS", "False").lower() == "false"
+    else True,
+    "VALIDATOR_URL": os.getenv("SWAGGER_VALIDATOR_URL", None),
 }
