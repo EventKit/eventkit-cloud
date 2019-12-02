42f2bbcf6c81488357601b5ba3040804ddbf824c
Carrie Savinell <carrielane89@gmail.com>
2019-12-02 10:23:03 -0500

    Adding ready() and used imports for jobs/apps.py

--- eventkit_cloud/jobs/apps.py
+++ eventkit_cloud/jobs/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class EventKitJobs(AppConfig):
     name = "eventkit_cloud.jobs"
     verbose_name = "Eventkit-Cloud Jobs"
+
+    def ready(self):
+        from eventkit_cloud.jobs.signals import user_post_save, job_post_save # NOQA
