7dbb030abcfdc427105d5aec3ce5a7deeace8d2d
Coop56 <JCooper@jeremiahcooper.com>
2019-12-05 01:14:11 -0500

    Increase memory per task.

--- eventkit_cloud/tasks/scheduled_tasks.py
+++ eventkit_cloud/tasks/scheduled_tasks.py
@@ -89,22 +89,22 @@ def pcf_scale_celery(max_tasks_memory):
         celery_group_name: {
             "command": "celery worker -A eventkit_cloud --concurrency=$CONCURRENCY --loglevel=$LOG_LEVEL -n worker@%h -Q $CELERY_GROUP_NAME ",
             "disk": 4096,
-            "memory": 250
+            "memory": 512
         },
         "celery": {
             "command": "celery worker -A eventkit_cloud --loglevel=$LOG_LEVEL -n celery@%h -Q celery ",
             "disk": 4096,
-            "memory": 250
+            "memory": 512
         },
         f"{celery_group_name}.osm": {
             "command": "celery worker -A eventkit_cloud --concurrency=1 --loglevel=$LOG_LEVEL -n osm@%h -Q $CELERY_GROUP_NAME.osm ",
             "disk": 4096,
-            "memory": 2000
+            "memory": 2048
         },
         f"{celery_group_name}.finalize": {
             "command": "celery worker -A eventkit_cloud --concurrency=2 -n finalize@%h -Q $CELERY_GROUP_NAME.finalize ",
             "disk": 4096,
-            "memory": 250
+            "memory": 512
         }
     }
     cancel_queue_command = "& exec celery worker -A eventkit_cloud --loglevel=$LOG_LEVEL -n cancel@%h -Q $HOSTNAME.cancel"
