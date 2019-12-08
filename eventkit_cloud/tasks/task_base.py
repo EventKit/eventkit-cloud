50ae634f34ec094a6f5bba00722443ec66294c59
Coop56 <JCooper@jeremiahcooper.com>
2019-12-08 13:36:33 -0500

    Add additional logging.

--- eventkit_cloud/tasks/task_base.py
+++ eventkit_cloud/tasks/task_base.py
@@ -1,6 +1,10 @@
 import os
 
 from audit_logging.celery_support import UserDetailsBase
+from celery.utils.log import get_task_logger
+
+# Get an instance of a logger
+logger = get_task_logger(__name__)
 
 
 class EventKitBaseTask(UserDetailsBase):
@@ -13,5 +17,5 @@ class EventKitBaseTask(UserDetailsBase):
 
             # In our current setup the queue name always mirrors the routing_key, if this changes this logic will break.
             queue_name = self.request.delivery_info["routing_key"]
-
+            logger.info(f"{self} has completed, sending pcf_shutdown_celery_workers task.")
             pcf_shutdown_celery_workers.s(queue_name, queue_type, hostname).apply_async(queue="scale")
