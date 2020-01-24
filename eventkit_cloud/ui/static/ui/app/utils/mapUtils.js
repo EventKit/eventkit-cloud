50e3a38a3d6d1fd4d94e713c8b387f411492c237
Caboose71 <50183410+Caboose71@users.noreply.github.com>
2020-01-24 10:01:13 -0500

    Display Query API data on create page. (#799)
    * bumped mapproxy, removed patch, fix pathing
    
    * adds handling for arcgis queries
    
    * removed unused log and comments
    
    * clean up geojson conversion
    
    * black fixes
    
    * Creating DisplayDataBox component and rendering it in the ExportAOI
    
    * Moving DisplayDataBox to render inside MapDrawer
    
    * fixed geojson properties and tests
    
    * Reorder MapDrawer display box positioning
    
    * Adding MaterialUi's Card component to DisplayDataBox
    
    * Resolving prop issues
    
    * Adding a close button to display box
    
    * Reconcile conflicts and allow map to be clicked
    
    * Resolving merge conflicts
    
    * Resolving more merge conflicts
    
    * Adding logic to onClick function for closing the display box
    
    * Moving DisplayDataBox to be rendered in MapQueryDisplay
    
    * Revising styles for DisplayDataBox
    
    * Pull data from Mock JSON
    
    * Adding IconButton and CloseButton for DisplayDataBox
    
    * Adding initial test setup for DisplayDataBox
    
    * Adding Grid Component and extra styling for DisplayDataBox
    
    * Removing unused class
    
    * Revising styles
    
    * Positioned icon button to be top left of display box
    
    * Adding more styling for display data box
    
    * Modifying date key
    
    * pin django version to prevent breaking API
    
    * update content-length
    
    * Begin addition of query creation
    
    * Complete Query logic
    
    * Modify display box test props
    
    * Update initial zoom on Export AOI map view
    
    * Pull all properties from feature response
    
    * Fixing issues from merge and modifying styles for DisplayDataBox
    
    * formatting fix
    
    * Complete query box
    
    * Rename DisplayBox to QueryBox
    
    * Add tests
    
    * Remove bad import
    
    * Remove added conda files
    
    * Re-add original conda files
    
    * Correct ExportAOI code for tests
    
    Co-authored-by: Joseph Svrcek <joseph.svrcek@rgi-corp.com>
    Co-authored-by: CarrieSav <36865861+CSavi@users.noreply.github.com>
    Co-authored-by: Jeremiah Cooper <JCooper@jeremiahcooper.com>

--- eventkit_cloud/ui/static/ui/app/utils/mapUtils.js
+++ eventkit_cloud/ui/static/ui/app/utils/mapUtils.js
@@ -369,6 +369,19 @@ export function featureToPoint(feature) {
     return new Point(center);
 }
 
+export function wrapX(tileGrid, tileCoord) {
+    const z = tileCoord[0];
+    const center = tileGrid.getTileCoordCenter(tileCoord);
+    const projectionExtent = tileGrid.getExtent();
+    if (!extent.containsCoordinate(projectionExtent, center)) {
+        const worldWidth = extent.getWidth(projectionExtent);
+        const worldsAway = Math.ceil((projectionExtent[0] - center[0]) / worldWidth);
+        center[0] += worldWidth * worldsAway;
+        return tileGrid.getTileCoordForCoordAndZ(center, z);
+    }
+    return tileCoord;
+}
+
 // if any coordinates are wrapped adjust them to be within the projection extent
 export function unwrapCoordinates(coords, projection) {
     // based on:
