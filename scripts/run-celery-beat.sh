03ddf804a8c51b6ee1fc9103dc893fb75268f200
Jeremiah Cooper <JCooper@prominentedge.com>
2020-01-30 11:02:21 -0500

    Resolve Linux Permission Issues & Add Makefile Commands (#800)
    * Setup webpack and mkdocs to run as the eventkit user instead of root.
    
    * Run mkdocs as root.
    
    * Add LAND_DATA_URL to environment variables.
    
    * Add additional helpful commands to Makefile.
    
    * Additional Makefile commands.
    
    * Add Windows compatability to the Makefile.
    
    * Fix Makefile Windows compatability.
    
    * Remove Makefile debug statement.
    
    * Add additonal OS checking.
    
    * Ensure celerybeat.pid is removed so that celery-beat can startup.
    
    * Create eventkit user on Linux machines if it doesn't already exist, update clean command to only target eventkit volumes.
    
    * Add user and group creation to the Makefile for Linux.

--- scripts/run-celery-beat.sh
+++ scripts/run-celery-beat.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+rm -rf celerybeat.pid & \
 celery beat -A eventkit_cloud --loglevel=$LOG_LEVEL & \
 celery worker -A eventkit_cloud --concurrency=1 --loglevel=$LOG_LEVEL -n scale@%h -Q scale
