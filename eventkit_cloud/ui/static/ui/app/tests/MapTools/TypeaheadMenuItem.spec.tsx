aeb79975c11a48ef162d7b23dcbf12576f933c21
Joseph Pritchett <joseph.pritchett@rgi-corp.com>
2020-03-03 16:26:00 -0500

    Adjust tests for typeahead; remove function test, leave side effect test - createDescription

--- eventkit_cloud/ui/static/ui/app/tests/MapTools/TypeaheadMenuItem.spec.tsx
+++ eventkit_cloud/ui/static/ui/app/tests/MapTools/TypeaheadMenuItem.spec.tsx
@@ -16,78 +16,37 @@ describe('TypeaheadMenuItem component', () => {
     const getProps = () => ({
         result: {},
         index: 1,
-        ...global.eventkit_test_props,
-    });
-
-    const getContext = () => ({
-        activeIndex: -1,
-        onActiveItemChange: () => {},
-        onInitialItemChange: () => {},
-        onMenuItemClick: () => {},
+        classes: {},
+        ...(global as any).eventkit_test_props,
     });
 
     it('should return a MenuItem with proper child components', () => {
         const props = getProps();
-        const context = getContext();
-        const wrapper = shallow(<TypeaheadMenuItem {...props} />, {
-            context,
-            childContextTypes: {
-                activeIndex: PropTypes.number.isRequired,
-                onActiveItemChange: PropTypes.func.isRequired,
-                onInitialItemChange: PropTypes.func.isRequired,
-                onMenuItemClick: PropTypes.func.isRequired,
-            },
-        });
-        expect(wrapper.find('div')).toHaveLength(6);
+        const wrapper = shallow(<TypeaheadMenuItem {...props} />);
+        expect(wrapper.find('div')).toHaveLength(5);
         expect(wrapper.find('.menuItem')).toHaveLength(1);
         expect(wrapper.find('.qa-TypeaheadMenuItem-icon-div')).toHaveLength(1);
         expect(wrapper.find(ActionRoom)).toHaveLength(0);
         expect(wrapper.find('.qa-TypeaheadMenuItem-name')).toHaveLength(1);
         expect(wrapper.find('.qa-TypeaheadMenuItem-description')).toHaveLength(1);
-        expect(wrapper.find('.qa-TypeaheadMenuItem-source')).toHaveLength(1);
+        expect(wrapper.find('.qa-TypeaheadMenuItem-source')).toHaveLength(0);
     });
 
-    it('createDescription should return the proper description', () => {
-        const result = {
-            name: 'test name',
-            province: 'province',
-            region: 'region',
-            country: 'country name',
-        };
+    it('should return a MenuItem with proper child components when source is present', () => {
         const props = getProps();
-        const context = getContext();
-        const wrapper = shallow(<TypeaheadMenuItem {...props} />, {
-            context,
-            childContextTypes: {
-                activeIndex: PropTypes.number.isRequired,
-                onActiveItemChange: PropTypes.func.isRequired,
-                onInitialItemChange: PropTypes.func.isRequired,
-                onMenuItemClick: PropTypes.func.isRequired,
-                muiTheme: PropTypes.object,
-            },
-        });
-        const description = wrapper.instance().createDescription(result);
-        expect(description).toEqual('province, region, country name');
+        const wrapper = shallow(<TypeaheadMenuItem {...props} {...{result: { source: 'source'}}} />);
+        expect(wrapper.find('div')).toHaveLength(6);
+        expect(wrapper.find('.qa-TypeaheadMenuItem-source')).toHaveLength(1);
     });
 
     it('should have the proper text and icon', () => {
         const props = getProps();
-        const context = getContext();
         props.result = { geometry: { type: 'Polygon' } };
         props.result.name = 'test name';
         props.result.province = 'province';
         props.result.region = 'region';
         props.result.country = 'country name';
-        const wrapper = shallow(<TypeaheadMenuItem {...props} />, {
-            context,
-            childContextTypes: {
-                activeIndex: PropTypes.number.isRequired,
-                onActiveItemChange: PropTypes.func.isRequired,
-                onInitialItemChange: PropTypes.func.isRequired,
-                onMenuItemClick: PropTypes.func.isRequired,
-                muiTheme: PropTypes.object,
-            },
-        });
+        const wrapper = shallow(<TypeaheadMenuItem {...props} />);
         expect(wrapper.find(IrregularPolygon)).toHaveLength(1);
         expect(wrapper.find('.qa-TypeaheadMenuItem-name').text()).toEqual('test name');
         expect(wrapper.find('.qa-TypeaheadMenuItem-description').text()).toEqual('province, region, country name');
