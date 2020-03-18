0bd9d52280323f8516533021d5fd3818ded128fa
Carrie Savinell <carrielane89@gmail.com>
2020-03-18 18:55:28 -0400

    Revising geocode authentication to pass a cert param in the request headers and adding tests

--- eventkit_cloud/utils/geocoding/geocode_auth.py
+++ eventkit_cloud/utils/geocoding/geocode_auth.py
@@ -1,7 +1,6 @@
 import logging
 import tempfile
 import traceback
-
 import requests
 from django.conf import settings
 from django.core.cache import cache
@@ -12,17 +11,20 @@ CACHE_TOKEN_TIMEOUT = 60 * 5  # 5 Minutes
 CACHE_TOKEN_KEY = "pelias_token"
 
 
-# def get_auth_headers():
-#     headers = {}
-#     if getattr(settings, "GEOCODING_AUTH_URL") is not None:
-#         token = cache.get(CACHE_TOKEN_KEY) or authenticate()
-#         if token:
-#             headers = {"Authorization": "Bearer " + str(token)}
-#     return headers
+def get_auth_headers():
+    headers = {}
+    if getattr(settings, "GEOCODING_AUTH_URL") is not None:
+        headers = cache.get(CACHE_TOKEN_KEY) or authenticate()
+    return headers
+    logger.info(f'END OF HEADERS: {headers}')
 
+
+# should not be hitting authenticate if headers are there -just for the first time
 
 def authenticate():
     try:
+        logger.info("Receiving new authentication token for geocoder")
+        logger.info(f'HITTING AUTHENTICATE HERE')
         cert = getattr(settings, "GEOCODING_AUTH_CERT", "")
         with tempfile.NamedTemporaryFile(suffix=".crt", delete=False) as temp_file:
             temp_file.write(cert.replace("\\n", "\n").encode())
@@ -33,20 +35,23 @@ def authenticate():
             verify = getattr(settings, "SSL_VERIFICATION", True)
             url = getattr(settings, "GEOCODING_AUTH_URL")
 
-            s = requests.session()
-            response = s.post(
-                url,
-                data=temp_file.name,
-                verify=verify
-            ).json()
-            print(response)
+            logger.info(f'PUBLIC CERT: {public_cert}')
+            logger.info(f'URL: {url}')
 
-            token = response.get("token")
-            cache.set(CACHE_TOKEN_KEY, token, CACHE_TOKEN_TIMEOUT)
-            return token
+            response = requests.get(
+                url=url,
+                verify=verify,
+                cert=temp_file.name,
+                headers={"SSL_CLIENT_CERT": public_cert},
+            )
+            headers = response.headers
+            logger.info(f'HEADERS!: {headers}')
 
+            my_cache = cache.set(CACHE_TOKEN_KEY, headers, CACHE_TOKEN_TIMEOUT)
+            logger.info(f'MORE HEADERS!: {headers}')
+            return headers
 
     except requests.exceptions.RequestException as e:
         logger.error(traceback.print_exc())
-        # cache.delete(CACHE_TOKEN_KEY)
+        cache.delete(CACHE_TOKEN_KEY)
         return None
--- eventkit_cloud/utils/tests/test_geocode_auth.py
+++ eventkit_cloud/utils/tests/test_geocode_auth.py
@@ -1,36 +1,51 @@
 import json
 import logging
+from unittest import mock
 
-import requests
 import requests_mock
+import requests
 from django.conf import settings
 from django.test import TestCase, override_settings
+from mock import patch
 
+from eventkit_cloud.utils.geocoding.geocode_auth import get_auth_headers, authenticate, CACHE_TOKEN_KEY, \
+    CACHE_TOKEN_TIMEOUT
 
 logger = logging.getLogger(__name__)
 
 
-# class TestGeoCodeAuth(TestCase):
-#
-#     def setUp(self):
-#         self.session = requests.Session()
-#         self.mock_requests = requests_mock.Mocker()
-#         self.mock_requests.start()
-#         self.addCleanup(self.mock_requests.stop)
-#
-#     @override_settings(GEOCODING_AUTH_URL="http://fake.url/",
-#                        GEOCODING_AUTH_CERT="-----BEGIN CERTIFICATE----------END CERTIFICATE----------BEGIN RSA "
-#                                            "PRIVATE KEY----------END RSA PRIVATE KEY-----")
-#     def test_authenticate(self):
-#         session = requests.session()
-#         with requests_mock.mock() as m:
-#             m.get(url=settings.GEOCODING_AUTH_URL, text=settings.GEOCODING_AUTH_CERT)
-#             response = session.request('POST', url=settings.GEOCODING_AUTH_URL)
-#         self.assertEqual(response.body, "-----BEGIN CERTIFICATE----------END CERTIFICATE----------BEGIN RSA "
-#                                         "PRIVATE KEY----------END RSA PRIVATE KEY-----")
-#
-#
-#         # session.request('POST', url=settings.GEOCODING_AUTH_URL, data=json.dumps(settings.GEOCODING_AUTH_CERT))
-#
-#         # self.assertEqual(authenticate(), {'data': settings.GEOCODING_AUTH_CERT})
-#         # self.assertEqual(authenticate(), {'Authorization': session['data']})
+class TestGeoCodeAuth(TestCase):
+
+    def setUp(self):
+        self.mock_requests = requests_mock.Mocker()
+        self.mock_requests.start()
+        self.addCleanup(self.mock_requests.stop)
+
+    @patch('eventkit_cloud.utils.geocoding.geocode_auth.cache')
+    @override_settings(GEOCODING_AUTH_URL="http://fake.url/",
+                       GEOCODING_AUTH_CERT="-----BEGIN CERTIFICATE----------END CERTIFICATE----------BEGIN RSA "
+                                           "PRIVATE KEY----------END RSA PRIVATE KEY-----"
+                       )
+    def test_get_headers(self, cache_mock):
+        test_jwt = {"token": "hello_world"}
+        cache_mock.get.return_value = test_jwt
+
+        self.mock_requests.get(settings.GEOCODING_AUTH_URL, headers=test_jwt, status_code=200)
+        self.assertEqual(get_auth_headers(), test_jwt)
+
+    @patch('eventkit_cloud.utils.geocoding.geocode_auth.cache')
+    @override_settings(GEOCODING_AUTH_URL="http://fake_search.url/",
+                       GEOCODING_AUTH_CERT="-----BEGIN CERTIFICATE----------END CERTIFICATE----------BEGIN RSA "
+                                           "PRIVATE KEY----------END RSA PRIVATE KEY-----"
+                       )
+    def test_authenticate(self, cache_mock):
+        expected_headers = {"jsessionid": "session id"}
+
+        self.mock_requests.register_uri(
+            'GET',
+            settings.GEOCODING_AUTH_URL,
+            headers=expected_headers,
+            status_code=200
+        )
+        self.assertEqual(authenticate(), expected_headers)
+        cache_mock.set.assert_called_with(CACHE_TOKEN_KEY, expected_headers, CACHE_TOKEN_TIMEOUT)
