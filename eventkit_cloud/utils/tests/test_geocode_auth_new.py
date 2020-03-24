1380c8f8cb88cb8d0cf43baea8f5acb6845a0d6e
Carrie Savinell <carrielane89@gmail.com>
2020-03-24 08:49:21 -0400

    Testing for test failure bug

---
+++ eventkit_cloud/utils/tests/test_geocode_auth_new.py
@@ -0,0 +1,30 @@
+import logging
+import requests_mock
+from django.core.cache import cache
+from django.test import TestCase
+from mock import patch
+from eventkit_cloud.utils.geocoding.geocode_auth import get_auth_headers
+
+logger = logging.getLogger(__name__)
+
+
+class TestGeoCodeAuth(TestCase):
+
+    def setup(self):
+        self.mock_requests = requests_mock.Mocker()
+        self.mock_requests.start()
+        self.addCleanup(self.mock_requests.stop)
+
+    def test_get_headers_with_no_URL(self):
+        self.assertEqual(get_auth_headers(), {})
+
+    def test_get_headers_with_cache(self):
+        cacheValue = 'this_was_in_cache'
+        cache.set('pelias_token', cacheValue, None)
+        self.assertEqual(get_auth_headers(), {'Authorization': 'Bearer ' + cacheValue})
+
+    @patch('eventkit_cloud.utils.geocoding.geocode_auth.authenticate')
+    def test_get_headers_with_authenticate(self, authenticate_mock):
+        token = "some token"
+        authenticate_mock.return_value = token
+        self.assertEqual(get_auth_headers(), {'Authorization': 'Bearer ' + token})
