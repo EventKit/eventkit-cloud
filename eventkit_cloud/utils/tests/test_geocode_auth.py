edf52ab7edaacfc41844b05f5ca7674bc72d9e52
Carrie Savinell <carrielane89@gmail.com>
2020-03-24 16:15:01 -0400

    Adding dummy test value to GEOCODING_AUTH_URL

--- eventkit_cloud/utils/tests/test_geocode_auth.py
+++ eventkit_cloud/utils/tests/test_geocode_auth.py
@@ -1,7 +1,7 @@
 import logging
 import requests_mock
 from django.core.cache import cache
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from mock import patch
 from eventkit_cloud.utils.geocoding.geocode_auth import get_auth_headers
 
@@ -18,11 +18,13 @@ class TestGeoCodeAuth(TestCase):
     def test_get_headers_with_no_URL(self):
         self.assertEqual(get_auth_headers(), {})
 
+    @override_settings(GEOCODING_AUTH_URL="http://fake.url")
     def test_get_headers_with_cache(self):
         cacheValue = 'this_was_in_cache'
         cache.set('pelias_token', cacheValue, None)
         self.assertEqual(get_auth_headers(), {'Authorization': 'Bearer ' + cacheValue})
 
+    @override_settings(GEOCODING_AUTH_URL="http://fake.url")
     @patch('eventkit_cloud.utils.geocoding.geocode_auth.authenticate')
     def test_get_headers_with_authenticate(self, authenticate_mock):
         token = "some token"
