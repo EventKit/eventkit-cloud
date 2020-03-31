843446117127393d90012cfa0f5cc151fea73935
CarrieSav <36865861+CSavi@users.noreply.github.com>
2020-03-31 13:14:05 -0400

    Geocode auth for pelias (#843)
    * Ensuring security by caching sessions in place of token within the headers for geocode auth
    
    * Testing authentication logic
    
    * Adding settings env variables for pelias in docker-compose
    
    * Updating geocoding authentication via certificate not token
    
    * Revising geocode authentication to pass a cert param in the request headers and adding tests
    
    * Changing geocode authorization to store session as cookie instead of header token and changing tests
    
    * Removing redundant variable in docker-compose
    
    * Revising geocode authentication to be cookie, not token-based
    
    * Resolving internal error issues by checking the json of a response and fixing other bugs
    
    * Fixing broken test
    
    * Adding/cleaning up tests
    
    * Reformatting using black
    
    * Revising status_code for geocode auth
    
    * Revising bad response for geocode auth
    
    * Testing for test failure bug
    
    * Removing test file
    
    * Adding dummy test value to GEOCODING_AUTH_URL
    
    * fixed cache logic, added tests
    
    * simplified geocode auth response, added tests
    
    * lint fixes
    
    * fix syntax error
    
    * don't cache headers
    
    * fix convert environment variable
    
    Co-authored-by: jsvrcek <joseph.svrcek@rgi-corp.com>
    Co-authored-by: Joseph Svrcek <joesvrcek@hotmail.com>

--- eventkit_cloud/utils/tests/test_geocode_auth.py
+++ eventkit_cloud/utils/tests/test_geocode_auth.py
@@ -1,34 +1,74 @@
-import json
 import logging
-
-import requests_mock
+import requests
 from django.conf import settings
-from django.core.cache import cache
 from django.test import TestCase, override_settings
-
-from eventkit_cloud.utils.geocoding.geocode_auth import get_auth_headers
+from mock import patch
+from eventkit_cloud.utils.geocoding.geocode_auth import get_auth_headers, CACHE_COOKIE_KEY, CACHE_TOKEN_KEY, \
+    CACHE_TOKEN_TIMEOUT, update_session_cookies, update_auth_headers, get_session_cookies, authenticate
 
 logger = logging.getLogger(__name__)
 
 
 class TestGeoCodeAuth(TestCase):
 
-    def setUp(self):
-        self.mock_requests = requests_mock.Mocker()
-        self.mock_requests.start()
-        self.addCleanup(self.mock_requests.stop)
+    @override_settings(GEOCODING_AUTH_URL="http://fake.url")
+    @patch('eventkit_cloud.utils.geocoding.geocode_auth.update_auth_headers')
+    @patch('eventkit_cloud.utils.geocoding.geocode_auth.cache')
+    @patch('eventkit_cloud.utils.geocoding.geocode_auth.authenticate')
+    def test_get_auth_headers(self, mock_authenticate, mock_cache, mock_update_auth_headers):
+        mock_cache.get.return_value = settings.GEOCODING_AUTH_URL
+        self.assertIsNone(get_auth_headers())
+        mock_cache.reset_mock()
 
-    def testGetHeadersWithNoURL(self):        
-        self.assertEqual(get_auth_headers(), {})
+        mock_cache.get.return_value = {}
+        example_token = "test_token"
+        mock_authenticate.return_value = example_token
+        expected_header = {"Authorization": "Bearer " + str(example_token)}
+        self.assertEquals(expected_header, get_auth_headers())
+        mock_update_auth_headers.assert_called_once_with(expected_header)
+        mock_cache.get.assert_called_once_with(CACHE_TOKEN_KEY, {})
 
-    @override_settings(GEOCODING_AUTH_URL="http://fake.url/", GEOCODING_AUTH_CERT="-----BEGIN CERTIFICATE----------END CERTIFICATE----------BEGIN RSA PRIVATE KEY----------END RSA PRIVATE KEY-----")
-    def testGetHeaders(self):
-        testJwt = {'token': 'hello_world'}
-        self.mock_requests.get(settings.GEOCODING_AUTH_URL, text=json.dumps(testJwt), status_code=200)
-        self.assertEqual(get_auth_headers(), {'Authorization': 'Bearer ' + testJwt['token']})
+    @patch('eventkit_cloud.utils.geocoding.geocode_auth.cache')
+    def test_update_auth_headers(self, mock_cache):
+        example_headers = "test_headers"
+        update_auth_headers(example_headers)
+        mock_cache.set.assert_called_once_with(CACHE_TOKEN_KEY, example_headers, CACHE_TOKEN_TIMEOUT)
 
-    @override_settings(GEOCODING_AUTH_URL="http://fake.url/")
-    def testGetHeaders(self):        
-        cacheValue = 'this_was_in_cache'
-        cache.set('pelias_token', cacheValue, None)
-        self.assertEqual(get_auth_headers(), {'Authorization': 'Bearer ' + cacheValue})
+    @patch('eventkit_cloud.utils.geocoding.geocode_auth.cache')
+    def test_update_session_cookies(self, mock_cache):
+        example_cookies = "test_cookies"
+        update_session_cookies(example_cookies)
+        mock_cache.set.assert_called_once_with(CACHE_COOKIE_KEY, example_cookies, CACHE_TOKEN_TIMEOUT)
+
+    @patch('eventkit_cloud.utils.geocoding.geocode_auth.cache')
+    def test_get_session_cookies(self, mock_cache):
+        example_cookies = "test_cookies"
+        mock_cache.get.return_value = example_cookies
+        self.assertEquals(example_cookies, get_session_cookies())
+        mock_cache.get.assert_called_once_with(CACHE_COOKIE_KEY)
+
+    @patch('eventkit_cloud.utils.geocoding.geocode_auth.cache')
+    @patch('eventkit_cloud.utils.geocoding.geocode_auth.auth_requests')
+    def test_authenticate(self, mock_auth_requests, mock_cache):
+        with self.settings(GEOCODING_AUTH_URL="http://test.test"):
+            example_token = "test_token"
+            example_response = {"token": example_token}
+            mock_auth_requests.get().json.return_value = example_response
+            self.assertEquals(example_token, authenticate())
+            mock_cache.set.assert_called_once_with(CACHE_TOKEN_KEY, example_token, CACHE_TOKEN_TIMEOUT)
+            mock_cache.reset_mock()
+
+        with self.settings(GEOCODING_AUTH_URL="http://test.test"):
+            example_response = {}
+            mock_auth_requests.get().json.return_value = example_response
+            self.assertIsNone(authenticate())
+            mock_cache.set.assert_called_once_with(CACHE_TOKEN_KEY, settings.GEOCODING_AUTH_URL, CACHE_TOKEN_TIMEOUT)
+            mock_cache.reset_mock()
+
+        with self.settings(GEOCODING_AUTH_URL=None):
+            self.assertIsNone(authenticate())
+
+        with self.settings(GEOCODING_AUTH_URL="http://test.test"):
+            mock_auth_requests.get().json.side_effect = requests.exceptions.RequestException()
+            self.assertIsNone(authenticate())
+            mock_cache.delete.assert_called_once_with(CACHE_TOKEN_KEY)
