409ffa0fa0d17eaf3d78e819ed65546b6b282027
Carrie Savinell <carrielane89@gmail.com>
2020-04-01 12:27:09 -0400

    Pulled from Pritchett's aoi branch in to build on and added mu components with event listeners and styling

--- eventkit_cloud/ui/static/ui/app/components/CreateDataPack/ProviderStatusCheck.tsx
+++ eventkit_cloud/ui/static/ui/app/components/CreateDataPack/ProviderStatusCheck.tsx
@@ -5,8 +5,11 @@ import ActionDone from '@material-ui/icons/Done';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Popover from '@material-ui/core/Popover';
 import Typography from '@material-ui/core/Typography';
+import BaseDialog from '../Dialog/BaseDialog';
 import {useState} from "react";
 import {useJobValidationContext} from "./context/JobValidation";
+import {Button, IconButton} from "@material-ui/core";
+import CloseIcon from '@material-ui/icons/Clear';
 
 interface Props extends React.HTMLAttributes<HTMLElement> {
     availability: {
@@ -40,6 +43,8 @@ ProviderStatusCheck.defaultProps = { availability: { status: '', type: '', messa
 function ProviderStatusCheck(props: Props) {
     const { areEstimatesLoading, providerHasEstimates } = props;
     const [ anchorElement, setAnchor ] = useState(null);
+    const [ open, setOpen ] = useState(true);
+    const [ isSubmissionOpen, setSubmissionOpen ] = useState(false);
 
     function handlePopoverOpen(e: React.MouseEvent<HTMLElement>) {
         setAnchor(e.currentTarget );
@@ -47,6 +52,24 @@ function ProviderStatusCheck(props: Props) {
 
     function handlePopoverClose() {
         setAnchor(null);
+        console.log('HITTING FINAL CLOSE EVENT')
+    }
+
+    function handleClose() {
+        // e.preventDefault();
+        alert("Alerted Browser Close");
+
+        setAnchor(null);
+        console.log('HITTING FIRST CLICK EVENT')
+        // setOpen(false);
+    }
+
+    function handleSubmissionOpen() {
+        setSubmissionOpen(true);
+    }
+
+    function handleSubmissionClose() {
+        setSubmissionOpen(false);
     }
 
     const style = {
@@ -118,7 +141,7 @@ function ProviderStatusCheck(props: Props) {
                 style.icon.color = 'rgba(192, 0, 0, 0.87)';
                 StatusIcon = AlertError;
                 title = 'AOI TOO LARGE';
-                message = makeMessage('Selected AoI too large for this provider.', false);
+                message = makeMessage('The selected AOI larger than the maximum allowed size for this provider.', false);
                 break;
             case STATUS.OVER_DATA_SIZE:
                 style.icon.color = 'rgba(192, 0, 0, 0.87)';
@@ -141,39 +164,116 @@ function ProviderStatusCheck(props: Props) {
         }
     }
 
+    let submissionPopover;
+    if (!!handleSubmissionOpen) {
+        submissionPopover = (
+            <div className="qa-ProviderStatusIcon-submission-popover">
+                <BaseDialog
+                    show={isSubmissionOpen}
+                    // title={provider.license.name}
+                    onClose={handleSubmissionClose}
+                >
+                    <div className="qa-ProviderStatusIcon-submission-popover-text">Your AOI increase request has been submitted.</div>
+                    <Button>OK</Button>
+                </BaseDialog>
+            </div>
+        );
+    }
+
+    let popOverBlock;
+    if ((status !== STATUS.OVER_AREA_SIZE) && (avail.type !== 'AOI TOO LARGE')) {
+        popOverBlock = (
+            <div style={style.base} className="qa-ProviderStatusIcon">
+                <StatusIcon
+                    style={style.icon}
+                    title={props.availability.message}
+                    onMouseEnter={handlePopoverOpen}
+                    onMouseLeave={handlePopoverClose}
+                    {...otherProps}
+                />
+                <Popover
+                    style={{pointerEvents: 'none'}}
+                    PaperProps={{
+                        style: {padding: '16px'},
+                    }}
+                    open={!!anchorElement}
+                    anchorEl={anchorElement}
+                    onClose={handlePopoverClose}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'center',
+                    }}
+                    transformOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'center',
+                    }}
+                >
+                    <div style={{maxWidth: 400}}>
+                        <Typography variant="h6" gutterBottom style={{fontWeight: 600}}>
+                            {title}
+                        </Typography>
+                        <div>{message}</div>
+                    </div>
+                </Popover>
+            </div>
+        )
+    } else {
+        popOverBlock = (
+            <div style={style.base} className="qa-ProviderStatusIcon">
+                <StatusIcon
+                    style={style.icon}
+                    title={props.availability.message}
+                    onClick={handlePopoverOpen}
+                    {...otherProps}
+                />
+                <Popover
+                    style={{pointerEvents: 'none'}}
+                    PaperProps={{
+                        style: {padding: '16px'},
+                    }}
+                    open={!!anchorElement}
+                    anchorEl={anchorElement}
+                    onClose={handlePopoverClose}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'center',
+                    }}
+                    transformOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'center',
+                    }}
+                >
+                    <div style={{maxWidth: 400}}>
+                        <Typography variant="h6" gutterBottom style={{fontWeight: 600}}>
+                            {title}
+                            <IconButton
+                                className='qa-ProviderStatusIcon-icon-btn'
+                                type='button'
+                                onClick={handlePopoverClose}
+                                // onClick={() => handlePopoverClose()}
+                            >
+                                <CloseIcon className='qa-ProviderStatusIcon-close-icon'/>
+                            </IconButton>
+                        </Typography>
+                        <div>{message}</div>
+                        <span
+                            role="button"
+                            onClick={handleSubmissionOpen}
+                            onKeyPress={handleSubmissionOpen}
+                            className="qa-ProviderStatusIcon-submission-popover-title"
+                        >
+                            <strong>Request Larger AOI Limit</strong>
+                        </span>
+                        {submissionPopover}
+                    </div>
+                </Popover>
+            </div>
+        )
+    }
+
     return (
         <div style={style.base} className="qa-ProviderStatusIcon" >
-            <StatusIcon
-                style={style.icon}
-                title={props.availability.message}
-                onMouseEnter={handlePopoverOpen}
-                onMouseLeave={handlePopoverClose}
-                {...otherProps}
-            />
-            <Popover
-                style={{ pointerEvents: 'none' }}
-                PaperProps={{
-                    style: { padding: '16px' },
-                }}
-                open={!!anchorElement}
-                anchorEl={anchorElement}
-                onClose={handlePopoverClose}
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'center',
-                }}
-                transformOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'center',
-                }}
-            >
-                <div style={{ maxWidth: 400 }}>
-                    <Typography variant="h6" gutterBottom style={{ fontWeight: 600 }}>
-                        {title}
-                    </Typography>
-                    <div>{message}</div>
-                </div>
-            </Popover>
+            {popOverBlock}
         </div>
     );
 }
