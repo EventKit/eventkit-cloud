17bd721eb8ec443334c38a26367ad5280fd47a1b
Joseph Pritchett <joseph.pritchett@rgi-corp.com>
2020-04-07 22:23:14 -0400

    Modify hook and add prelim tests

---
+++ eventkit_cloud/ui/static/ui/app/tests/utils/hooks.spec.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
+import { mount } from 'enzyme';
+import { useAsyncRequest } from '../../utils/hooks';
+
+
+let container = null;
+beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement('div');
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+const apiUrl = '/api/fake_url';
+
+function ApiHookTester() {
+    const [{ status, response }, requestCaller] = useAsyncRequest();
+    const makeRequest = () => requestCaller({
+        url: apiUrl,
+        method: 'get',
+        data: {},
+    });
+
+    if (!status) {
+        return (
+            <div>
+                {/* eslint-disable-next-line react/button-has-type */}
+                <button onClick={makeRequest}>Request not fired.</button>
+            </div>
+        );
+    }
+    if (status === 'success') {
+        return (
+            <div>
+                <div>{status}</div>
+                <div>{response.data}</div>
+            </div>
+        );
+    }
+    return (<div>{status}</div>);
+}
+
+//
+// it('should render correctly based on status', async () => {
+//     const mock = new MockAdapter(axios, { delayResponse: 1 });
+//     mock.onGet(apiUrl).reply(500);
+//     act(() => {
+//         render(<ApiHookTester />, container);
+//     });
+//     expect(container.querySelector('div').textContent).toContain('Request not fired.');
+//     const button = container.querySelector('button');
+//     expect(button.textContent).toBe('Request not fired.');
+//
+//     act(() => {
+//         button.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+//     });
+//     expect(container.querySelector('div').textContent).toContain('fetching');
+// });
+
+
+it('should render correctly based on status', async () => {
+    // const mock = new MockAdapter(axios, { delayResponse: 1 });
+    // mock.onGet(apiUrl).reply(500);
+
+    const wrapper = mount(<ApiHookTester />);
+    expect(wrapper.find('div').html()).toContain('Request not fired.');
+
+    const getButton = wrapper.find('button');
+    getButton.simulate('click');
+    return new Promise(resolve => setImmediate(resolve)).then(() => {
+        expect(wrapper.find('div').html()).toContain('fetching');
+    });
+});
--- eventkit_cloud/ui/static/ui/app/utils/hooks.ts
+++ eventkit_cloud/ui/static/ui/app/utils/hooks.ts
@@ -1,4 +1,5 @@
-import {useEffect, useRef} from "react";
+import {useCallback, useEffect, useReducer, useRef} from "react";
+import axios from "axios";
 
 // Convenience function that acts like componentDidMount.
 // useEffect replaces componentDidMount AND componentDidUpdate
@@ -27,12 +28,9 @@ enum ACTIONS {
     CLEAR,
 }
 
-const initialState = {
-    status: null,
-    response: {},
-};
-
-function submitReducer(state = initialState, { type = undefined, response = undefined } = {}) {
+interface RequestState {status: any, response: any}
+const initialState = { status: null, response: {} } as RequestState;
+function submitReducer(state = initialState, { type = undefined, response = undefined } = {}) : RequestState {
     switch (type) {
         case ACTIONS.FETCHING:
             return { ...initialState, status: 'fetching' };
@@ -51,10 +49,11 @@ interface Dispatcher {
     fetching: () => void;
     success: (response) => void;
     error: (response) => void;
+    clear: () => void;
 }
 
 // Async request hook with more fine grained ability to control the request.
-export function useAsyncRequest_Control(): [any, Dispatcher] {
+export function useAsyncRequest_Control(): [RequestState, Dispatcher] {
     const [state, dispatch] = useReducer(submitReducer, initialState);
     const dispatches = {
         fetching: () => dispatch({ type: ACTIONS.FETCHING }),
@@ -65,7 +64,7 @@ export function useAsyncRequest_Control(): [any, Dispatcher] {
     return [state, dispatches]
 }
 
-export function useAsyncRequest() {
+export function useAsyncRequest(): [RequestState, ((params: any) => Promise<void>)] {
     const [state, dispatches] = useAsyncRequest_Control();
     const makeRequest = useCallback(async (params: any) => {
         dispatches.fetching();
@@ -77,38 +76,4 @@ export function useAsyncRequest() {
         }
     }, []);
     return [state, makeRequest];
-}
-
-// Returns a callback function that wraps the passed setter in a debouncer mechanism
-// While the timer is off, the setter will be called immediately and trigger a new timer.
-// While the timer is on, the value to be set is captured and used at the end of the timer period.
-// Subsequent calls during the timer period will replace the captured value to be set, there is no queue.
-export function useDebouncedSetter(setter: (value: any) => void, timeout = 1000) {
-    const timerActive = useRef(false);
-    const valueCapture = useRef(null);
-    const setterCapture = useRef(setter);  // Capture the setter value to detect if a new one is passed
-    return useCallback(async (value: any) => {
-        if (timerActive.current === true) {
-            valueCapture.current = value;
-        } else {
-            setter(value);
-            valueCapture.current = value;
-            timerActive.current = true;
-            setterCapture.current = setter;
-            setTimeout(() => {
-                timerActive.current = false;
-                // If the last captured value is the same as the value when the timeout was started, do nothing
-                // If the last captured setter is different from the current setter, do nothing
-                // This later case means a new setter was passed invalidating the old one.
-                if (valueCapture.current !== value && setterCapture.current === setter) {
-                    setter(valueCapture.current);
-                }
-            }, timeout);
-        }
-        return () => {
-            // Cleanup, set the flag to false, get rid of the last value.
-            timerActive.current = false;
-            valueCapture.current = null;
-        }
-    }, [setter, timeout]);
 }
