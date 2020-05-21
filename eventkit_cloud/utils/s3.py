8f65837204329579fc4f546ed0d067c8f26f40fd
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2020-05-21 16:41:30 -0400

    Lint fixes.

--- eventkit_cloud/utils/s3.py
+++ eventkit_cloud/utils/s3.py
@@ -8,7 +8,9 @@ logger = logging.getLogger(__name__)
 
 
 def get_s3_client():
-    return boto3.client("s3", aws_access_key_id=settings.AWS_ACCESS_KEY_ID, aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,)
+    return boto3.client(
+        "s3", aws_access_key_id=settings.AWS_ACCESS_KEY_ID, aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+    )
 
 
 def upload_to_s3(run_uuid, source_path, destination_filename, client=None, user_details=None):
