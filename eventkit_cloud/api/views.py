2a42415eb85ec2318284871ebe1db102fdb258c5
Carrie Savinell <carrielane89@gmail.com>
2020-07-23 10:31:25 -0400

    test

--- eventkit_cloud/api/views.py
+++ eventkit_cloud/api/views.py
@@ -2202,9 +2202,9 @@ def get_jobs_via_permissions(permissions):
 
 def api_docs_view(request):
     if request.user.is_authenticated:
-        return render(request, template_name="swagger-ui.html", context={"schema_url": "api:openapi-schema"})
+        return render(request, template_name="swagger-ui.html", context={"schema_url                      ": "api:openapi-schema"})
 
-    
+
     else:
         return redirect("/api/login?next=/api/docs")
 
