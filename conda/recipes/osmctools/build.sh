b6bc69bcecb5ce3640e7d7956394a4c21fa8de32
Joseph Svrcek <joesvrcek@hotmail.com>
2020-10-11 20:56:23 -0400

    added zlib and wget for osmctools

--- conda/recipes/osmctools/build.sh
+++ conda/recipes/osmctools/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-BIN_DIR="${PREFIX}/bin"
-OSMCTOOLS_SHARE="${PREFIX}/osmctools-share"
-OSMCTOOLS_DOC="${PREFIX}/osmctools-doc"
+set -e
+export BIN_DIR="${PREFIX}/bin"
+export OSMCTOOLS_SHARE="${PREFIX}/osmctools-share"
+export OSMCTOOLS_DOC="${PREFIX}/osmctools-doc"
 
 mkdir "${OSMCTOOLS_SHARE}" "${OSMCTOOLS_DOC}"
 
@@ -11,8 +12,8 @@ autoreconf --install
 ./configure --prefix="${PREFIX}" \
             --bindir="${BIN_DIR}" \
             --datarootdir="${OSMCTOOLS_SHARE}" \
-            --docdir="${OSMCTOOLS_DOC}" \
-
+            --docdir="${OSMCTOOLS_DOC}"
+make
 make install
 
 rm -rf "${OSMCTOOLS_SHARE}" "${OSMCTOOLS_DOC}"
