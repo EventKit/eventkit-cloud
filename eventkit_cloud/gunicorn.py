128c847d1349d28647f6bdfc76807ca34e60407a
jsvrcek <joseph.svrcek@rgi-corp.com>
2020-10-13 14:55:45 -0400

    lint fixes

--- eventkit_cloud/gunicorn.py
+++ eventkit_cloud/gunicorn.py
@@ -6,7 +6,7 @@ from gunicorn.http import wsgi
 class Response(wsgi.Response):
     def default_headers(self, *args, **kwargs):
         headers = super(Response, self).default_headers(*args, **kwargs)
-        return [header for header in headers if not header.startswith('Server:')]
+        return [header for header in headers if not header.startswith("Server:")]
 
 
 wsgi.Response = Response
