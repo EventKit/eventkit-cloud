60a9760d0068f64b0a567bd49540e7ccf307f331
jsvrcek <joseph.svrcek@rgi-corp.com>
2020-10-19 11:57:06 -0400

    added helper method

--- eventkit_cloud/utils/helpers.py
+++ eventkit_cloud/utils/helpers.py
@@ -2,6 +2,8 @@ import logging
 import os
 
 from django.conf import settings
+import xml.etree.Element
+import xml.etree.ElementTree as ET
 
 logger = logging.getLogger()
 
@@ -26,3 +28,23 @@ def get_download_paths(relative_path):
     )
     download_url = os.path.join(settings.EXPORT_MEDIA_ROOT.rstrip(os.path.sep), relative_path.lstrip(os.path.sep),)
     return downloads_filepath, download_url
+
+
+def extract_kml_styles(kml_file_path: str) -> str:
+    """
+    Parses KML document for styles descriptors and maps them to file names then stores the result in a file to be
+    mapped into a kml file.
+    :param kml_file_path: A path to a kml file.
+    :return: A file path to the location of the stored styles
+    """
+    placemarks = get_all_kml_placemarks(kml_file_path)
+    for placemark in placemarks:
+
+
+
+def get_all_kml_placemarks(kml_file_path: str) -> List[xml.etree.Element]:
+    namespaces = {'kml': 'http://www.opengis.net/kml/2.2'}
+    tree = ET.parse('output.kml')
+    root = tree.getroot()
+    # xpath -> findall from this node (root) for all subnodes in entire tree.
+    return root.findall('.//kml:Placemark', namespaces)
---
+++ eventkit_cloud/utils/qgis.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import glob
+from qgis.core import QgsApplication, QgsProject, QgsVectorLayer
+import osgeo
+import gdal
+
+# Need to resolve dependencies to include this as a part of our pipeline.
+
+def convert_qgis_gpkg_to_kml(qgs_file: str, output_kml_path: str) -> str:
+    app = None
+    driverName = 'libkml'
+
+    try:
+        # Load application to load QApplication dependencies.
+        app_directory = "./"  # hosts various session type files like qgis.db etc...
+        app = QgsApplication([], False, app_directory)
+        app.initQgis()
+
+        # Application needs to be initialized before importing.
+        from qgis.core import QgsVectorFileWriter
+        # Load project from template output.
+        project = QgsProject.instance()
+        if not project.read(qgs_file):
+            raise Exception(f"Could not read from {qgs_file}")
+
+        layers = project.mapLayers()
+        symbology_scale = 20000
+        output_dir = os.path.join('.', 'kml')
+
+        for layer_name, layer in layers.items():
+            output_file = os.path.join(output_dir, f"{layer.name()}.kml")
+            if not os.path.exists(output_dir):
+                os.mkdir(output_dir)
+            QgsVectorFileWriter.writeAsVectorFormat(layer=layer,
+                                                    fileName=output_file,
+                                                    fileEncoding="utf-8",
+                                                    driverName=driverName,
+                                                    symbologyExport=QgsVectorFileWriter.SymbolLayerSymbology,
+                                                    symbologyScale=symbology_scale)
+
+        out_driver = osgeo.ogr.GetDriverByName(driverName)
+        if os.path.exists(output_kml):
+            out_driver.DeleteDataSource(output_kml)
+
+        out_driver.CreateDataSource(output_kml_path)
+
+        kml_files = os.listdir(output_dir)
+        for kml_file in kml_files:
+            kml_path = os.path.join(output_dir, kml_file)
+            gdal.VectorTranslate(output_kml, kml_path, accessMode="append")
+
+        return output_kml_path
+    finally:
+        if app:
+            app.exit()
