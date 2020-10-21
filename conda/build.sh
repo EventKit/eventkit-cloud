1af081b31adff28c95ea3384547e5e5ae572a70e
jsvrcek <joesvrcek@hotmail.com>
2020-10-21 10:56:39 -0400

    removed build pins

--- conda/build.sh
+++ conda/build.sh
@@ -26,8 +26,9 @@ conda config --add channels file://root/repo/
 
 function create_index {
   echo "Move files and create index"
-  cp -f /root/miniconda3/pkgs/*.tar.bz2 /root/repo/linux-64/
-  cp -f /root/miniconda3/conda-bld/linux-64/*.tar.bz2 /root/repo/linux-64/
+  # Note the exit 0 are because the cp conda-bld command could fail if only indexing and not building.
+  cp -f /root/miniconda3/pkgs/*.tar.bz2 /root/repo/linux-64/ || exit 0
+  cp -f /root/miniconda3/conda-bld/*/*.tar.bz2 /root/repo/linux-64/ || exit 0
 
   conda config --add channels file://root/repo/
   pushd /root/repo
@@ -35,17 +36,21 @@ function create_index {
   popd
 }
 
-echo "Building recipes"
 cd /root/recipes
+export RECIPES="/root/recipes.txt"
+
 if [ -z "$1" ]; then
-  echo "***Building all recipes in recipes.txt**"
-  while read recipe; do
-      echo "***Building  $recipe ...***"
-      NAME=$(echo "$recipe" | tr -d '\r')
-      for i in 1 2 3; do conda build $NAME && \
-      echo "y" | conda install $NAME && s=0 && break || s=$? && sleep 5; done; (exit $s)
-      create_index
-  done < /root/recipes.txt
+  echo "***Building all recipes in recipe.txt**"
+  NAMES=$(tr '\n' ' ' < $RECIPES)
+  echo $NAMES
+  conda build $NAMES
+#  while read recipe; do
+#      echo "***Building  $recipe ...***"
+#      NAME=$(echo $recipe | tr -d '\r')
+#      for i in 1 2 3; do conda build $NAME && \
+#      echo "y" | conda install $NAME && s=0 && break || s=$? && sleep 5; done; (exit $s)
+#      create_index
+#  done < $RECIPES
 else
   echo "***Building $@...***"
   for i in 1 2 3; do conda build $@ && echo "y" | conda install $@ && s=0 && break || s=$? && sleep 5; done; (exit $s)
