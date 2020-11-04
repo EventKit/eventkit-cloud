bcdb31c7056a2b4b175e59d92067e898fb915d5f
Joseph Pritchett <joseph.pritchett@rgi-corp.com>
2020-11-04 18:52:24 -0500

    Remove unneeded change

--- eventkit_cloud/ui/static/ui/app/components/CreateDataPack/MapZoomLimiter.tsx
+++ eventkit_cloud/ui/static/ui/app/components/CreateDataPack/MapZoomLimiter.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useEffect, useRef, useState} from 'react';
+import {useEffect, useState} from 'react';
 import {RegionJustification} from '../StatusDownloadPage/RegionJustification';
 
 interface Props {
@@ -24,10 +24,8 @@ export function MapZoomLimiter(props: Props) {
 
     const [displayDialog, setDisplayDialog] = useState(false);
     useEffect(() => {
-        if (!displayDialog) {
-            if ((!!zoomLevel || zoomLevel === 0) && olZoom > zoomLevel) {
-                setDisplayDialog(true);
-            }
+        if ((!!zoomLevel || zoomLevel === 0) && olZoom > zoomLevel) {
+            setDisplayDialog(true);
         }
     }, [olZoom]);
 
