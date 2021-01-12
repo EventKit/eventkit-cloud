7d0fbf330bac05cdc29ef5921a1a3426da304044
Pablo Vargas <pablo.vargas@octoconsulting.com>
2021-01-12 09:09:57 -0500

    Fix rebase issue

--- eventkit_cloud/tasks/export_tasks.py
+++ eventkit_cloud/tasks/export_tasks.py
@@ -1057,10 +1057,11 @@ def wfs_export_task(
 
     configuration = load_provider_config(config)
 
-    if "layers" in configuration:
+    vector_layer_data = configuration.get("vector_layers", [])
+    if len(vector_layer_data):
         layers = {}
 
-        for layer in configuration.get("layers"):
+        for layer in vector_layer_data:
             path = get_export_filepath(stage_dir, job_name, f"{layer.get('name')}-{projection}", provider_slug, "gpkg")
             url = get_wfs_query_url(name, layer.get("url"), layer.get("name"), projection)
             layers[layer["name"]] = {"url": url, "path": path, "cert_var": configuration.get("cert_var")}
@@ -1239,10 +1240,11 @@ def arcgis_feature_service_export_task(
 
     configuration = load_provider_config(config)
 
-    if "layers" in configuration:
+    vector_layer_data = configuration.get("vector_layers", [])
+    if len(vector_layer_data):
         layers = {}
 
-        for layer in configuration.get("layers"):
+        for layer in vector_layer_data:
             path = get_export_filepath(stage_dir, job_name, f"{layer.get('name')}-{projection}", provider_slug, "json")
             url = get_arcgis_query_url(layer.get("url"), bbox)
             layers[layer["name"]] = {"url": url, "path": path, "cert_var": configuration.get("cert_var")}
