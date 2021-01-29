6cf14ed532b865967c6311d180302985d6adf087
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2021-01-29 13:54:43 -0500

    Update Celery syntax for version 5.

--- scripts/run-celery.sh
+++ scripts/run-celery.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-celery worker -A eventkit_cloud --concurrency=$RUNS_CONCURRENCY --loglevel=$LOG_LEVEL -n runs@%h -Q runs & \
-celery worker -A eventkit_cloud --concurrency=$CONCURRENCY --loglevel=$LOG_LEVEL -n worker@%h -Q $CELERY_GROUP_NAME & \
-celery worker -A eventkit_cloud --concurrency=1 --loglevel=$LOG_LEVEL -n celery@%h -Q celery & \
-celery worker -A eventkit_cloud --concurrency=1 --loglevel=$LOG_LEVEL -n priority@%h -Q $CELERY_GROUP_NAME.priority,$HOSTNAME.priority & \
-celery worker -A eventkit_cloud --concurrency=$OSM_CONCURRENCY --loglevel=$LOG_LEVEL -n large@%h -Q $CELERY_GROUP_NAME.large
+celery -A eventkit_cloud worker --concurrency=$RUNS_CONCURRENCY --loglevel=$LOG_LEVEL -n runs@%h -Q runs & \
+celery -A eventkit_cloud worker --concurrency=$CONCURRENCY --loglevel=$LOG_LEVEL -n worker@%h -Q $CELERY_GROUP_NAME & \
+celery -A eventkit_cloud worker --concurrency=1 --loglevel=$LOG_LEVEL -n celery@%h -Q celery & \
+celery -A eventkit_cloud worker --concurrency=1 --loglevel=$LOG_LEVEL -n priority@%h -Q $CELERY_GROUP_NAME.priority,$HOSTNAME.priority & \
+celery -A eventkit_cloud worker --concurrency=$OSM_CONCURRENCY --loglevel=$LOG_LEVEL -n large@%h -Q $CELERY_GROUP_NAME.large
