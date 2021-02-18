6cd1d89fa1cd3cff8db5c72cfc2210ae3e427094
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2021-02-17 20:10:19 -0500

    Use temporary key during Docker image builds.

--- eventkit_cloud/settings/utils.py
+++ eventkit_cloud/settings/utils.py
@@ -9,20 +9,3 @@ DJANGO_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__
 
 def ABS_PATH(*args):
     return os.path.join(DJANGO_ROOT, *args)
-
-
-def ensure_secret_key_file():
-    """Checks that secret.py exists in settings dir. If not, creates one
-with a random generated SECRET_KEY setting."""
-    secret_path = ABS_PATH("eventkit_cloud", "settings", "secret.py")
-    if not os.path.exists(secret_path):
-        from django.utils.crypto import get_random_string
-
-        secret_key = get_random_string(50, "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)")
-        with open(secret_path, "w") as f:
-            f.write("import os\n")
-            f.write("SECRET_KEY = os.environ.get('SECRET_KEY', " + repr(secret_key) + "\n")
-
-
-# Import the secret key
-ensure_secret_key_file()
