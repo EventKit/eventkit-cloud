535b507059f10659ff47c1455cff3bce1a3cde7b
Joseph Pritchett <joseph.pritchett@rgi-corp.com>
2021-02-24 17:38:57 -0500

    Add content security env

--- eventkit_cloud/gunicorn.py
+++ eventkit_cloud/gunicorn.py
@@ -1,3 +1,5 @@
+import os
+
 from gunicorn.http import wsgi
 
 # Used to configure gunicorn settings.
@@ -6,6 +8,7 @@ from gunicorn.http import wsgi
 class Response(wsgi.Response):
     def default_headers(self, *args, **kwargs):
         headers = super(Response, self).default_headers(*args, **kwargs)
+        headers.append(f"Content-Security-Policy: ${os.getenv('CONTENT_SECURITY_POLICY')}")
         return [header for header in headers if not header.startswith("Server:")]
 
 
