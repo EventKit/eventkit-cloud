5f52617351a3479d2276e40779f87a5dd2efcd94
Caboose71 <50183410+Caboose71@users.noreply.github.com>
2021-03-22 11:03:37 -0400

    Add bbox chunking for WCS requests (#1043)
    * Add chunking function for WCS tiles
    
    * Add tests
    
    * Adjust tile size calculation
    
    * Allow tile size to be set from config
    
    * Lint fixes and replace get with session get
    
    * Update test to reflect new return type
    
    Co-authored-by: Jeremiah Cooper <JCooper@prominentedge.com>
    Co-authored-by: Pablo Vargas <12192559+pvargas@users.noreply.github.com>

--- eventkit_cloud/utils/gdalutils.py
+++ eventkit_cloud/utils/gdalutils.py
@@ -10,24 +10,27 @@ import time
 from tempfile import NamedTemporaryFile
 from functools import wraps
 
+from mapproxy.grid import tile_grid
 from osgeo import gdal, ogr, osr
+
 
 from eventkit_cloud.tasks.task_process import TaskProcess, update_progress
 from eventkit_cloud.utils.generic import requires_zip, create_zip_file, get_zip_name
 from eventkit_cloud.utils.geocoding.geocode import GeocodeAdapter, is_valid_bbox
 from django.conf import settings
 
-
 logger = logging.getLogger(__name__)
 
 MAX_DB_CONNECTION_RETRIES = 8
 TIME_DELAY_BASE = 2  # Used for exponential delays (i.e. 5^y) at 8 would be about 4 minutes 15 seconds max delay.
 
-
 # The retry here is an attempt to mitigate any possible dropped connections. We chose to do a limited number of
 # retries as retrying forever would cause the job to never finish in the event that the database is down. An
 # improved method would perhaps be to see if there are connection options to create a more reliable connection.
 # We have used this solution for now as I could not find options supporting this in the gdal documentation.
+
+
+GOOGLE_MAPS_FULL_WORLD = [-20037508.342789244, -20037508.342789244, 20037508.342789244, 20037508.342789244]
 
 
 def retry(f):
@@ -807,3 +810,27 @@ def strip_prefixes(dataset: str) -> (str, str):
             removed_prefix = prefix
         output_dataset = cleaned_dataset
     return removed_prefix, output_dataset
+
+
+def get_chunked_bbox(bbox, size: tuple = None):
+    """
+    Chunks a bbox into a grid of sub-bboxes.
+
+    :param bbox: bbox in 4326, representing the area of the world to be chunked
+    :param size: optional image size to use when calculating the resolution.
+    :return: enclosing bbox of the area, dimensions of the grid, bboxes of all tiles.
+    """
+    from eventkit_cloud.utils.image_snapshot import get_resolution_for_extent
+
+    # Calculate the starting res for our custom grid
+    # This is the same method we used when taking snap shots for data packs
+    resolution = get_resolution_for_extent(bbox, size)
+    # Make a subgrid of 4326 that spans the extent of the provided bbox
+    # min res specifies the starting zoom level
+    mapproxy_grid = tile_grid(srs=4326, bbox=bbox, bbox_srs=4326, origin="ul", min_res=resolution)
+    # bbox is the bounding box of all tiles affected at the given level, unused here
+    # size is the x, y dimensions of the grid
+    # tiles at level is a generator that returns the tiles in order
+    tiles_at_level = mapproxy_grid.get_affected_level_tiles(bbox, 0)[2]
+    # convert the tiles to bboxes representing the tiles on the map
+    return [mapproxy_grid.tile_bbox(_tile) for _tile in tiles_at_level]
