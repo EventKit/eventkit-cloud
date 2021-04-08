14af9d69e18cd3da34d3b7a4c754941fb018603b
jsvrcek <joesvrcek@hotmail.com>
2021-04-08 15:31:42 -0400

    improved calculating statistics

--- eventkit_cloud/settings/celery.py
+++ eventkit_cloud/settings/celery.py
@@ -38,6 +38,10 @@ BEAT_SCHEDULE = {
     "clean-up-queues": {"task": "Clean Up Queues", "schedule": crontab(minute="0", hour="0")},
     "clear-tile-cache": {"task": "Clear Tile Cache", "schedule": crontab(minute="0", day_of_month="*/14")},
     "clear-user-sessions": {"task": "Clear User Sessions", "schedule": crontab(minute="0", day_of_month="*/2")},
+    "update-statistics-cache": {
+        "task": "Update Statistics Caches",
+        "schedule": crontab(minute="0", day_of_month="*/4"),
+    },
 }
 
 PCF_SCALING = os.getenv("PCF_SCALING", False)
