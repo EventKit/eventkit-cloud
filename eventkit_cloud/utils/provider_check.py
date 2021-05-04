a2a845438871d6bcd4801adfbc4b55e18554b8eb
zta6 <zta6@pitt.edu>
2021-05-04 14:38:49 -0600

    cleanup

--- eventkit_cloud/utils/provider_check.py
+++ eventkit_cloud/utils/provider_check.py
@@ -198,6 +198,7 @@ class ProviderCheck(object):
         Return True if the AOI selection's area is lower than the maximum for this provider, otherwise False.
         :return: True if AOI is lower than area limit
         """
+
         if self.aoi is None or int(self.max_area) <= 0:
             return True
 
@@ -206,7 +207,7 @@ class ProviderCheck(object):
 
         area_sq_km = area / 1000000
 
-        return area_sq_km
+        return area_sq_km < self.max_area
 
     def get_check_response(self):
         """
