fd0ba80e21bd15876d767ed721601a56da5b6322
jsvrcek <joesvrcek@hotmail.com>
2021-10-06 10:05:16 -0400

    fix overpass checks

--- eventkit_cloud/utils/services/overpass.py
+++ eventkit_cloud/utils/services/overpass.py
@@ -15,14 +15,14 @@ class Overpass(GisClient):
     def get_layer_name(self):
         raise NotImplementedError("Method is specific to service type")
 
-    def get_provider_response(self) -> requests.Response:
+    def get_response(self) -> requests.Response:
         query = "out meta;"
 
-        response = self.client.session.post(url=self.client.service_url, data=query, timeout=self.client.timeout)
+        response = self.session.post(url=self.service_url, data=query, timeout=self.timeout)
 
         if not response.ok:
             # Workaround for https://bugs.python.org/issue27777
             query = {"data": query}
-            response = self.client.session.post(url=self.client.service_url, data=query, timeout=self.client.timeout)
+            response = self.session.post(url=self.service_url, data=query, timeout=self.timeout)
 
         return response
