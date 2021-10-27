55c948dbf987ae5f5d31adfd0301991cb923b8ca
Zachary Adam <zta6@pitt.edu>
2021-10-27 04:46:08 -0600

    change UserSizeRule display and sorting (#1111)
    Co-authored-by: Joseph Svrcek <joesvrcek@hotmail.com>

--- eventkit_cloud/user_requests/admin.py
+++ eventkit_cloud/user_requests/admin.py
@@ -60,6 +60,31 @@ class SizeIncreaseRequestAdmin(admin.ModelAdmin):
     ]
 
 
+class UserSizeRuleAdmin(admin.ModelAdmin):
+    list_display = [
+        "get_last_name",
+        "get_first_name",
+        "user",
+        "provider",
+        "max_data_size",
+        "max_selection_size",
+    ]
+
+    search_fields = ("user__last_name", "user__first_name", "user", "provider__slug", "provider__name")
+
+    ordering = ("user__last_name", "user__first_name", "user__username", "provider__name")
+
+    def get_last_name(self, obj):
+        return obj.user.last_name
+
+    get_last_name.short_description = "Last Name"
+
+    def get_first_name(self, obj):
+        return obj.user.first_name
+
+    get_first_name.short_description = "First Name"
+
+
 admin.site.register(DataProviderRequest, DataProviderRequestAdmin)
 admin.site.register(SizeIncreaseRequest, SizeIncreaseRequestAdmin)
-admin.site.register(UserSizeRule)
+admin.site.register(UserSizeRule, UserSizeRuleAdmin)
