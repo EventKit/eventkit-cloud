b418172ffdaa2527effad1535ff57f3c9d5851d5
jsvrcek <joesvrcek@hotmail.com>
2022-04-26 13:08:57 -0400

    removed url for re_path

--- eventkit_cloud/urls.py
+++ eventkit_cloud/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import include, re_path
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 
 from eventkit_cloud.core.urls import urlpatterns as eventkit_cloud_urlpatterns
 
 urlpatterns = [
-    url(
+    re_path(
         r"^favicon.png$",
         RedirectView.as_view(url=staticfiles_storage.url("images/favicon.png"), permanent=False),
         name="favicon",
