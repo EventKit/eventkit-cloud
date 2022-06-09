c6b29de85b97c6bf935425c629c2322269c40d20
jsvrcek <joesvrcek@hotmail.com>
2022-06-09 12:03:17 -0400

    fixes bad defaults

--- eventkit_cloud/tasks/task_builders.py
+++ eventkit_cloud/tasks/task_builders.py
@@ -283,7 +283,7 @@ def create_export_task_record(task_name=None, export_provider_task=None, worker=
     try:
         export_task, created = ExportTaskRecord.objects.get_or_create(
             export_provider_task=export_provider_task,
-            defaults={"status": "PENDING", "name": "TaskName", "worker": "Worker", "display": display},
+            defaults={"status": "PENDING", "name": task_name, "worker": worker, "display": display},
         )
         logger.debug("Saved task: {0}".format(task_name))
     except DatabaseError as e:
