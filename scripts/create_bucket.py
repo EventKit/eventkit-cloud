e15fad2a0b2d39531f96e39039324d74a3c0d597
jsvrcek <joesvrcek@hotmail.com>
2022-08-11 15:05:22 -0400

    adds retry for create bucket

--- scripts/create_bucket.py
+++ scripts/create_bucket.py
@@ -1,14 +1,18 @@
 import os
 import sys
 
-from botocore.exceptions import ClientError
+import botocore
+
 import django
+import time
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventkit_cloud.settings.prod")
 django.setup()
 
 from eventkit_cloud.utils.s3 import get_s3_resource
+import logging
 
+logger = logging.getLogger(__name__)
 
 def create_bucket(bucket_name):
     """Create an S3 bucket in a specified region
@@ -20,12 +24,24 @@ def create_bucket(bucket_name):
     :param region: String region to create bucket in, e.g., 'us-west-2'
     :return: True if bucket created, else False
     """
-    try:
-        s3 = get_s3_resource()
-        s3.create_bucket(Bucket=bucket_name)
-    except ClientError as e:
-        if "BucketAlreadyOwnedByYou" not in str(e):
-            raise
+    tries = 3
+    while tries:
+        try:
+            s3 = get_s3_resource()
+            s3.create_bucket(Bucket=bucket_name)
+            return True
+        except botocore.exceptions.ClientError as e:
+            if "NoSuchBucket" in str(e):
+                tries -= 1
+                if not tries:
+                    raise
+                logger.info("Waiting for S3 Connection to try to create the bucket.")
+                time.sleep(1)
+            elif "BucketAlreadyOwnedByYou" in str(e):
+                logger.info("Bucket already created.")
+                return True
+            else:
+                raise
     return True
 
 
