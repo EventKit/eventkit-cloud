65e6af6c60ec173041a1e0036dbe215106eb1c74
Douglas Shoemaker <dougshmkr@gmail.com>
2022-09-12 14:00:23 -0400

    Initial work (#1232)
    Co-authored-by: doug.shoemaker <douglas.shoemaker@rgi-corp.com>
    Co-authored-by: Joseph Svrcek <joesvrcek@hotmail.com>

--- scripts/load_test_data.py
+++ scripts/load_test_data.py
@@ -1,28 +1,34 @@
-import os
 import json
+import os
 import random
+from sys import exc_info
 import time
+
 import django
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventkit_cloud.settings.prod")
 django.setup()
 
+from eventkit_cloud.auth.models import OAuth
 from eventkit_cloud.tasks.models import *
 from eventkit_cloud.jobs.models import *
 from eventkit_cloud.jobs.enumerations import GeospatialDataType
-from django.contrib.auth.models import Group, User
 from django.db import connection, reset_queries
+from django.contrib.auth.models import Group, User
+from eventkit_cloud.core.models import update_all_users_with_attribute_class
 
 NAME_FIELD = "NAME"
 GEOJSON_FILEPATH = os.path.join(os.path.dirname(__name__), "adhoc/cities.geojson")
 if not os.path.exists(GEOJSON_FILEPATH):
     raise Exception(f"Could not find a geojson file at {GEOJSON_FILEPATH}")
 PREFIX = "test-load-"
-DOWNLOAD_URL = "/path/something"
 BATCH_SIZE = 200
 
+
 def get_fptrs():
-    return FileProducingTaskResult.objects.filter(download_url=DOWNLOAD_URL)
+    # return FileProducingTaskResult.objects.filter(filename__contains=PREFIX)
+    return FileProducingTaskResult.objects.filter(directory=PREFIX)
 
 
 def get_user_downloads():
@@ -58,8 +64,20 @@ def get_users():
     return User.objects.filter(username__contains=PREFIX)
 
 
+def get_topics():
+    return Topic.objects.filter(slug__contains=PREFIX)
+
+
+def get_attribute_classes():
+    return AttributeClass.objects.filter(slug__contains=PREFIX)
+
+
+def get_oauths(users):
+    return OAuth.objects.filter(user__in=users)
+
+
 def cleanup():
-    print("Removing old test data.")
+    logger.info("Removing old test data.")
     downloads = get_user_downloads()
     downloads._raw_delete(downloads.db)
     etrs = get_etrs()
@@ -74,11 +92,19 @@ def cleanup():
     user_activity = UserJobActivity.objects.filter(job__in=jobs)
     user_activity._raw_delete(user_activity.db)
     jobs._raw_delete(jobs.db)
+    topics = get_topics()
+    # topics._raw_delete(topics.db)
+    topics.delete()
     providers = get_data_providers()
     providers._raw_delete(providers.db)
     groups = Group.objects.filter(name__contains=PREFIX)
     groups._raw_delete(groups.db)
+    attribute_classes = get_attribute_classes()
+    # attribute_classes._raw_delete(attribute_classes.db)
+    attribute_classes.delete()
     users = get_users()
+    oauths = get_oauths(users)
+    oauths._raw_delete(oauths.db)
     users._raw_delete(users.db)
 
 
@@ -88,24 +114,31 @@ def get_user_ids() -> list:
 
 def create_users(count):
     try:
-        print(f"Creating {count} Users")
-        User.objects.bulk_create([
+        logger.info(f"Creating {count} Users")
+        users = User.objects.bulk_create([
             User(username=f"{PREFIX}{count_value}",
                  email=f"user{count_value}@email.test", password='pass'
                  ) for count_value in range(count)
         ], batch_size=1000)
+
+        users = OAuth.objects.bulk_create([
+            OAuth(user=user, identification=f"{PREFIX}{count_value}",
+                  commonname=f"{PREFIX}{count_value}", user_info={"group": f"{count%2}"})
+            for count_value, user in enumerate(users)
+        ], batch_size=1000)
+
     except Exception:
-        print("Failed to create all users.")
+        logger.info("Failed to create all users.")
 
 
 def create_groups(count):
     try:
-        print(f"Creating {count} Groups")
+        logger.info(f"Creating {count} Groups")
         Group.objects.bulk_create([
             Group(name=f"{PREFIX}{count_value}") for count_value in range(count)
         ], batch_size=1000)
     except Exception:
-        print("Failed to create all Groups.")
+        logger.info("Failed to create all Groups.")
 
 
 def batch_create(model, objects):
@@ -155,22 +188,21 @@ def create_runs(count):
     fptr_count = count * max_provider_per_run * export_format_count
 
     results = list(batch_create(FileProducingTaskResult,
-                           (FileProducingTaskResult(filename="test.gpkg", size=random.randint(10, 1000000) / 1000,
-                                                    download_url=DOWNLOAD_URL) for _ in range(fptr_count))))
+                                (FileProducingTaskResult(filename=f"{PREFIX}-test.gpkg", directory=PREFIX, size=random.randint(10, 1000000) / 1000) for _ in range(fptr_count))))
 
     run_state = ["COMPLETED", "COMPLETED", "COMPLETED", "COMPLETED", "INCOMPLETE"]
     geojson_data = get_geojson_data(geojson_filepath=GEOJSON_FILEPATH)
     user_ids = get_user_ids()
-    print(f"Creating {count} jobs and runs.")
+    logger.info(f"Creating {count} jobs and runs.")
     jobs = batch_create(Job, (get_job(geojson_data, user_ids) for _ in range(count)))
     runs = batch_create(ExportRun, (ExportRun(job=job, user=job.user, status=random.choice(run_state)) for job in jobs))
     result_index = 0
     etr_count = 0
     for run in runs:
         dptrs = batch_create(DataProviderTaskRecord, (
-        DataProviderTaskRecord(name=data_provider.name, provider=data_provider, display=True, run=run,
-                               status="COMPLETED") for data_provider in
-        random.sample(data_providers, random.randint(1, min(len(data_providers), max_provider_per_run)))))
+            DataProviderTaskRecord(name=data_provider.name, provider=data_provider, display=True, run=run,
+                                   status="COMPLETED") for data_provider in
+            random.sample(data_providers, random.randint(1, min(len(data_providers), max_provider_per_run)))))
         for dptr in dptrs:
             etr = []
             for file_format in dptr.provider.export_provider_type.supported_formats.all():
@@ -181,10 +213,10 @@ def create_runs(count):
                                          result=results[result_index])]
                 result_index += 1
             etr_count += len(ExportTaskRecord.objects.bulk_create(etr, batch_size=BATCH_SIZE))
-    print("Cleaning up unused files.")
+    logger.info("Cleaning up unused files.")
     empty_fptrs = get_fptrs().filter(export_task__isnull=True)
     empty_fptrs._raw_delete(empty_fptrs.db)
-    print(f"Created {etr_count} example files.")
+    logger.info(f"Created {etr_count} example files.")
 
 
 def get_random_time(start, end):
@@ -198,14 +230,17 @@ def get_random_time(start, end):
 def create_data_providers(count):
     data_types = [gdt.value for gdt in list(GeospatialDataType)]
     try:
-        print(f"Creating {count} DataProviders.")
+        logger.info(f"Creating {count} DataProviders.")
+        created_data_providers = []
         for provider in batch_create(DataProvider,
-                          (DataProvider(name=f"Product {i}", slug=f"{PREFIX}{i}", data_type=random.choice(data_types),
-                                        export_provider_type_id=1, display=True) for i in range(count))):
-            pass
+                                      (DataProvider(name=f"Product {i}", slug=f"{PREFIX}{i}", data_type=random.choice(data_types),
+                                                    export_provider_type_id=1, display=True) for i in range(count))):
+            created_data_providers += [provider]
+        logger.info(f"Returning {created_data_providers} DataProviders.")
+        return created_data_providers
 
     except Exception as e:
-        logger.error(e)
+        logger.error(e, exc_info=True)
 
 
 def create_downloads():
@@ -216,31 +251,65 @@ def create_downloads():
         raise Exception(f"Can't create downloads without test users already created.")
     fptr_ids = list(get_fptrs().values_list("pk", flat=True))
     try:
-        print(f"Creating downloads associated with {len(fptr_ids)} task results.")
+        logger.info(f"Creating downloads associated with {len(fptr_ids)} task results.")
         for downloads in batch_create(UserDownload, (UserDownload(user_id=user_id, downloadable_id=fptr_id,
-                                                 downloaded_at=get_random_time("2000-01-01", "2022-07-22")) for fptr_id
-                                    in fptr_ids for user_id in random.sample(user_ids, random.randint(0, user_count)))):
+                                                                  downloaded_at=get_random_time("2000-01-01", "2022-07-22")) for fptr_id
+                                                     in fptr_ids for user_id in random.sample(user_ids, random.randint(0, user_count)))):
             pass
 
     except Exception as e:
         logger.error(e)
-    print("Downloads created.")
+    logger.info("Downloads created.")
+
+
+def create_attribute_classes(count: int):
+    try:
+        logger.info(f"Creating {count} AttributeClass")
+        for count_value in range(count):
+            attrib = AttributeClass.objects.create(name=f"AttributeClass-{count_value}", slug=f"{PREFIX}-AttributeClass-{count_value}", complex=[f"{count_value%2}", "==", "group"])
+            # attrib.save()
+            logger.info(f"Updating users with attribute classes {attrib}")
+            update_all_users_with_attribute_class(attrib)
+    except Exception:
+        logger.error("Failed to create all AttributeClass.", exc_info=True)
+
+
+def divide_chunks(items: list, chunk_size: int):
+    logger.info(f"Breaking up {items} into {chunk_size} chunk(s)")
+    # looping till length items
+    size = chunk_size or 1
+    for i in range(0, len(items), size):
+        out = items[i:i + size]
+        logger.info(out)
+        yield out
+
+
+def create_topics(providers: list, count: int):
+    logger.info(f"Setting topics with {providers}")
+    for index, provider_chunk in enumerate(divide_chunks(providers, int(len(providers) / count))):
+        topic = Topic.objects.create(name=f"Topic-{index}", slug=f"{PREFIX}-Topic-{index}", 
+                                     topic_description=f"example description for {index}")
+        logger.info(f"Setting {topic} with {provider_chunk}")
+        topic.providers.set(provider_chunk)
+        topic.save()
 
 
 def create_test_data():
     reset_queries()
     count = 500  # A random number of products.
     create_users(count * 10)  # Random assumption that an average of 10 users came for each different product.
+    create_attribute_classes(4)
     create_groups(int(count / 10))  # Every 100 users makes a group.
-    create_data_providers(count)
+    providers = create_data_providers(count)
+    create_topics(list(providers), 5)
     create_runs(count * 10)  # Random assumption that every user will make 10 exports on average.
     create_downloads()
-    print(f"Query count: {len(connection.queries)}")
+    logger.info(f"Query count: {len(connection.queries)}")
 
 
 if __name__ == "__main__":
     """
-    To run this script you need a geojson file which you can define at the top of the script 
+    To run this script you need a geojson file which you can define at the top of the script
     From an EK image run
     """
     import sys
