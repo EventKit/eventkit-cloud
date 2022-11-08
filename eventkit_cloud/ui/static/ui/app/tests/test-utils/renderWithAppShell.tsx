e651295fb84fb990d57b2d7aa09b1520717b62be
JWileczek <john.wileczek@prominentedge.com>
2022-11-08 10:04:51 -0500

    update deprecated calls to createMuiTheme with createTheme

--- eventkit_cloud/ui/static/ui/app/tests/test-utils/renderWithAppShell.tsx
+++ eventkit_cloud/ui/static/ui/app/tests/test-utils/renderWithAppShell.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import ekTheme from '../../styles/eventkit_theme';
-import {createMuiTheme, MuiThemeProvider} from "@material-ui/core/styles";
+import {MuiThemeProvider, createTheme} from "@material-ui/core/styles";
 import { render } from "@testing-library/react";
 import { AppConfigProvider, ApplicationContext } from "../../components/ApplicationContext";
 import { Provider } from "react-redux";
@@ -9,7 +9,7 @@ import { getDefaultTestState } from "./defaultTestState";
 import { ToastContainer } from "react-toastify";
 import { MemoryRouter } from "react-router";
 
-const theme = createMuiTheme(ekTheme);
+const theme = createTheme(ekTheme);
 
 export const renderComponent = (component: any, {
     initialState = getDefaultTestState(),
